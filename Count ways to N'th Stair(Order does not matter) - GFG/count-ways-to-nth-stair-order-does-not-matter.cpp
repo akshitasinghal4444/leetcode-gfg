a5f61e2ee2d229d65a7458e1488d951ff97fa4ae
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-01-26 21:41:58 +0530

    Added solution - LeetHub

---
+++ Count ways to N'th Stair(Order does not matter) - GFG/count-ways-to-nth-stair-order-does-not-matter.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        // your code here
+        vector<long long> dp(m+1,0);
+        dp[0]=1;
+        int i;
+        
+        for(i=1;i<=m;i++)
+        dp[i]+=dp[i-1];
+        
+        for(i=2;i<=m;i++)
+        dp[i]+=dp[i-2];
+        
+        return dp[m];
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}  // } Driver Code Ends
