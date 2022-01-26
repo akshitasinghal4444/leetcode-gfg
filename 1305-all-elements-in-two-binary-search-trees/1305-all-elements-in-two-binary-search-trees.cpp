c2721e03a1658fb918c31fddddbe29ef6e6a0f67
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-01-26 13:39:56 +0530

    Time: 166 ms (49.95%), Space: 85.7 MB (42.32%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,6 +11,51 @@
  */
 class Solution {
 public:
+    void inorder(stack<int> &ans,TreeNode* r)
+    {
+        if(!r)
+            return;
+        
+        inorder(ans,r->right);
+        ans.push(r->val);
+        inorder(ans,r->left);        
+    }
+    
+    vector<int> getAllElements(TreeNode* r1, TreeNode* r2) {
+        vector<int> ans;
+        stack<int> t1,t2;
+        inorder(t1,r1);
+        inorder(t2,r2);
+        
+        while(!t1.empty() && !t2.empty())
+        {
+            if(t1.top()<=t2.top())
+            {
+                ans.push_back(t1.top());
+                t1.pop();
+            }
+            else
+            {
+                ans.push_back(t2.top());
+                t2.pop();
+            }
+        }
+        while(!t1.empty())
+        {
+            ans.push_back(t1.top());
+            t1.pop();
+        }
+        
+        while(!t2.empty())
+        {
+            ans.push_back(t2.top());
+            t2.pop();
+        }
+        
+        return ans;
+    }
+    
+    /*
     void inorder(vector<int> &ans,TreeNode* r)
     {
         if(!r)
@@ -51,4 +96,5 @@ public:
         
         return ans;
     }
+    */
 };
