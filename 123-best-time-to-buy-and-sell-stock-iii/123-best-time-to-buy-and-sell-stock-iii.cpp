50f347c822d9fc0c9294d225b7345c3c62ee2356
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-03 22:52:48 +0530

    Time: 258 ms (29.10%), Space: 81.5 MB (33.94%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& a) {
+        int n=a.size();
+        int i,mn=INT_MAX,mx=INT_MIN,ans=0;
+        vector<int> maxl(n,0),maxr(n,0);
+        
+        for(i=0;i<n;i++)
+        {
+            mn=min(mn,a[i]);
+            
+            if(i==0)
+                maxl[i]=a[i]-mn;
+            else
+                maxl[i]=max(maxl[i-1],a[i]-mn);
+        }
+        
+        for(i=n-1;i>=0;i--)
+        {
+            mx=max(mx,a[i]);
+            
+            if(i==n-1)
+                maxr[i]=mx-a[i];
+            else
+                maxr[i]=max(maxr[i+1],mx-a[i]);
+        }
+        
+        for(i=0;i<n;i++)
+        {
+            ans=max(ans,maxl[i]+maxr[i]);
+        }
+        
+        return ans;
+    }
+};
