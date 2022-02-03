82908f3e62714272bb936aa4fcc8d0579dd4cf20
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-03 21:16:57 +0530

    Time: 149 ms (45.79%), Space: 54.9 MB (85.37%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -16,19 +16,4 @@ public:
         }
         return s;
     }
-    
-    /*
-    int maxProfit(vector<int>& a, int f) {
-        int n=a.size();
-        int i,b=-a[0],s=0,nb,ns;
-        for(i=1;i<n;i++)
-        {
-            nb=s-a[i];
-            ns=b+a[i]-f;
-            b=max(b,nb);
-            s=max(s,ns);
-        }
-        return s;
-    }
-    */
 };
