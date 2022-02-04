71f6f5f66a9b1bee579cd8bd10bf7f4618cb6f4d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-04 16:22:19 +0530

    Time: 16 ms (79.18%), Space: 16.6 MB (42.85%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
-
+    int jump(vector<int>& a)
+    {
+        int n=a.size();
+        if(n==1)
+            return 0;
+        
+        int j=0,curr_reach=0,max_reach=0;
+        int i;
+        
+        for(i=0;i<n;i++)
+        {
+            max_reach=max(max_reach,i+a[i]);
+            if(max_reach>=n-1)
+                return j+1;
+            
+            if(i==curr_reach)
+            {
+                curr_reach=max_reach;
+                j++;
+            }
+        }
+        return j;
+    }
+    
+    /*
     int jump(vector<int>& a) {
         int n=a.size();
         vector<int> dp(n,INT_MAX);
@@ -15,14 +39,11 @@ public:
                     dp[j]=dp[i]+1;
                 else
                     break;
-                
-                // if(j==n-1)
-                //     return dp[j];
             }
         }
         return dp[n-1];
     }
-    
+    */
     
     /*
     int jump(vector<int>& a) {
