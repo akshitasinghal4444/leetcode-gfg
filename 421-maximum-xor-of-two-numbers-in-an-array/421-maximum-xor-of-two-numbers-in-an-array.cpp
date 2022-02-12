87c4cb95208e59b2f84ac92b9bc11153067b6fd2
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-12 13:15:34 +0530

    Time: 285 ms (83.00%), Space: 65.3 MB (75.74%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     struct trie
@@ -31,6 +32,60 @@ public:
             t=t->next[x];
         }
         return (n^t->end);
+    }
+    
+    int findMaximumXOR(vector<int>& a) {
+        int n=a.size();
+        root=new trie();
+        int ans=0;
+        
+        for(int x:a)
+            insert(x);
+        
+        for(int x:a)
+            ans=max(ans,getxor(x));
+        
+        return ans;
+    }
+};
+*/
+
+class Solution {
+public:
+    struct trie
+    {
+        trie *next[2];
+    }*root;
+    
+    void insert(int n)
+    {
+        trie *t=root;
+        for(int i=31;i>=0;i--)
+        {
+            int x=(n>>i)&1;
+            if(!t->next[x])
+                t->next[x]=new trie();
+            t=t->next[x];
+        }
+    }
+    
+    int getxor(int n)
+    {
+        trie *t=root;
+        int ans=0;
+        for(int i=31;i>=0;i--)
+        {
+            int x=!((n>>i)&1);
+            if(!t->next[x])
+            t=t->next[!x];
+            else
+            {
+                t=t->next[x];
+                ans+=(1<<i);
+            }
+            
+        }
+        return ans;
     }
     
     int findMaximumXOR(vector<int>& a) {
