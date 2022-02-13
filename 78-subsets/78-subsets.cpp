7ac8b88812c866a1aaf8652e3c1abaa462423bdd
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-13 12:26:07 +0530

    Time: 0 ms (100.00%), Space: 10.9 MB (33.03%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> t;
+    
+    void subsets(vector<int>& a,int in,int n)
+    {
+        if(in==n)
+        {
+            ans.push_back(t);
+            return ;
+        }
+        
+        subsets(a,in+1,n);
+        t.push_back(a[in]);
+        subsets(a,in+1,n);
+        t.pop_back();
+    }
+    
+    vector<vector<int>> subsets(vector<int>& a) {
+        int n=a.size();
+        ans={};
+        t={};
+        
+        subsets(a,0,n);
+        
+        return ans;
+    }
+};
