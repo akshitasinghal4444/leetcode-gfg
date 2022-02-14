5e44270d0eed62d771b1376a222a870101c8af11
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-14 21:57:16 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (51.39%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    vector<int> getRow(int r)
+    {
+        vector<int> v={1};
+        int i;
+        long c=1;
+        
+        for(i=0;i<r;i++)
+        {
+            c=c*(r-i)/(i+1);
+            v.push_back(c);
+        }
+        
+        return v;
+    }
+    
+    /*
     vector<int> getRow(int r) {
         if(r==0)
             return {1};
@@ -24,4 +40,5 @@ public:
         }
         return ans;
     }
+    */
 };
