d6850595195d1bfd11200ec7c31cd4ec3d73b907
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-15 12:21:44 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (96.34%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -26,34 +26,4 @@ public:
         }
         return st.empty();
     }
-    
-    /*
-    bool isValid(string s) {
-        int n=s.length();
-        if(n%2!=0)
-            return false;
-        stack<char> st;
-        for(int i=0;i<n;i++)
-        {
-            switch(s[i])
-            {
-                case '(':st.push(')');
-                    break;
-                case '{':st.push('}');
-                    break;
-                case  '[':st.push(']');
-                    break;
-                case ')':
-                case '}':
-                case ']':if(st.empty() || st.top()!=s[i])
-                            return false;
-                         st.pop();
-            }
-        }
-        
-        if(st.empty())
-        return 1;
-        return 0;
-    }
-    */
 };
