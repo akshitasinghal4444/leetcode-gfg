0031ada4df38656bfbbf4578ddfdee3fe0cb8056
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-17 12:45:47 +0530

    Time: 329 ms (7.69%), Space: 79.9 MB (8.18%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,5 +1,55 @@
 class Solution {
 public:
+    int largestRectangleArea(vector<int>& a)
+    {
+        int n=a.size();
+        int lmin[n],rmin[n];
+        stack<int> s;
+        
+        int i;
+        
+        for(i=0;i<n;i++)
+        {
+            while(!s.empty() && a[s.top()]>a[i])
+            {
+                rmin[s.top()]=i;
+                s.pop();
+            }
+            s.push(i);
+        }
+        
+        while(!s.empty())
+        {
+            rmin[s.top()]=n;
+            s.pop();
+        }
+        
+        for(i=n-1;i>=0;i--)
+        {
+            while(!s.empty() && a[s.top()]>a[i])
+            {
+                lmin[s.top()]=i;
+                s.pop();
+            }
+            s.push(i);
+        }
+        
+        while(!s.empty())
+        {
+            lmin[s.top()]=-1;
+            s.pop();
+        }
+        
+        int ans=0;
+        
+        for(i=0;i<n;i++)
+        {
+            ans=max(ans,(rmin[i]-lmin[i]-1)*a[i]);
+        }
+        return ans;
+    }
+    
+    /*
     int largestRectangleArea(vector<int>& a) {
         int n=a.size();
         vector<int> l(n),r(n);
@@ -40,4 +90,5 @@ public:
 
         return ans;
     }
+    */
 };
