808261d6915709015f10cd180067bc59567ba10f
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-17 11:47:22 +0530

    Time: 8 ms (74.76%), Space: 17.3 MB (32.79%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -64,33 +64,4 @@ public:
         return dp[t];
     }
     */
-    
-    /*
-    vector<int> x;
-    void helper(vector<int>& a,int n,int t,int i,vector<vector<int>> &ans)
-    {
-        if(t==0)
-        {
-            ans.push_back(x);
-            return;
-        }
-        
-        if(i==n || t<0)
-            return;
-
-        helper(a,n,t,i+1,ans);
-        x.push_back(a[i]);
-        helper(a,n,t-a[i],i,ans);
-        x.pop_back();
-    }
-    
-    vector<vector<int>> combinationSum(vector<int>& a, int t)
-    {
-        int n=a.size();
-        vector<vector<int>> ans;
-        helper(a,n,t,0,ans);
-        return ans;
-    }
-    
-    */
 };
