7ef1a629e752875b830ccb3478997a49ffc3cd31
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-17 11:46:24 +0530

    Time: 15 ms (48.81%), Space: 17.2 MB (33.18%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
+    vector<int> v;
     
+    void helper(vector<int>& a, int in,int sum,int n,int t)
+    {
+        if(sum==t)
+        {
+            ans.push_back(v);
+            return;
+        }
+        
+        if(in==n)
+            return;
+        
+        helper(a,in+1,sum,n,t);
+        
+        if(sum+a[in]<=t)
+        {
+            v.push_back(a[in]);
+            helper(a,in,sum+a[in],n,t);
+            v.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& a, int t)
+    {
+        int n=a.size();
+        ans={};
+        v={};
+        
+        helper(a,0,0,n,t);
+        
+        return ans;
+    }
+    
+    /*
     vector<vector<int>> combinationSum(vector<int>& a, int t)
     {
         int n=a.size();
@@ -28,6 +63,7 @@ public:
         }
         return dp[t];
     }
+    */
     
     /*
     vector<int> x;
@@ -56,32 +92,5 @@ public:
         return ans;
     }
     
-    */
-    
-    /*
-    vector<vector<int>> combinationSum(vector<int>& a, int t) {
-        vector<vector<vector<int>>> dp(t+1);
-        int i,j,n=a.size();
-        
-        for(i=0;i<n;i++)
-        {
-            for(j=1;j<=t;j++)
-            {
-                if(a[i]==j)
-                {
-                    dp[j].push_back({a[i]});
-                }
-                else if(a[i]<j)
-                {
-                    for(vector<int> t:dp[j-a[i]])
-                    {
-                        t.push_back(a[i]);
-                        dp[j].push_back(t);
-                    }
-                }
-            }
-        }
-        return dp[t];
-    }
     */
 };
