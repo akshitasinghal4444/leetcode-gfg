71a549385802d3c9d1ca7bee3bc7f7dc1ead0ffd
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-19 23:46:13 +0530

    Time: 118 ms (49.72%), Space: 14.4 MB (66.04%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -26,70 +26,24 @@ public:
         return dp[amt];
     }
     
-    /*
-    int coinChange(vector<int>& a, int amt) 
+  /*  
+    int count(vector<int>& a,int n,int k)
     {
+        if(k==0)
+            return 0;
+        if(n==0)
+            return INT_MAX;
+        int x=k/a[n-1];
+        return min(count(a,n-1,k),count(a,n-1,k-x*a[n-1])!=INT_MAX?x+count(a,n-1,k-x*a[n-1]):INT_MAX);
+    }
+        
+    int coinChange(vector<int>& a, int t) {
         int n=a.size();
-        int i,j;
-        int dp[amt+1];
-        dp[0]=0;
-        for(j=1;j<=amt;j++)
-            dp[j]=INT_MAX;
-        for(i=0;i<n;i++)
-        {
-            for(j=1;j<=amt;j++)
-            {
-                if(a[i]<=j)
-                    dp[j]=min(dp[j],(dp[j-a[i]]==INT_MAX?INT_MAX:dp[j-a[i]]+1));
-            }
-        }
-        return dp[amt]==INT_MAX?-1:dp[amt];
+        int c=count(a,n,t);
+        if(c==INT_MAX)
+            return -1;
+        return c;
     }
     */
-    
-//     int count(vector<int>& a,int n,int k)
-//     {
-//         if(k==0)
-//             return 0;
-//         if(n==0)
-//             return INT_MAX;
-//         int x=k/a[n-1];
-//         return min(count(a,n-1,k),count(a,n-1,k-x*a[n-1])!=INT_MAX?x+count(a,n-1,k-x*a[n-1]):INT_MAX);
-//     }
-        
-//     int coinChange(vector<int>& a, int t) {
-//         int n=a.size();
-//         int c=count(a,n,t);
-//         if(c==INT_MAX)
-//             return -1;
-//         return c;
-//     }
-    
-    // int coinChange(vector<int>& a, int amt) 
-    // {
-    //     int n=a.size();
-    //     int dp[amt+1];
-    //     int i,j;
-    //     sort(a.begin(),a.end());
-    //     for(i=0;i<=amt;i++)
-    //     {
-    //         if(i==0)
-    //             dp[i]=0;
-    //         else
-    //             dp[i]=INT_MAX;
-    //     }
-    //     for(i=1;i<=amt;i++)
-    //     {
-    //         for(j=0;j<n;j++)
-    //         {
-    //             if(i-a[j]<0)
-    //                 break;
-    //             if(dp[i-a[j]]!=INT_MAX)
-    //                 dp[i]=min(dp[i],dp[i-a[j]]+1);
-    //         }
-    //     }
-    //     if(dp[amt]!=INT_MAX)
-    //         return dp[amt];
-    //     return -1;
-    // }
+
 };
