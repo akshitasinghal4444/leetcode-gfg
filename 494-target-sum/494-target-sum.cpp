aca2fcb0c69445f25f6dcc6c911cbdaec39d7b2d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-19 22:34:44 +0530

    Time: 992 ms (18.37%), Space: 8.9 MB (93.59%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -16,9 +16,34 @@ public:
     }
     
     int findTargetSumWays(vector<int>& a, int t) {
+        
         int n=a.size();
         c=0;
         count(a,n,t,0,0);
         return c;
     }
+    
+    /*
+    int c;
+    
+    void count(vector<int>& a, int n,int t,int in,int s)
+    {
+        if(in==n)
+        {
+            if(s==t)
+                c++;
+            return;
+        }
+        
+        count(a,n,t,in+1,s+a[in]);
+        count(a,n,t,in+1,s-a[in]);
+    }
+    
+    int findTargetSumWays(vector<int>& a, int t) {
+        int n=a.size();
+        c=0;
+        count(a,n,t,0,0);
+        return c;
+    }
+    */
 };
