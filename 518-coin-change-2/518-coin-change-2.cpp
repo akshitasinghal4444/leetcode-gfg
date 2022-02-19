c591231772063cdfd48aa0e4f5884aa6b44a99ae
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-19 22:42:34 +0530

    Time: 28 ms (52.18%), Space: 7.2 MB (83.20%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int change(int n, vector<int>& a)
+    {
+        int m=a.size();
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        int i,j;
+        
+        for(i=0;i<m;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(a[i]<=j)
+                dp[j]+=dp[j-a[i]];
+            }
+        }
+        
+        return dp[n];
+    }
+    
+    /*
+    int change(int amt, vector<int>& a) {
+        int n=a.size();
+        int i,j;
+        
+        vector<int> dp(amt+1,0);
+        dp[0]=1;
+        
+        for(i=0;i<n;i++)
+        {
+            for(j=1;j<=amt;j++)
+            {
+                if(a[i]<=j)
+                    dp[j]+=dp[j-a[i]];
+            }
+        }    
+        return dp[amt];
+    }
+    */
+};
