8a03d925408336d41b07ec6ba8902d24e61f77d3
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-19 22:20:22 +0530

    Time: 205 ms (52.35%), Space: 11.3 MB (71.49%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -31,39 +31,4 @@ public:
         return dp[n][s];
     }
     
-    /*
-     bool canPartition(vector<int>& a)
-     {
-         int j,i,n,sum=0;
-         n=a.size();
-         
-         for(i=0;i<n;i++)
-             sum+=a[i];
-         
-         if(sum%2!=0)
-             return 0;
-         
-         sum=sum/2;
-         bool dp[n+1][sum+1];
-
-         for(i=0;i<=n;i++)
-         {
-             for(j=0;j<=sum;j++)
-             {
-                 if(j==0)
-                     dp[i][j]=1;
-                 else if(i==0)
-                     dp[i][j]=0;
-                 else
-                 {
-                     if(a[i-1]>j)
-                         dp[i][j]=dp[i-1][j];
-                     else
-                         dp[i][j]=dp[i-1][j] || dp[i-1][j-a[i-1]];
-                 }
-             }
-         }
-         return dp[n][sum];
-     }
-    */
 };
