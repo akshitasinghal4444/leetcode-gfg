5535104550f1a12efa7fe0aa0fd370f28241a056
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-20 16:53:19 +0530

    Time: 4 ms (84.35%), Space: 7.3 MB (70.88%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -8,19 +8,14 @@ public:
         string t="";
         for(i=n-1;i>=0;i--)
         {
-            if(s[i]==' ')
-            {
-                if(t.size()>0)
-                {
-                    reverse(t.begin(),t.end());
-                    ans+=t;
-                    ans+=' ';
-                    t="";
-                }
-            }
-            else
-            {
+            if(s[i]!=' ')
                 t+=s[i];
+            else if(t.size()>0)
+            {
+                reverse(t.begin(),t.end());
+                ans+=t;
+                ans+=' ';
+                t="";
             }
         }
         
@@ -36,32 +31,4 @@ public:
         
         return ans;
     }
-    
-    /*
-    string reverseWords(string s) {
-        string ans="";
-        string t="";
-        int n=s.length();
-        for(int i=n-1;i>=0;i--)
-        {
-            if(s[i]!=' ')
-                t+=s[i];
-            else if(t.size())
-            {
-                reverse(t.begin(),t.end());
-                ans+=t;
-                ans+=' ';
-                t="";
-            }
-        }
-        if(t.size())
-        {
-            reverse(t.begin(),t.end());
-                ans+=t;
-        }
-        if(*ans.rbegin()==' ')
-            ans.pop_back();
-        return ans;
-    }
-    */
 };
