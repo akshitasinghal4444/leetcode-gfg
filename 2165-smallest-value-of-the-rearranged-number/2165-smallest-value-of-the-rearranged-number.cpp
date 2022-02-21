ca10b615ce9831bb7389cb4f6f914594cffa1d41
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-21 21:16:05 +0530

    Time: 0 ms (100.00%), Space: 6 MB (89.22%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -1,5 +1,61 @@
 class Solution {
 public:
+    long long smallestNumber(long long n)
+    {
+        int f[10]={0};
+        long long ans=0;
+        bool s=0;
+        
+        if(n<0)
+        {
+            s=1;
+            n=-n;
+        }
+        
+        while(n>0)
+        {
+            f[n%10]++;
+            n=n/10;
+        }
+        
+        if(!s)
+        {
+            int i=1;
+        
+            while(i<=9)
+            {
+                if(f[i]>0)
+                {
+                    ans=(ans*10)+i;
+                    f[i]--;
+                    if(f[0]>0)
+                        i=0;
+                }
+                else
+                    i++;
+            }
+        }
+        else
+        {
+            int i=9;
+            
+            while(i>=0)
+            {
+                if(f[i]>0)
+                {
+                    ans=(ans*10)+i;
+                    f[i]--;
+                }
+                else
+                    i--;
+            }
+            ans=-ans;
+        }
+
+        return ans;
+    }
+    
+    /*
     long long smallestNumber(long long n) {
         string s=to_string(abs(n));
         
@@ -25,4 +81,5 @@ public:
         
         return -stoll(s);       
     }
+    */
 };
