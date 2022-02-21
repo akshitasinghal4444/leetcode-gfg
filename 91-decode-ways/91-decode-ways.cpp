732acd05ea6244578c944867793c369f089aa58d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-21 15:29:16 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (71.55%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,5 +1,42 @@
 class Solution {
 public:
+    
+    int numDecodings(string s)
+    {
+        if(s[0]=='0')
+            return 0;
+        
+        int n=s.length();
+        int dp[n];
+        dp[0]=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(s[i]=='0')
+            {
+                if(s[i-1]=='1' || s[i-1]=='2')      //10,20
+                    dp[i]=(i-2>=0)?dp[i-2]:1;
+                else                                //00,30,40,....
+                    return 0;
+            }
+            else
+            {
+                dp[i]=dp[i-1];                      //0 !0
+                
+                if(s[i-1]!='0')
+                {
+                    string x="";
+                    x+=s[i-1];
+                    x+=s[i];
+                    if(stoi(x)<=26)                 //!0 !0 
+                        dp[i]+=(i-2>=0)?dp[i-2]:1;
+                }
+            }
+        }
+        return dp[n-1];
+    }
+    
+    /*
     int numDecodings(string s)
     {
         int n=s.length();
@@ -40,41 +77,5 @@ public:
         }
         return dp[n-1];
     }
-    /*
-    int numDecodings(string s)
-    {
-        int n=s.length();
-        int i;
-        vector<int> dp(n,0);
-        
-        if(s[0]=='0')
-            return 0;
-        
-        dp[0]=1;
-        for(i=1;i<n;i++)
-        {
-            if(s[i]=='0' && s[i-1]=='0')
-                return 0;
-            
-            if(s[i]=='0' && s[i-1]!='0')
-            {
-                if(s[i-1]=='1' || s[i-1]=='2')
-                    dp[i]+=(i-2>=0?dp[i-2]:1);
-                else
-                    return 0;
-            }
-            else if(s[i]!='0' && s[i-1]=='0')
-            {
-                dp[i]+=dp[i-1];
-            }
-            else
-            {
-                dp[i]+=dp[i-1];
-                if(stoi(s.substr(i-1,2))<=26)
-                    dp[i]+=(i-2>=0?dp[i-2]:1);
-            }
-        }
-        return dp[n-1];
-    }
-     */  
+    */
 };
