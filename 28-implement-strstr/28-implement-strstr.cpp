c4c30b72e70d82a5bd6bee3ac31938caf5450285
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-23 21:14:04 +0530

    Time: 3 ms (98.32%), Space: 7.9 MB (21.49%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,5 +1,60 @@
 class Solution {
 public:
+    int strStr(string h, string n)
+    {
+        if(n=="")
+            return 0;
+        
+        int nh=h.length(),nn=n.length();
+        if(nn>nh)
+            return -1;
+        
+        string s=n+"."+h;
+        int len=nh+nn+1;
+        int z[len];
+        z[0]=0;
+        
+        int l=0,r=0;
+        int i;
+        
+        for(i=1;i<len;i++)
+        {
+            if(i>r)
+            {
+                l=r=i;
+                
+                while(r<len && s[r]==s[r-l])
+                    r++;
+                
+                z[i]=r-l;
+                r--;
+            }
+            else
+            {
+                int j=i-l;
+                
+                if(z[j]+i<=r)
+                    z[i]=z[j];
+                else
+                {
+                    l=i;
+                    while(r<len && s[r]==s[r-l])
+                    r++;
+                
+                    z[i]=r-l;
+                    r--;
+                }
+            }
+            
+            if(z[i]==nn)
+                return i-nn-1;
+        }
+        
+        return -1;
+    }
+    
+    
+    /*
     void createlps(string &n,int nn,int *lps)
     {
         int i,j;
@@ -56,10 +111,10 @@ public:
             if(j==nn-1)
                 return i-nn+1;
         }
-        if(j==nn)
-         return i-nn;
+
         return -1;
     }
+    */
     
     /*
     bool comp(string &s1,string &s2,int in,int n)
