144f8f92f2ad248e8ced778d6cfb9db616b2a730
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-25 22:21:58 +0530

    Time: 444 ms (5.00%), Space: 7.4 MB (86.83%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    int score(vector<int>& a,int l,int r)
+    {
+        if(l>r)
+            return 0;
+        return max(a[l]-score(a,l+1,r),a[r]-score(a,l,r-1));
+        
+        // if(turn==0)
+        // {
+        //     return max(a[l]-score(a,l+1,r,!turn),a[r]-score(a,l,r-1,!turn));
+        // }
+        // else
+        //     return min(a[l]-score(a,l+1,r,!turn),a[r]-score(a,l,r-1,!turn));        
+    }
+    
+    bool PredictTheWinner(vector<int>& a) {
+        return score(a,0,a.size()-1)>=0;
+    }
+    
+    /*
     int score(vector<int>& a,int l,int r,bool turn)
     {
         if(l>r)
@@ -16,4 +35,6 @@ public:
     bool PredictTheWinner(vector<int>& a) {
         return score(a,0,a.size()-1,0)>=0;
     }
+    
+    */
 };
