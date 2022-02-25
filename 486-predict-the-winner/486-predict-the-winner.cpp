35ca41259db0693f38f2a8f0c75f526c3196f5f0
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-25 22:25:21 +0530

    Time: 4 ms (61.59%), Space: 7.8 MB (23.96%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
+    int score(vector<int>& a,int l,int r,vector<vector<int>> &dp)
+    {
+        if(l>r)
+            return 0;
+        
+        if(dp[l][r]==-1)
+        dp[l][r]=max(a[l]-score(a,l+1,r,dp),a[r]-score(a,l,r-1,dp));    
+        
+        return dp[l][r];
+    }
+    
+    bool PredictTheWinner(vector<int>& a) {
+        
+        int n=a.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        
+        return score(a,0,n-1,dp)>=0;
+    }
+    
+    /*
     int score(vector<int>& a,int l,int r)
     {
         if(l>r)
             return 0;
-        return max(a[l]-score(a,l+1,r),a[r]-score(a,l,r-1));
-        
-        // if(turn==0)
-        // {
-        //     return max(a[l]-score(a,l+1,r,!turn),a[r]-score(a,l,r-1,!turn));
-        // }
-        // else
-        //     return min(a[l]-score(a,l+1,r,!turn),a[r]-score(a,l,r-1,!turn));        
+        return max(a[l]-score(a,l+1,r),a[r]-score(a,l,r-1));     
     }
     
     bool PredictTheWinner(vector<int>& a) {
         return score(a,0,a.size()-1)>=0;
     }
+    */
     
     /*
     int score(vector<int>& a,int l,int r,bool turn)
