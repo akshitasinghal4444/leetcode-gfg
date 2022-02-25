b29d2bda2d42a5af2cc2f11212b4cf8325d2ae9a
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-25 13:08:13 +0530

    Time: 3 ms (92.41%), Space: 6.6 MB (78.81%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,5 +1,36 @@
 class Solution {
 public:
+    string countAndSay(int n)
+    {   
+        string s="1";
+        
+        while(--n)
+        {
+            string ans="";
+            int c=0;
+            char ch=s[0];
+            
+            for(char x:s)
+            {
+                if(x==ch)
+                    c++;
+                else
+                {
+                    ans+=(c+'0');
+                    ans+=ch;
+                
+                    ch=x;
+                    c=1;
+                }
+            }
+            ans+=(c+'0');
+            ans+=ch;
+            s=ans;
+        }
+        return s;
+    }
+    
+    /*
     string countAndSay(int n) {
         
         if(n==1)
@@ -27,4 +58,5 @@ public:
         
         return ans;
     }
+    */
 };
