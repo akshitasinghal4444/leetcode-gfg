52866fdfb2119aecf58d96a3b3634712a86bef07
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-27 22:23:30 +0530

    Time: 124 ms (94.73%), Space: 75.2 MB (74.66%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int maxProfit(vector<int>& a)
     {
         int i,n=a.size(),ans=0;
@@ -23,6 +24,24 @@ public:
             ans=max(ans,maxpl[i]+maxpr[i]);
         }
         return ans;
+    }
+    */
+    
+    int maxProfit(vector<int>& a)
+    {
+        int i,n=a.size();
+        int fb=-a[0],fs=INT_MIN,sb=INT_MIN,ss=INT_MIN;
+        
+        for(i=1;i<n;i++)
+        {
+            fb=max(fb,-a[i]);
+            fs=max(fs,fb+a[i]);
+            
+            sb=max(sb,fs-a[i]);
+            ss=max(ss,sb+a[i]);
+        }
+        
+        return max(ss,0);
     }
     
     /*
@@ -76,38 +95,5 @@ public:
     }
     */
     
-    /*
-    int maxProfit(vector<int>& a) {
-        int n=a.size();
-        int i,mn=INT_MAX,mx=INT_MIN,ans=0;
-        vector<int> maxl(n,0),maxr(n,0);
-        
-        for(i=0;i<n;i++)
-        {
-            mn=min(mn,a[i]);
-            
-            if(i==0)
-                maxl[i]=a[i]-mn;
-            else
-                maxl[i]=max(maxl[i-1],a[i]-mn);
-        }
-        
-        for(i=n-1;i>=0;i--)
-        {
-            mx=max(mx,a[i]);
-            
-            if(i==n-1)
-                maxr[i]=mx-a[i];
-            else
-                maxr[i]=max(maxr[i+1],mx-a[i]);
-        }
-        
-        for(i=0;i<n;i++)
-        {
-            ans=max(ans,maxl[i]+maxr[i]);
-        }
-        
-        return ans;
-    }
-    */
+    
 };
