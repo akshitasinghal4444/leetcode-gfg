d1c684b3feb143b7ffa1752c298c1f28bfa19613
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-27 23:15:30 +0530

    Time: 3 ms (97.34%), Space: 11.2 MB (69.77%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    int maxProfit(int k, vector<int>& a)
+    {
+        int i,j;
+        int n=a.size();
+        
+        if(k==0 || n==0)
+            return 0;
+        
+        int dp[k+1][n];
+        
+        for(i=0;i<=k;i++)
+        {
+            int mx=INT_MIN;
+            for(j=0;j<n;j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j]=0;
+                else
+                {
+                    mx=max(mx,dp[i-1][j-1]-a[j-1]); 
+                    dp[i][j]=max(dp[i][j-1],mx+a[j]);  
+                }
+            }
+        }
+        return dp[k][n-1];
+    }
+    
+    /*
     int maxProfit(int k, vector<int>& a) {
         int n=a.size();
         
@@ -25,4 +53,5 @@ public:
         }
         return dp[k][n-1];
     }
+    */
 };
