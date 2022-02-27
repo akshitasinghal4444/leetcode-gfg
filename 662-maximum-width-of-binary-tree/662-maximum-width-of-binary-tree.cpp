e3cf418422056032871eb3b07ade2b8a128f2145
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-02-27 21:35:58 +0530

    Time: 8 ms (85.32%), Space: 17.6 MB (14.58%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -22,6 +22,40 @@ public:
         while(!q.empty())
         {
             int s=q.size();
+            long st=q.front().second;
+            long end=q.back().second;
+            
+            for(int i=0;i<s;i++)
+            {
+                TreeNode *t=q.front().first;
+                long in=q.front().second-st;
+                q.pop();
+                
+                if(t->left)
+                    q.push({t->left,2*in+1});
+                if(t->right)
+                    q.push({t->right,2*in+2});
+                
+            }
+            
+            ans=max(ans,end-st+1);
+        }
+        
+        return ans;
+    }
+    
+    /*
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root)
+            return 0;
+        
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
+        long ans=0;
+        
+        while(!q.empty())
+        {
+            int s=q.size();
             long st,end;
             
             for(int i=0;i<s;i++)
@@ -48,4 +82,5 @@ public:
         
         return ans;
     }
+    */
 };
