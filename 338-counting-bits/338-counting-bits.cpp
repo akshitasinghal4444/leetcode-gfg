65742291cad17b37bd99ec07c2f04ec724e8f4cd
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-01 14:23:49 +0530

    Time: 7 ms (62.32%), Space: 7.9 MB (75.07%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    vector<int> countBits(int n)
+    {
+        vector<int> a(n+1);
+        a[0]=0;
+        
+        for(int i=0;i<=n/2;i++)
+        {
+            a[2*i]=a[i];
+            
+            if(2*i+1<=n)
+                a[2*i+1]=a[i]+1;
+        }
+        
+        return a;
+    }
+    
+    /*
     vector<int> countBits(int n) {
         if(n==0)
             return {0};
@@ -17,4 +34,5 @@ public:
         }
         return a;
     }
+    */
 };
