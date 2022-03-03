69d3115ba1a74ce021088997b6813d4779c8433d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-03 18:40:47 +0530

    Time: 156 ms (51.34%), Space: 24.7 MB (44.60%) - LeetHub

---
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -0,0 +1,114 @@
+class Solution {
+public:
+    int l,r;
+    unordered_set<string> st,vis;
+    
+    void countmin(string &s)
+    {
+        stack<int> st;
+        
+        for(char ch:s)
+        {
+            if(ch=='(')
+                st.push(ch);
+            else if(ch==')')
+            {
+                if(!st.empty() && st.top()=='(')
+                    st.pop();
+                else
+                    st.push(ch);
+            }
+        }
+        
+        while(!st.empty())
+        {
+            if(st.top()==')')
+                r++;
+            else
+                l++;
+            
+            st.pop();
+        }
+    }
+    
+    void removemin(string s,int c)
+    {
+        if(vis.find(s)==vis.end())
+            vis.insert(s);
+        else
+            return;
+
+        
+        if(c==0)
+        {
+            st.insert(s);
+            return;
+        }
+                
+        int n=s.length();
+        
+        for(int i=0;i<n;i++)
+        {
+            removemin(s.substr(0,i)+s.substr(i+1),c-1);
+        }     
+    }
+    
+//     void removemin(string s,int l,int r)
+//     {
+//         if(l==0 && r==0)
+//         {
+//             st.insert(s);
+//             return;
+//         }
+        
+//         int n=s.length();
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(s[i]=='(')
+//             {
+//                 removemin(s.substr(0,i)+s.substr(i+1),l-1,r);
+//             }
+//             else if(s[i]==')')
+//             {
+//                 removemin(s.substr(0,i)+s.substr(i+1),l,r-1);
+//             }
+//         }     
+//     }
+    
+    bool isValid(string s)
+    {
+        int c=0;
+        
+        for(char ch:s)
+        {
+            if(ch=='(')
+                c++;
+            else if(ch==')')
+            {
+                if(c>0)
+                    c--;
+                else return 0;
+            }
+        }
+        return c==0;
+    }
+    
+    vector<string> removeInvalidParentheses(string s) {
+        l=0;
+        r=0;
+        vector<string> ans;
+        countmin(s);
+        
+        removemin(s,l+r);
+        
+        for(string str:st)
+        {
+            // cout<<str<<endl;
+            if(isValid(str))
+                ans.push_back(str);
+        }
+        
+        return ans;
+    }
+};
