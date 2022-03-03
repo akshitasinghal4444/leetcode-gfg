d9255ba5256cee5f103e7d40eac9f02a05198eac
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-03 22:14:39 +0530

    Time: 16 ms (97.20%), Space: 17.8 MB (39.03%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+        int candy(vector<int>& a) {
+        int n=a.size();
+        int i;
+        vector<int> l(n,1);
+        int ans=0;
+        
+        for(i=1;i<n;i++)
+        {
+            if(a[i]>a[i-1])
+                l[i]=l[i-1]+1;
+        }
+        
+        ans+=l[n-1];
+        int r=1;
+        for(i=n-2;i>=0;i--)
+        {
+            if(a[i]>a[i+1])
+                r=r+1;
+            else
+                r=1;
+            
+            ans+=max(l[i],r);
+        }
+        
+        return ans;
+    }
+    
+    /*
     int candy(vector<int>& a) {
         int n=a.size();
         int i;
@@ -24,4 +52,5 @@ public:
         }
         return ans;
     }
+    */
 };
