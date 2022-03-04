3f007838836c1ab109fb12e30ab765b39f3f54cc
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-04 15:25:03 +0530

    Time: 8 ms (73.16%), Space: 10 MB (64.05%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -1,5 +1,46 @@
 class Solution {
 public:
+    double champagneTower(int p, int r, int g)
+    {
+        if(p==0)
+            return 0;
+        
+        vector<double> v1,v2;
+        
+        v1.push_back(p);
+        
+        int row=1;
+        while(row<=r)
+        {
+            int empty=0;
+            if(v1[0]==0)
+                empty++;
+            
+            v2.push_back(max(0.0,(v1[0]-1)/2));
+                   
+            int i,n=v1.size();
+            for(int i=1;i<n;i++)
+            {
+                if(v1[i]==0)
+                    empty++;
+                
+                v2.push_back(max(0.0,(v1[i]-1)/2)+max(0.0,(v1[i-1]-1)/2));
+            }
+            
+            v2.push_back(max(0.0,(v1[0]-1)/2));
+            
+            if(empty==n)
+                return 0;
+            
+            v1=v2;
+            v2.clear();
+            row++;
+        }
+        
+        return min(v1[g],1.0);
+    }
+    
+    /*
     double champagneTower(int p, int r, int g) {
        if(p==0)
            return 0;
@@ -39,4 +80,5 @@ public:
         }
         return min(1.0,a[g]);
     }
+    */
 };
