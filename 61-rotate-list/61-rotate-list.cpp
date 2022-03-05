dffb4a5dfa2331a59cf7fc03d3699a2312e5ed38
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-05 20:37:01 +0530

    Time: 14 ms (34.83%), Space: 11.7 MB (55.92%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.cpp
@@ -0,0 +1,117 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k)
+    {
+        if(!head)
+            return head;
+        
+        int len=1;
+        ListNode *t=head,*t2;
+        while(t->next)
+        {
+            t=t->next;
+            len++;
+        }
+        
+        k=k%len;
+        
+        if(k==0)
+            return head;
+        
+        t->next=head;
+        
+        t=head;
+        for(int i=0;i<len-k-1;i++)
+        {
+            t=t->next;
+        }
+        head=t->next;
+        t->next=NULL;
+        
+        return head;
+    }
+    
+    /*
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head)
+            return head;
+        
+        ListNode *t;
+        int l=1;
+        t=head;
+        
+        while(t->next)
+        {
+            l++;
+            t=t->next;
+        }
+        
+        k=k%l;
+        if(k==0)
+            return head;
+        
+        t->next=head;
+        t=head;
+        
+        for(int i=0;i<(l-k-1);i++)
+            t=t->next;
+        
+        head=t->next;
+        t->next=NULL;
+
+        return head;
+    }
+    */
+    
+    /*
+    int len(ListNode* head)
+    {
+        int l=0;
+        while(head)
+        {
+            head=head->next;
+            l++;
+        }
+        return l;
+    }
+    
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head)
+            return head;
+        
+        int l=len(head);
+        
+        k=k%l;
+        if(k==0)
+            return head;
+        
+        ListNode *f,*s;
+        f=s=head;
+        int i=0;
+        while(i++<k)
+        {
+            f=f->next;
+        }
+        while(f->next)
+        {
+            f=f->next;
+            s=s->next;
+        }
+        f->next=head;
+        head=s->next;
+        s->next=NULL;
+
+        return head;
+    }
+    */
+};
