00354236f847b3afeb10f9cb9ec8b1917dbb6af3
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-07 12:38:35 +0530

    Time: 19 ms (14.53%), Space: 14.8 MB (82.97%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -17,6 +17,31 @@ public:
         if(!l2)
             return l1;
         
+        ListNode *t;
+        
+        if(l1->val<=l2->val)
+        {
+            t=l1;
+            l1=l1->next;
+        }
+        else
+        {
+            t=l2;
+            l2=l2->next;
+        }
+        
+        t->next=mergeTwoLists(l1,l2);
+        return t;
+    }
+    
+    /*
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+    {
+        if(!l1)
+            return l2;
+        if(!l2)
+            return l1;
+        
         ListNode *t,*h;
         
         if(l1->val<=l2->val)
@@ -54,49 +79,6 @@ public:
         
         return h;
     }
-    
-    /*
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode *t,*h;
-        if(!l1)
-            return l2;
-        
-        if(!l2)
-            return l1;
-        
-        if(l1->val<=l2->val)
-        {
-            t=h=l1;
-            l1=l1->next;
-        }
-        else
-         {
-            t=h=l2;
-            l2=l2->next;
-        } 
-        
-        while(l1 && l2)
-        {
-            if(l1->val<=l2->val)
-            {
-                t->next=l1;
-                t=t->next;
-                l1=l1->next;
-            }
-            else
-            {
-                t->next=l2;
-                t=t->next;
-                l2=l2->next;
-            }   
-        }
-        
-        if(l1)
-            t->next=l1;
-        if(l2)
-            t->next=l2;
-        
-        return h;
-    }
     */
+   
 };
