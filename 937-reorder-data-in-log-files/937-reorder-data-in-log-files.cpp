d50b55c4127ea39b7867e9caa6113a24649ce4dc
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-12 21:53:54 +0530

    Time: 15 ms (64.92%), Space: 14.2 MB (23.82%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -1,5 +1,44 @@
 class Solution {
 public:
+    bool isdigitlog(string &s)
+    {
+        return s.back()>='0' && s.back()<='9';
+    }
+    
+    static bool comp(string s1,string s2)
+    {
+        string t1,t2;
+        t1=s1.substr(s1.find(' '));
+        t2=s2.substr(s2.find(' '));
+        
+        if(t1==t2)
+            return s1<s2;
+        
+        return t1<t2;
+    }
+    
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        vector<string> digit,ans;
+        int n=logs.size();
+        int i;
+        
+        for(i=0;i<n;i++)
+        {
+            if(isdigitlog(logs[i]))
+                digit.push_back(logs[i]);
+            else
+                ans.push_back(logs[i]);
+        }
+        
+        sort(ans.begin(),ans.end(),comp);
+                
+        for(string s:digit)
+            ans.push_back(s);
+        
+        return ans;
+    }
+    
+    /*
     bool isdigitlog(string s)
     {
         int i,n=s.length();
@@ -61,4 +100,5 @@ public:
         
         return ans;
     }
+    */
 };
