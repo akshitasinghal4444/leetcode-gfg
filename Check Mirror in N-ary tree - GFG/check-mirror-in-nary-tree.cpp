255ec80228ada31db392676c62076cebdab758c2
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-13 11:07:57 +0530

    Added solution - LeetHub

--- Check Mirror in N-ary tree - GFG/check-mirror-in-nary-tree.cpp
+++ Check Mirror in N-ary tree - GFG/check-mirror-in-nary-tree.cpp
@@ -9,6 +9,29 @@ using namespace std;
 
 class Solution {
   public:
+  int checkMirrorTree(int n, int e, int a[], int b[])
+  {
+      unordered_map<int,vector<int>> m1,m2;
+      int i;
+      
+      for(i=0;i<e;i++)
+      {
+          m1[a[2*i]].push_back(a[2*i+1]);
+          m2[b[2*i]].push_back(b[2*i+1]);
+      }
+      
+      for(auto it:m1)
+      {
+          int x=it.first;
+          reverse(m2[x].begin(),m2[x].end());
+          
+          if(m1[x]!=m2[x])
+          return 0;
+      }
+      return 1;
+  }
+  
+  /*
     int checkMirrorTree(int n, int e, int A[], int B[]) {
         // code here
         vector<vector<int>> TA(n+1),TB(n+1);
@@ -26,6 +49,7 @@ class Solution {
         }
         return true;
     }
+    */
 };
 
 // { Driver Code Starts.
