144c8e76b282fbd222e8dbb307c277080a0dd578
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-15 20:39:43 +0530

    Time: 1609 ms (5.00%), Space: 57.1 MB (86.74%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,5 +1,54 @@
 class Solution {
 public:
+    int partition(vector<vector<int>>& a, int l,int r)
+    {
+        int x=rand()%(r-l+1)+l;
+        swap(a[x],a[r]);
+    
+        long pivot=a[r][0]*a[r][0]+a[r][1]*a[r][1];
+        int i=l,j=l;
+        
+        while(j<r)
+        {
+            long d=a[j][0]*a[j][0]+a[j][1]*a[j][1];
+                
+            if(d<pivot)
+            {
+                swap(a[i],a[j]);
+                i++;
+            }
+            j++;
+        }
+        
+        swap(a[i],a[r]);
+        return i;
+    }
+    
+    void quickselect(vector<vector<int>>& a, int l,int r,int k)
+    {
+        if(l<=r)
+        {
+            int p=partition(a,l,r);
+            
+            if(p==k)
+                return;
+            else if(p>k)
+                quickselect(a,l,p-1,k);
+            else
+                quickselect(a,p+1,r,k);
+        }
+    }
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& a, int k)
+    {
+        quickselect(a,0,a.size()-1,k-1);
+        vector<vector<int>> ans(a.begin(),a.begin()+k);
+        
+        return ans;
+    }
+    // 19,98,80,018
+    // 19,98,80,020
+    /*
     static bool comp(vector<int> a,vector<int> b)
     {
         long d1,d2;
@@ -10,13 +59,13 @@ public:
     }
     
     vector<vector<int>> kClosest(vector<vector<int>>& a, int k)
-    {
-        int n=a.size();        
+    {        
         sort(a.begin(),a.end(),comp);
         vector<vector<int>> ans(a.begin(),a.begin()+k);
         
         return ans;
     }
+    */
     
     /*
     vector<vector<int>> kClosest(vector<vector<int>>& a, int k)
