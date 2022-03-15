1853fac61443066d7cb5b73481a003992c90ad34
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-15 11:25:57 +0530

    Time: 6 ms (73.95%), Space: 7.1 MB (97.81%) - LeetHub

--- 71-simplify-path/71-simplify-path.cpp
+++ 71-simplify-path/71-simplify-path.cpp
@@ -2,6 +2,61 @@ class Solution {
 public:
     string simplifyPath(string p)
     {
+        if(p.back()!='/')
+            p+='/';
+        
+        int i,n=p.length();
+        string s="";
+        int flag=0;
+        
+        for(i=n-2;i>=0;i--)
+        {
+            if(p[i]=='/' && (p[i+1]=='/' || s.back()=='/'))
+                continue;
+            else if(p[i]=='.' && p[i+1]=='/')
+            {
+                int c=0;
+                while(i>=0 && p[i]=='.')
+                {
+                    c++;
+                    i--;
+                }
+                
+                if((i>=0 && p[i]!='/') || c>2)
+                {
+                    while(c--)
+                        s+='.';
+                }
+                else if(c==2)
+                    flag++;
+                
+                i++;
+            }
+            else if(flag>0 && p[i]!='/')
+            {
+                while(i>=0 && p[i]!='/')
+                    i--;
+                
+                flag--;
+            }
+            else
+                s+=p[i];
+        }
+        
+        reverse(s.begin(),s.end());
+        
+        if(s.back()=='/' && s.size()>1)
+            s.pop_back();
+            
+        if(s.size()==0)
+            return "/";
+        
+        return s;
+    }
+    
+    /*
+    string simplifyPath(string p)
+    {
         int i,n=p.length();
         string s="/";
         
@@ -42,6 +97,7 @@ public:
         
         return s;
     }
+    */
     
     /*
     string simplifyPath(string path) {
