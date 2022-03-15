62b27bcf6d939e8cc424ddceaf4588fca5a443a5
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-15 10:56:22 +0530

    Added solution - LeetHub

--- Biconnected Graph - GFG/biconnected-graph.cpp
+++ Biconnected Graph - GFG/biconnected-graph.cpp
@@ -5,6 +5,29 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
+    int biGraph(int a[], int n, int e)
+    {
+        
+        if(n==1 || (n==2 && e==1))
+        return 1;
+        
+        vector<int> f(n);
+        
+        for(int i=0;i<e;i++)
+        {
+            f[a[2*i]]++;
+            f[a[2*i+1]]++;
+        }
+        
+        for(int x:f)
+        {
+            if(x<2)
+            return 0;
+        }
+        return 1;
+    }
+  
+  /*
     int biGraph(int arr[], int n, int e) {
        // code here
        if(n==1)return 1;
@@ -23,6 +46,7 @@ class Solution {
        }
        return 1;
    }
+   */
 };
 
 // { Driver Code Starts.
