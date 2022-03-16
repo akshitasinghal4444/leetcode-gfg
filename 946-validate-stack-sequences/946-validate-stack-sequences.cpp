b739a97fb04dccf42088d32bbf8296c256ca6138
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-16 10:56:56 +0530

    Time: 10 ms (70.16%), Space: 15.4 MB (59.16%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -15,6 +15,35 @@ public:
 
         while(i<n1 && j<n2)
         {
+            st.push(pushed[i]);
+            i++;
+            
+            while(j<n2 && !st.empty() && st.top()==popped[j])
+            {
+                st.pop();
+                j++;
+            }
+        }
+        
+        return st.empty();
+    }
+    
+    /*
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n1,n2;
+        n1=pushed.size();
+        n2=popped.size();
+        
+        if(n1!=n2)
+            return 0;
+        
+        int i,j;
+        i=0,j=0;
+        
+        stack<int> st;
+
+        while(i<n1 && j<n2)
+        {
             while(i<n1 && (st.empty() || st.top()!=popped[j]))
             {
                 st.push(pushed[i]);
@@ -33,4 +62,5 @@ public:
         
         return 0;
     }
+    */
 };
