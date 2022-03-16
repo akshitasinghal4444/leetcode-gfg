084890e8e9719caa29345cc45c1bb132555a74c0
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-16 19:05:30 +0530

    Time: 145 ms (6.68%), Space: 74.9 MB (5.01%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,5 +1,66 @@
 class Solution {
 public:
+    void bfs(vector<vector<char>>& g,int m,int n,int i,int j)
+    {
+        queue<pair<int,int>> q;
+        g[i][j]='0';
+        
+        if(i-1>=0 && g[i-1][j]=='1')
+        {
+            q.push({i-1,j});
+            g[i-1][j]='0';
+        }
+        
+        if(j+1<n && g[i][j+1]=='1')
+        {
+            q.push({i,j+1});
+            g[i][j+1]='0';
+        }
+        
+        if(i+1<m && g[i+1][j]=='1')
+        {
+            q.push({i+1,j});
+            g[i+1][j]='0';
+        }
+        
+        if(j-1>=0 && g[i][j-1]=='1')
+        {
+            q.push({i,j-1});
+            g[i][j-1]='0';
+        }
+        
+        while(!q.empty())
+        {
+            i=q.front().first;
+            j=q.front().second;
+            
+            q.pop();
+            bfs(g,m,n,i,j);
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& g)
+    {
+        int m,n;
+        m=g.size();
+        n=g[0].size();
+        int i,j,c=0;
+        
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(g[i][j]=='1')
+                {
+                    bfs(g,m,n,i,j);
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+    
+    /*
     void dfs(vector<vector<char>>& g,int m,int n,int i,int j)
     {
         g[i][j]='0';
@@ -37,38 +98,6 @@ public:
             }
         }
         return c;
-    }
-    
-    /*
-    bool isValid(vector<vector<char>>& g,int i,int j,int m,int n)
-    {
-        return (i>=0 && j>=0 && i<m && j<n && g[i][j]=='1');
-    }
-    void dfs(vector<vector<char>>& g,int i,int j,int m,int n)
-    {
-        g[i][j]='0';
-        int dx[]={-1,0,0,1},dy[]={0,-1,1,0};
-        int k;
-        for(k=0;k<4;k++)
-        {
-            if(isValid(g,i+dx[k],j+dy[k],m,n))
-                dfs(g,i+dx[k],j+dy[k],m,n);
-        }
-    }
-    int numIslands(vector<vector<char>>& g) {
-        int m,n;
-        m=g.size();
-        n=g[0].size();
-        int ans=0;
-        int i,j;
-        for(i=0;i<m;i++)
-            for(j=0;j<n;j++)
-                if(g[i][j]!='0')
-                {
-                    ans++;
-                    dfs(g,i,j,m,n);
-                }
-        return ans;
     }
     */
 };
