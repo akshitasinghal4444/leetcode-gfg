676815fd071df2d0efd8b34fd5977ef20de35a85
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-16 11:08:57 +0530

    Time: 8 ms (83.81%), Space: 15.1 MB (99.15%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped)
+    {
+        int n1,n2;
+        n1=pushed.size();
+        n2=popped.size();
+        
+        if(n1!=n2)
+            return 0;
+        
+        int i,j;
+        i=0,j=0;
+        
+        for(auto n:pushed)
+        {
+            pushed[i++]=n;
+            
+            while(i>0 && pushed[i-1]==popped[j])
+            {
+                i--;
+                j++;
+            }
+        }
+        return i==0;
+    }
+    
+    /*
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         int n1,n2;
         n1=pushed.size();
@@ -26,41 +52,6 @@ public:
         }
         
         return st.empty();
-    }
-    
-    /*
-    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        int n1,n2;
-        n1=pushed.size();
-        n2=popped.size();
-        
-        if(n1!=n2)
-            return 0;
-        
-        int i,j;
-        i=0,j=0;
-        
-        stack<int> st;
-
-        while(i<n1 && j<n2)
-        {
-            while(i<n1 && (st.empty() || st.top()!=popped[j]))
-            {
-                st.push(pushed[i]);
-                i++;
-            }
-            
-            while(j<n2 && !st.empty() && st.top()==popped[j])
-            {
-                st.pop();
-                j++;
-            }
-        }
-        
-        if(i==n1 && j==n2)
-            return 1;
-        
-        return 0;
     }
     */
 };
