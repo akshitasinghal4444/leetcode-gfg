649d00ba5f1ea8927962259508259908f64fe520
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-17 10:24:15 +0530

    Time: 4 ms (21.82%), Space: 6.2 MB (45.05%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -2,6 +2,29 @@ class Solution {
 public:
     int scoreOfParentheses(string s) 
     {
+        int ans=0;
+        int pwr=0;
+        int i,n=s.length();
+        
+        for(i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+                pwr++;
+            else
+            {
+                pwr--;
+                
+                if(s[i-1]=='(')
+                    ans+=(1<<pwr);
+            }
+        }
+        
+        return ans;
+    }
+    
+    /*
+    int scoreOfParentheses(string s) 
+    {
         stack<int> st;
         
         for(char ch:s)
@@ -35,66 +58,6 @@ public:
         
         return ans;
     }
-    
-    /*
-    int scoreOfParentheses(string s) 
-    {
-        int pwr=0;
-        int ans=0;
-        int i,n=s.length();
-        
-        for(i=0;i<n;i++)
-        {
-            if(s[i]=='(')
-                pwr++;
-            else 
-            {
-                pwr--;
-                if(s[i-1]=='(')
-                    ans+=(1<<pwr);
-            }
-        }
-        return ans;
-    }
     */
-    
-    /*
-    int scoreOfParentheses(string s) {
-        
-        stack<int> st;
-        
-        for(char c:s)
-        {
-            if(c=='(')
-                st.push(-1);
-            else
-            {
-                if(st.top()==-1)
-                {
-                    st.pop();
-                    st.push(1);
-                }
-                else
-                {
-                    int x=0;
-                    while(!st.empty() && st.top()!=-1)
-                    {
-                        x+=st.top();
-                        st.pop();
-                    }
-                    st.pop();
-                    st.push(2*x);
-                }
-            }
-        }
-        int x=0;
-        while(!st.empty())
-        {
-            x+=st.top();
-            st.pop();
-        }
-        
-        return x;
-    }
-    */
+
 };
