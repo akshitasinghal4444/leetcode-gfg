5d2594167d87ce1754173379f2cf9b14c9b30c74
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-17 10:19:25 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (8.12%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -2,6 +2,43 @@ class Solution {
 public:
     int scoreOfParentheses(string s) 
     {
+        stack<int> st;
+        
+        for(char ch:s)
+        {
+            if(ch=='(')
+                st.push(-1);
+            else
+            {
+                int x=0;
+                while(!st.empty() && st.top()!=-1)
+                {
+                    x+=st.top();
+                    st.pop();
+                }
+                
+                st.pop();
+                
+                if(x==0)
+                    st.push(1);
+                else
+                    st.push(2*x);
+            }
+        }
+        
+        int ans=0;
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        
+        return ans;
+    }
+    
+    /*
+    int scoreOfParentheses(string s) 
+    {
         int pwr=0;
         int ans=0;
         int i,n=s.length();
@@ -19,6 +56,7 @@ public:
         }
         return ans;
     }
+    */
     
     /*
     int scoreOfParentheses(string s) {
