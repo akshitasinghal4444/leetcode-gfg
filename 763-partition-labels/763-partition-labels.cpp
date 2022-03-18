16ef39584482c6ba52b9e2157c764e00db2da2f2
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-18 18:29:47 +0530

    Time: 9 ms (25.78%), Space: 6.8 MB (23.89%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> ans;
+        int i,n=s.length();
+        
+        unordered_map<char,int> last;
+        
+        for(i=0;i<n;i++)
+            last[s[i]]=i;
+        
+        int b=0,e=0;
+        
+        for(i=0;i<n;i++)
+        {
+            if(i>e)
+            {
+                ans.push_back(e-b+1);
+                b=i;
+                e=last[s[i]];
+            }
+            else if(last[s[i]]>e)
+                    e=last[s[i]];
+            
+        }
+        
+        ans.push_back(e-b+1);
+        
+        return ans;
+    }
+};
