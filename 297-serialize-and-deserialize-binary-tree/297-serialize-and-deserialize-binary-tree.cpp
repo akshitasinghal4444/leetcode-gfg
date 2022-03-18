d1fd0d27d59c527009fc6871b42b41b156bb4254
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-18 20:07:14 +0530

    Time: 680 ms (5.01%), Space: 715.5 MB (5.11%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -9,6 +9,77 @@
  */
 class Codec {
 public:
+    
+    // Encodes a tree to a single string.
+    string s;
+    
+    void preorder(TreeNode* root)
+    {
+        if(!root)
+        {
+            s+='n';
+            s+=',';
+            return ;
+        } 
+        
+        s+=to_string(root->val);
+        s+=',';
+        
+        preorder(root->left);
+        preorder(root->right);
+    }
+    
+    string serialize(TreeNode* root) {
+        
+        if(!root)
+            return "";
+        
+        s="";
+        preorder(root);
+        
+        return s;
+    }
+
+    // Decodes your encoded data to tree.
+    int i;
+    
+    TreeNode* deserialize(string s,int n)
+    {
+        string t="";
+        for(i;i<n;i++)
+        {
+            if(s[i]==',')
+                break;
+            
+            t+=s[i];
+        }
+        i++;
+        
+        if(t=="n")
+            return NULL;
+        
+        TreeNode *root=new TreeNode(stoi(t));
+        root->left=deserialize(s,n);
+        root->right=deserialize(s,n);
+        
+        return root;
+    }
+    
+    TreeNode* deserialize(string s) {
+        
+        if(s.size()==0)
+            return NULL;
+        
+        int n=s.size();  
+        i=0;
+        return deserialize(s,n);
+    }
+};
+
+/*
+class Codec {
+public:
+    
     void serialize(TreeNode* root,string &s)
     {
         if(!root)
@@ -104,6 +175,7 @@ public:
         return root;
     }
 };
+*/
 
 /*
 class Codec {
