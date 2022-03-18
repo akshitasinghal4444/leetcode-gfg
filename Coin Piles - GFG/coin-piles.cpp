0b0027a6b39174bb04970440ed8478c4b53f2f24
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-18 21:51:11 +0530

    Added solution - LeetHub

--- Coin Piles - GFG/coin-piles.cpp
+++ Coin Piles - GFG/coin-piles.cpp
@@ -8,6 +8,42 @@ using namespace std;
 
 class Solution {
   public:
+  int minSteps(int a[], int n, int k)
+  {
+      sort(a,a+n);
+      int i;
+      
+      int p[n];
+      p[0]=a[0];
+      for(i=1;i<n;i++)
+        p[i]=p[i-1]+a[i];
+        
+        
+      int ans=INT_MAX;    
+      for(i=0;i<n;i++)
+      {
+          if(i>0 && a[i]==a[i-1])
+          continue;
+          
+          int prev=(i==0)?0:p[i-1];
+          int in=upper_bound(a,a+n,a[i]+k)-a;
+          int fwd=(in!=n)?p[n-1]-p[in-1]-(a[i]+k)*(n-in):0;
+          
+        //   cout<<in<<endl;
+          ans=min(ans,prev+fwd);
+      }
+      return ans;
+  }
+  
+  // 5 3 9 3 6 5 2 7 2
+  
+  // 0 1 2 3 4 5 6 7 8
+  // 2 2 3 3 5 5 6 7 9
+  
+
+
+  
+  /*
     int minSteps(int A[], int N, int K) {
        sort(A,A+N);
        
@@ -26,6 +62,7 @@ class Solution {
        
        return (mn);
    }
+   */
 };
 
 
