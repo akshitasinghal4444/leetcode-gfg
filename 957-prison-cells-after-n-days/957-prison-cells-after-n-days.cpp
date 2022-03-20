c1b70cacf0227d372b5d6bfdcfd9f8d6bd14742b
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-20 20:18:17 +0530

    Time: 12 ms (27.37%), Space: 12.4 MB (49.52%) - LeetHub

--- 957-prison-cells-after-n-days/957-prison-cells-after-n-days.cpp
+++ 957-prison-cells-after-n-days/957-prison-cells-after-n-days.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int n)
+    {
+        int i,j;
+        vector<vector<int>> a;
+        
+        for(i=0;i<n;i++)
+        {
+            vector<int> t(8,0);
+            
+            for(j=1;j<7;j++)
+            {
+                if(cells[j-1]==cells[j+1])
+                    t[j]=1;
+            }
+            
+            if(i!=0 && t==a[0])
+            {
+                if(n%a.size()>0)
+                    return a[n%a.size()-1];
+                
+                return a.back();
+            }
+            else
+                a.push_back(t);
+            
+            cells=t;
+        }
+        
+        return cells;
+    }
+    
+    /*
     vector<int> prisonAfterNDays(vector<int>& cells, int n) {
         vector<vector<int>> a;
 
@@ -28,4 +60,5 @@ public:
         
         return cells;
     }
+    */
 };
