7b9ed0bc6e3ba623856a5d0df1b9194b71cda7bf
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-20 12:39:37 +0530

    Time: 228 ms (27.91%), Space: 111.7 MB (29.27%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+    int minDominoRotations(vector<int>& t, vector<int>& b) {
+        int n=t.size();
+        int i,j,ans=INT_MAX;
+        
+        // c1 - store count of t[0] in top & bottom
+        // c2 - store count of b[0] in top & bottom
+        int c1[2]={0},c2[2]={0};
+        bool f1=0,f2=0;
+            
+        for(i=0;i<n;i++)
+        {
+            if(t[i]==t[0])
+                c1[0]++;
+                
+            if(b[i]==t[0])
+                c1[1]++;
+            
+            if(t[i]==b[0])
+                c2[0]++;
+                
+            if(b[i]==b[0])
+                c2[1]++;
+            
+            if(t[i]!=t[0] && b[i]!=t[0])
+                f1=1;
+            
+            if(t[i]!=b[0] && b[i]!=b[0])
+                f2=1;
+        }
+            
+        if(f1==0)
+            ans=min(ans,n-max(c1[0],c1[1]));
+        
+        if(f2==0)
+            ans=min(ans,n-max(c2[0],c2[1]));
+        
+        if(ans==INT_MAX)
+            return -1;
+        
+        return ans;
+    }
+    
+    /*
     int check(vector<int>& t, vector<int>& b,int j,int n)
     {
         int tc=0,bc=0;
@@ -32,6 +75,7 @@ public:
         
         return ans;
     }
+    */
     
     /*
     int minDominoRotations(vector<int>& t, vector<int>& b) {
