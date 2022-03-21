f9be167d2afa94ddc6e875a6606b81417f549472
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-21 20:18:36 +0530

    Time: 48 ms (19.50%), Space: 13 MB (97.58%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,6 +1,47 @@
 class Solution {
 public:
+    bool canFinish(int n, vector<vector<int>>& a)
+    {
+        vector<int> g[n];
+        vector<int> indegree(n,0);
+        int i,l=a.size();
+        
+        for(i=0;i<l;i++)
+        {
+            g[a[i][1]].push_back(a[i][0]);
+            indegree[a[i][0]]++;
+        }
+            
+        int c=0;
+        queue<int> q;
+        
+        for(i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+                q.push(i);
+        }
+        
+        while(!q.empty())
+        {
+            int in=q.front();
+            q.pop();
+            c++;
+            
+            l=g[in].size();
+            
+            for(i=0;i<l;i++)
+            {
+                indegree[g[in][i]]--;
+                
+                if(indegree[g[in][i]]==0)
+                    q.push(g[in][i]);
+            }
+        }
+        
+        return c==n;
+    }
     
+    /*
     bool topo_sort(vector<int> *g,int in,vector<int> &vis)
     {
         int n=g[in].size();
@@ -35,4 +76,5 @@ public:
             
         return 1;
     }
+    */
 };
