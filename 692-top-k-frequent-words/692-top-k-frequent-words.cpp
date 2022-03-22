826f5a1a6d3154743cd57a1ff93764cb60db4fe5
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-22 20:54:03 +0530

    Time: 25 ms (25.48%), Space: 12.6 MB (50.93%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,6 +1,49 @@
 class Solution {
 public:
+    struct comp
+    {
+        bool operator()(const pair<int,string> &a,const pair<int,string> &b)
+        {
+            if(a.first==b.first)
+                return a.second<b.second;
+        
+            return a.first>b.first;
+        }
+    };
     
+    
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        vector<string> ans;
+        
+        unordered_map<string,int> f;
+        priority_queue<pair<int,string>,vector<pair<int,string>>,comp> q;
+        int i,n=words.size();
+        
+        for(i=0;i<n;i++)
+            f[words[i]]++;
+        
+        for(auto it:f)
+        {
+            q.push({it.second,it.first});
+            
+            if(q.size()>k)
+                q.pop();
+        }
+            
+        
+        while(k--)
+        {
+            string s=q.top().second;
+            q.pop();
+            
+            ans.push_back(s);
+        }
+        
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    
+    /*
     struct comp
     {
         bool operator()(const pair<int,string> &a,const pair<int,string> &b)
@@ -36,6 +79,7 @@ public:
         
         return ans;
     }
+    */
     
     /*
     vector<string> topKFrequent(vector<string>& words, int k) {
