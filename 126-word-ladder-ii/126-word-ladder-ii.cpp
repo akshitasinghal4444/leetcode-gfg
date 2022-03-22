7e3c259695c5a51c6f3dc5470dced2dd90a473ae
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-22 11:24:44 +0530

    Time: 6 ms (98.41%), Space: 9 MB (85.33%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.cpp
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -1,5 +1,104 @@
 class Solution {
 public:
+    vector<vector<string>> ans;
+    vector<string> t;
+    
+    void dfs(string b,string e,unordered_map<string,vector<string>> &g,int c)
+    {
+        if(c==0)
+        {
+            if(b==e)
+                ans.push_back(t);
+            
+            return;
+        }
+        
+        int i,n=g[b].size();
+        
+        for(i=0;i<n;i++)
+        {
+            t.push_back(g[b][i]);
+            dfs(g[b][i],e,g,c-1);
+            t.pop_back();
+        }
+    }
+    
+    vector<vector<string>> findLadders(string b, string e, vector<string>& w) {
+        
+        unordered_set<string> words(w.begin(),w.end());
+        unordered_map<string,int> level;
+        unordered_map<string,vector<string>> g;
+        
+        if(words.find(e)==words.end())
+            return ans;
+        
+        queue<string> q;
+        q.push(b);
+        level[b]=0;
+        
+        int c=0;
+        bool found=0;
+        
+        while(!q.empty())
+        {
+            int s=q.size();
+            c++;
+            
+            while(s--)
+            {
+                string s1,s2;
+                s1=s2=q.front();
+                q.pop();
+                
+                int n=s1.length();
+                int i;
+                char j;
+                
+                for(i=0;i<n;i++)
+                {
+                    char ch=s2[i];
+                    
+                    for(j='a';j<='z';j++)
+                    {
+                        if(j==ch)
+                            continue;
+                        
+                        s2[i]=j;
+                        
+                        if(words.find(s2)!=words.end())
+                        {
+                            if(s2==e)
+                                found=1;
+                            
+                            if(level.find(s2)==level.end())
+                            {
+                                level[s2]=c;
+                                g[s1].push_back(s2);
+                                q.push(s2);
+                            }
+                            else if(level[s2]==c)
+                                g[s1].push_back(s2);
+                        }
+                    }
+                    
+                    s2[i]=ch;
+                }
+            }
+            
+            if(found)
+                break;
+        }
+        
+        t.push_back(b);
+        dfs(b,e,g,c);
+        
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
     vector<string> t;
     
     void dfs(vector<vector<string>> &ans,unordered_map<string,vector<string>> &g,string b,int c,string e)
@@ -98,3 +197,4 @@ public:
         return ans;
     }
 };
+*/
