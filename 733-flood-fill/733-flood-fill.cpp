8d58b557b25a84889242e7c4774b5b51d92ee29e
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-22 15:52:03 +0530

    Time: 4 ms (97.69%), Space: 13.9 MB (91.47%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,5 +1,38 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& img,int m,int n,int i,int j,int nc,int col)
+    {      
+        img[i][j]=nc;
+        
+        if(i-1>=0 && img[i-1][j]==col)
+            dfs(img,m,n,i-1,j,nc,col);
+        
+        if(j-1>=0 && img[i][j-1]==col)
+            dfs(img,m,n,i,j-1,nc,col);
+        
+        if(i+1<m && img[i+1][j]==col)
+            dfs(img,m,n,i+1,j,nc,col);
+        
+        if(j+1<n && img[i][j+1]==col)
+            dfs(img,m,n,i,j+1,nc,col);
+        
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int nc) {
+        
+        if(img[sr][sc]==nc)
+            return img;
+        
+        int m=img.size(),n=img[0].size();   
+        
+        int col=img[sr][sc];
+        
+        dfs(img,m,n,sr,sc,nc,col);
+        
+        return img;
+    }
+    
+    /*
     vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int nc) {
         if(img[sr][sc]==nc)
             return img;
@@ -46,60 +79,8 @@ public:
         
         return img;
     }
+*/
 
-    /*
-    vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int nc) {
-        
-        int m=img.size(),n=img[0].size();
-        
-        vector<vector<bool>> vis(m,vector<bool>(n,0));
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        vis[sr][sc]=1;
-        
-        int col=img[sr][sc];
-        img[sr][sc]=nc;
-        
-        while(!q.empty())
-        {
-            sr=q.front().first;
-            sc=q.front().second;
-            q.pop();
-            
-            if(sr-1>=0 && img[sr-1][sc]==col && !vis[sr-1][sc])
-            {
-                img[sr-1][sc]=nc;
-                q.push({sr-1,sc});
-                vis[sr-1][sc]=1;
-            }
-                
-        
-            if(sc-1>=0 && img[sr][sc-1]==col && !vis[sr][sc-1])
-            {
-                img[sr][sc-1]=nc;
-                q.push({sr,sc-1});
-                vis[sr][sc-1]=1;
-            }    
-        
-            if(sr+1<m && img[sr+1][sc]==col && !vis[sr+1][sc])
-            {
-                img[sr+1][sc]=nc;
-                q.push({sr+1,sc});
-                vis[sr+1][sc]=1;
-            }    
-        
-            if(sc+1<n && img[sr][sc+1]==col && !vis[sr][sc+1])
-            {
-                img[sr][sc+1]=nc;
-                q.push({sr,sc+1});
-                vis[sr][sc+1]=1;
-            }    
-        
-        }
-        
-        return img;
-    }
-    */
     
     /*
     void dfs(vector<vector<int>>& img,vector<vector<bool>> &vis,int m,int n,int i,int j,int nc)
