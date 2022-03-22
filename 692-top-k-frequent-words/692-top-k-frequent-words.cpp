77564b404e05ea7f9504b3ed6be2521d70d3644c
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-22 20:48:05 +0530

    Time: 18 ms (52.72%), Space: 12.7 MB (35.72%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,5 +1,43 @@
 class Solution {
 public:
+    
+    struct comp
+    {
+        bool operator()(const pair<int,string> &a,const pair<int,string> &b)
+        {
+            if(a.first==b.first)
+                return a.second>b.second;
+        
+            return a.first<b.first;
+        }
+    };
+    
+    
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        vector<string> ans;
+        
+        unordered_map<string,int> f;
+        priority_queue<pair<int,string>,vector<pair<int,string>>,comp> q;
+        int i,n=words.size();
+        
+        for(i=0;i<n;i++)
+            f[words[i]]++;
+        
+        for(auto it:f)
+            q.push({it.second,it.first});
+        
+        while(k--)
+        {
+            string s=q.top().second;
+            q.pop();
+            
+            ans.push_back(s);
+        }
+        
+        return ans;
+    }
+    
+    /*
     vector<string> topKFrequent(vector<string>& words, int k) {
         vector<string> ans;
         
@@ -29,4 +67,5 @@ public:
         
         return ans;
     }
+    */
 };
