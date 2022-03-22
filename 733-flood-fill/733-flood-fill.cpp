3d7a374d1a416980e1e4c8d17536ba4868ce557e
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-22 15:41:48 +0530

    Time: 12 ms (62.07%), Space: 14.4 MB (12.59%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,5 +1,58 @@
 class Solution {
 public:
+    vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int nc) {
+        
+        int m=img.size(),n=img[0].size();
+        
+        vector<vector<bool>> vis(m,vector<bool>(n,0));
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        vis[sr][sc]=1;
+        
+        int col=img[sr][sc];
+        img[sr][sc]=nc;
+        
+        while(!q.empty())
+        {
+            sr=q.front().first;
+            sc=q.front().second;
+            q.pop();
+            
+            if(sr-1>=0 && img[sr-1][sc]==col && !vis[sr-1][sc])
+            {
+                img[sr-1][sc]=nc;
+                q.push({sr-1,sc});
+                vis[sr-1][sc]=1;
+            }
+                
+        
+            if(sc-1>=0 && img[sr][sc-1]==col && !vis[sr][sc-1])
+            {
+                img[sr][sc-1]=nc;
+                q.push({sr,sc-1});
+                vis[sr][sc-1]=1;
+            }    
+        
+            if(sr+1<m && img[sr+1][sc]==col && !vis[sr+1][sc])
+            {
+                img[sr+1][sc]=nc;
+                q.push({sr+1,sc});
+                vis[sr+1][sc]=1;
+            }    
+        
+            if(sc+1<n && img[sr][sc+1]==col && !vis[sr][sc+1])
+            {
+                img[sr][sc+1]=nc;
+                q.push({sr,sc+1});
+                vis[sr][sc+1]=1;
+            }    
+        
+        }
+        
+        return img;
+    }
+    
+    /*
     void dfs(vector<vector<int>>& img,vector<vector<bool>> &vis,int m,int n,int i,int j,int nc)
     {
         vis[i][j]=1;
@@ -28,4 +81,5 @@ public:
         
         return img;
     }
+    */
 };
