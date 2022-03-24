650b0dcebff50e4dc96bfd92f7f280b85e278e1d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-24 21:28:03 +0530

    Time: 20 ms (21.12%), Space: 9.6 MB (23.96%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,5 +1,64 @@
 class Solution {
 public:
+    unordered_set<int> cycle;
+    int start=-1;
+    
+    void dfs(vector<int> *g,vector<bool> &vis,int in,int parent)
+    {
+        if(vis[in])
+        {
+            cycle.insert(in);
+            start=in;
+            return;
+        }
+        
+        vis[in]=1;
+        int i,n=g[in].size();
+        
+        for(i=0;i<n;i++)
+        {
+            if(g[in][i]==parent)
+                continue;
+            
+            if(cycle.empty())
+                dfs(g,vis,g[in][i],in);
+            
+            if(in==start)
+            {
+                start=-1;
+                return;
+            }
+            
+            if(start!=-1)
+                cycle.insert(in);
+        }
+    }
+    
+    vector<int> findRedundantConnection(vector<vector<int>>& e)
+    {
+        int n=e.size();
+        int i;
+        vector<int> g[n+1];
+        vector<bool> vis(n+1,0);
+        
+        for(i=0;i<n;i++)
+        {
+            g[e[i][0]].push_back(e[i][1]);
+            g[e[i][1]].push_back(e[i][0]);
+        }
+        
+        dfs(g,vis,1,-1);
+        
+        for(i=n-1;i>=0;i--)
+        {
+            if(cycle.find(e[i][0])!=cycle.end() && cycle.find(e[i][1])!=cycle.end())
+                return e[i];
+        }
+        
+        return {};
+    }
+    
+    /*
     bool dfs(vector<int> *g,int in,vector<bool> &vis,int parent)
     {
         if(vis[in])
@@ -36,6 +95,7 @@ public:
         }
         return {};
     }
+    */
     
     /*
     int find(int i,vector<int> &parent)
