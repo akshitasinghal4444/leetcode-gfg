ced41d4f3983e3665fc94a455d74001eb80ab38b
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-24 21:34:14 +0530

    Time: 8 ms (76.88%), Space: 8.8 MB (72.79%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,5 +1,50 @@
 class Solution {
 public:
+    int find(int i,vector<int> &parent)
+    {
+        if(parent[i]<0)
+            return i;
+        
+        parent[i]=find(parent[i],parent);
+        return parent[i];
+    }
+    
+    bool union_by_weight(int u,int v,vector<int> &parent)
+    {
+        int pu=find(u,parent),pv=find(v,parent);
+        
+        if(pu==pv)
+            return 0;
+        
+        if(parent[pu]<parent[pv])
+        {
+            parent[pu]+=parent[pv];
+            parent[pv]=pu;
+        }
+        else
+        {
+            parent[pv]+=parent[pu];
+            parent[pu]=pv;
+        }
+        return 1;
+    }
+    
+    vector<int> findRedundantConnection(vector<vector<int>>& e) {
+        int n=e.size();
+        int i;
+        vector<int> parent(n+1,-1);
+        
+        for(i=0;i<n;i++)
+        {
+            int u=e[i][0],v=e[i][1];
+            
+            if(!union_by_weight(u,v,parent))
+                return e[i];
+        }
+        return {};
+    }
+    
+    /*
     unordered_set<int> cycle;
     int start=-1;
     
@@ -57,6 +102,7 @@ public:
         
         return {};
     }
+    */
     
     /*
     bool dfs(vector<int> *g,int in,vector<bool> &vis,int parent)
