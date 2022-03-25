e87df3a30762e4299e48e7f2fb451355d2b8851b
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-25 13:14:34 +0530

    Time: 8 ms (42.77%), Space: 7.9 MB (78.03%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     
+    int twoCitySchedCost(vector<vector<int>>& cost)
+    {
+        int n=cost.size(),i;
+        vector<int> diff;
+        int ans=0;
+        
+        for(i=0;i<n;i++)
+        {
+            ans+=cost[i][0];
+            diff.push_back(cost[i][1]-cost[i][0]);
+        }
+        
+        sort(diff.begin(),diff.end());
+        
+        for(i=0;i<n/2;i++)
+            ans+=diff[i];
+        
+        return ans;
+    }
+    
+   /* 
     int mincost(vector<vector<int>>& cost,int in,int a,int b,vector<vector<vector<int>>> &dp)
     {
         if(a==0 && b==0)
@@ -23,31 +44,8 @@ public:
         int n=cost.size();
 
         vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(n/2+1,vector<int> (n/2+1,INT_MAX)));
+        
         return mincost(cost,0,n/2,n/2,dp);
-    }
-    
-    /*
-    int mincost(vector<vector<int>>& cost,int in,int a,int b)
-    {
-        if(a==0 && b==0)
-            return 0;
-        
-        int x=INT_MAX;
-        
-        if(a>0)
-            x=min(x,cost[in][0]+mincost(cost,in+1,a-1,b));
-        
-        if(b>0)
-            x=min(x,cost[in][1]+mincost(cost,in+1,a,b-1));
-        
-        return x;
-        
-    }
-    
-    int twoCitySchedCost(vector<vector<int>>& cost) {
-        int n=cost.size();
-
-        return mincost(cost,0,n/2,n/2);
     }
     */
 };
