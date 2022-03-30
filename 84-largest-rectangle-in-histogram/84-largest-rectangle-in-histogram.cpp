fd121d608fd7a99c35b17eeaa5cb849fa799d6cc
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-30 19:22:37 +0530

    Time: 233 ms (40.08%), Space: 85.1 MB (13.94%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -3,10 +3,10 @@ public:
     int largestRectangleArea(vector<int>& a)
     {
         int n=a.size();
-        int lmin[n],rmin[n];
-        stack<int> s;
-        
         int i;
+        
+        stack<int> s;
+        vector<int> lmin(n),rmin(n);
         
         for(i=0;i<n;i++)
         {
@@ -33,7 +33,6 @@ public:
             }
             s.push(i);
         }
-        
         while(!s.empty())
         {
             lmin[s.top()]=-1;
@@ -41,12 +40,9 @@ public:
         }
         
         int ans=0;
-        
         for(i=0;i<n;i++)
-        {
             ans=max(ans,(rmin[i]-lmin[i]-1)*a[i]);
-        }
+        
         return ans;
     }
-
 };
