50561c16659a42f87819be6b36095765e7e159ba
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-30 22:38:25 +0530

    Time: 40 ms (71.96%), Space: 7.4 MB (71.66%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,5 +1,55 @@
 class Solution {
 public:
+    
+    void expand(string &s,int &l,int &r,int &len,int n)
+    {
+        while(l>=0 && r<n && s[l]==s[r])
+        {
+            l--;
+            r++;
+            len+=2;
+        }
+    }
+    
+    string longestPalindrome(string s)
+    {
+        int n=s.length();
+        int i;
+        int mx=1,b=0,e=0;
+        
+        for(i=0;i<n-1;i++)
+        {
+            int l=i;
+            int r=i+1;
+            int len=0;
+            expand(s,l,r,len,n);
+            if(len>mx)
+            {
+                mx=len;
+                b=l+1;
+                e=r-1;
+            }
+            
+            l=i-1;
+            r=i+1;
+            len=1;
+            expand(s,l,r,len,n);
+            if(len>mx)
+            {
+                mx=len;
+                b=l+1;
+                e=r-1;
+            }            
+        }
+        
+        string ans="";
+        for(i=b;i<=e;i++)
+            ans+=s[i];
+        
+        return ans;
+    }
+    
+    /*
     string longestPalindrome(string s)
     {
         int n=s.length();
@@ -33,6 +83,7 @@ public:
         
         return ans;
     }
+    */
     
     /*
     string longestPalindrome(string s)
