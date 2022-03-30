6dd1c20663e707925f6b2b5e547510f7ada10e3a
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-30 22:25:22 +0530

    Time: 423 ms (28.10%), Space: 8.5 MB (62.53%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -3,6 +3,41 @@ public:
     string longestPalindrome(string s)
     {
         int n=s.length();
+        bool palindrome[n][n];
+        int i,j,k;
+        int mx=0,b=-1,e=-1;
+        
+        for(k=0;k<n;k++)
+        {
+            for(i=0,j=k;j<n;i++,j++)
+            {
+                if(k==0)
+                    palindrome[i][j]=1;
+                else if(k==1)
+                    palindrome[i][j]=(s[i]==s[j]);
+                else
+                    palindrome[i][j]=((s[i]==s[j]) && palindrome[i+1][j-1]);
+                
+                if(palindrome[i][j] && k+1>mx)
+                {
+                    mx=k;
+                    b=i;
+                    e=j;
+                }
+            }
+        }
+        
+        string ans="";
+        for(i=b;i<=e;i++)
+            ans+=s[i];
+        
+        return ans;
+    }
+    
+    /*
+    string longestPalindrome(string s)
+    {
+        int n=s.length();
         int i;
         int mx=1,b=0,e=0;
         
@@ -47,117 +82,5 @@ public:
         
         return ans;
     }
-    
-    /*
-    string longestPalindrome(string s)
-    {
-        int n=s.length();
-        if(n==0 || n==1)
-            return s;
-        int maxl=0,maxr=0,maxlen=1;
-        int l=0,r=0,len=1;
-        int i;
-        for(i=1;i<n;i++)
-        {
-            l=i-1;
-            r=i+1;
-            len=1;
-            while(l>=0 && r<n)
-            {
-                if(s[l]==s[r])
-                {
-                    len+=2;
-                    l--;
-                    r++;
-                } 
-                else
-                    break;
-            }
-            if(maxlen<len)
-            {
-                maxlen=len;
-                maxl=l+1;
-                maxr=r-1;
-            }
-            
-            l=i-1;
-            r=i;
-            len=0;
-            while(l>=0 && r<n)
-            {
-                if(s[l]==s[r])
-                {
-                    len+=2;
-                    l--;
-                    r++;
-                } 
-                else
-                    break;
-            }
-            if(maxlen<len)
-            {
-                maxlen=len;
-                maxl=l+1;
-                maxr=r-1;
-            }
-        }
-        // cout<<maxl<<" "<<maxr;
-        string ans="";
-        while(maxl<=maxr)
-            ans+=s[maxl++];
-        return ans;
-    }
     */
-
-//     string longestPalindrome(string s) {
-//         int n=s.length();
-//         if(n==0)
-//             return "";
-//         if(n==1)
-//             return s;
-//         int i,l,r;
-//         int maxl=1,ml=0,mr=0,len;
-//         for(i=1;i<n;i++)
-//         {
-//             l=i-1;
-//             r=i+1;
-//             len=1;
-//             while(l>=0 && r<n && s[l]==s[r])
-//             {
-//                 len+=2;
-//                 l--;
-//                 r++;
-//             }
-//             if(len>maxl)
-//             {
-//                 maxl=len;
-//                 ml=l+1;
-//                 mr=r-1;
-//             }
-//             // cout<<len<<" "<<maxl<<endl;
-            
-//             l=i-1;
-//             r=i;
-//             len=0;
-//             while(l>=0 && r<n && s[l]==s[r])
-//             {
-//                 len+=2;
-//                 l--;
-//                 r++;
-//             }
-//             if(len>maxl)
-//             {
-//                 maxl=len;
-//                 ml=l+1;
-//                 mr=r-1;
-//             }
-//             // cout<<len<<" "<<maxl<<endl;
-//         }
-//         string ans="";
-//         while(ml<=mr)
-//         {
-//             ans+=s[ml++];
-//         }
-//         return ans;
-//     }
 };
