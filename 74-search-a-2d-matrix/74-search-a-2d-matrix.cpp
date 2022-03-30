79076e5f3b145d64dc1a43f6bc2d5c07c78c6425
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-30 11:16:28 +0530

    Time: 4 ms (77.73%), Space: 9.6 MB (5.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     bool searchMatrix(vector<vector<int>>& a, int t)
     {
         int m=a.size(),n=a[0].size();
@@ -46,23 +47,25 @@ public:
         
         return 0;
     }
+    */
     
-    
-    /*
-    int binSearch(vector<int> &a,int k)
+    bool binSearch(vector<int> &a,int k)
     {
-        int l=0,h=a.size()-1,mid;
-        while(l<=h)
+        int l=0,r=a.size()-1,mid;
+        
+        while(l<=r)
         {
-            mid=(l+h)/2;
+            mid=(l+r)/2;
+            
             if(a[mid]==k)
-                return mid;
+                return 1;
+            
             if(a[mid]<k)
                 l=mid+1;
             else
-                h=mid-1;
+                r=mid-1;
         }
-        return -1;
+        return 0;
     }
     
     bool searchMatrix(vector<vector<int>>& a, int t) {
@@ -72,19 +75,13 @@ public:
         while(i<m)
         {
             if(a[i][n-1]<t)
-            {
                 i++;
-                continue;
-            }
-            if(a[i][0]>t)
-                return 0;
-            int ind=binSearch(a[i],t);
-            if(ind==-1)
+            else if(a[i][0]>t)
                 return 0;
             else
-                return 1;
+                return binSearch(a[i],t);
         }
         return 0;
     }
-    */
+
 };
