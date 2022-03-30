6ff1253a25b7eb27c6b782e2de6a7b2aec968766
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-30 21:08:58 +0530

    Time: 52 ms (53.83%), Space: 27.2 MB (10.42%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+        vector<vector<string>> groupAnagrams(vector<string>& str)
+    {
+        unordered_map<string,vector<string>> m;
+        vector<vector<string>> ans;
+        
+        for(string s:str)
+        {
+            vector<int> f(26,0);
+            
+            for(char c:s)
+                f[c-'a']++;
+            
+            string t="";
+            for(int i=0;i<26;i++)
+                t+=f[i];
+            
+            m[t].push_back(s);
+        }
+        
+        for(auto it:m)
+            ans.push_back(it.second);
+        
+        return ans;
+    }
+    
+    /*
     vector<vector<string>> groupAnagrams(vector<string>& str)
     {
         unordered_map<string,vector<string>> m;
@@ -18,6 +44,7 @@ public:
         
         return ans;
     }
+    */
     
     /*
     vector<vector<string>> groupAnagrams(vector<string>& str)
