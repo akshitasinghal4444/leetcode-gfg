b095893295a2d4c09ab6a9cac81e6386fa6208cc
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-03-30 12:32:42 +0530

    Time: 46 ms (20.43%), Space: 13.7 MB (70.34%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,5 +1,44 @@
 class Solution {
 public:
+    vector<int> findOrder(int n, vector<vector<int>>& a)
+    {
+        vector<int> g[n],ans;
+        vector<int> indegree(n,0);
+        
+        int i,j=0;
+        
+        for(auto v:a)
+        {
+            g[v[1]].push_back(v[0]);
+            indegree[v[0]]++;
+        }
+            
+        for(i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+                ans.push_back(i);
+        }
+        
+        while(j<ans.size())
+        {
+            int in=ans[j];
+            j++;
+            
+            for(auto x:g[in])
+            {
+                indegree[x]--;
+                
+                if(indegree[x]==0)
+                    ans.push_back(x);
+            }
+        }
+        
+        if(ans.size()==n)
+            return ans;
+        return {};
+    }
+    
+    /*
     vector<int> ans;
     
     bool topo_sort(vector<int> *g,int in,vector<int> &vis)
@@ -14,10 +53,10 @@ public:
             if(vis[g[in][i]]==0 && !topo_sort(g,g[in][i],vis))
                 return 0;
         }
-        
+                
         ans.push_back(in);
         vis[in]=2;
-        
+
         return 1;
     }
     
@@ -40,6 +79,7 @@ public:
         
         return ans;
     }
+    */
     
     /*
     vector<int> ans;
