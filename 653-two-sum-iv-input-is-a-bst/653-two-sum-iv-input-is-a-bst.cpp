3da05a8571a512748e2185b613b7607af26c91fd
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-01 11:53:52 +0530

    Time: 80 ms (17.92%), Space: 36.9 MB (45.98%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,8 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
 class Solution {
 public:
+    vector<int> v;
+    
+    void inorder(TreeNode* root)
+    {
+        if(!root)
+            return;
+        
+        inorder(root->left);
+        v.push_back(root->val);
+        inorder(root->right);
+    }
+    
+    bool findTarget(TreeNode* root, int k)
+    {
+        if(!root)
+            return 0;
+        
+        inorder(root);
+        
+        int i=0,j=v.size()-1;
+        
+        while(i<j)
+        {
+            int s=v[i]+v[j];
+            
+            if(s==k)
+                return 1;
+            
+            if(s<k)
+                i++;
+            else
+                j--;
+        }
+        
+        return 0;
+    }
+    
+    /*
     unordered_set<int> s;
     
     bool findTarget(TreeNode* root, int k) {
@@ -24,4 +64,6 @@ public:
         
         return findTarget(root->left,k) || findTarget(root->right,k);
     }
+    */
+    
 };
