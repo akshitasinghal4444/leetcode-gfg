4e17fcac5884eb3ddbfc0e542b1caa0bb9c3cd4d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-03 18:32:37 +0530

    Time: 4 ms (73.50%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -24,42 +24,12 @@ public:
         j=in+1;
         for(i=in+2;i<n;i++)
         {
-            if(a[i]>a[in] && a[i]<a[j])
+            if(a[i]>a[in] && a[i]<=a[j])
                 j=i;
         }
         
         swap(a[in],a[j]);
-        sort(a.begin()+in+1,a.end());
+        reverse(a.begin()+in+1,a.end());
     }
     
-    /*
-    void nextPermutation(vector<int>& a) {
-        int n=a.size();
-        int i,j,ind=-1;
-        for(i=n-1;i>0;i--)
-        {
-            if(a[i]>a[i-1])
-            {
-                ind=i-1;
-                break;
-            }
-        }
-        if(ind==-1)
-        {
-            reverse(a.begin(),a.end());
-            return;
-        }
-        for(i=ind+2,j=ind+1;i<n;i++)
-        {
-            if(a[i]>a[ind] && a[i]<a[j])
-                j=i;
-        }
-        
-        int t=a[j];
-        a[j]=a[ind];
-        a[ind]=t;
-        
-        sort(a.begin()+ind+1,a.end());
-    }
-    */
 };
