13ab1cafbafddc5d7fe4b6c97d111129e7a18a6b
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-05 18:07:35 +0530

    Time: 26 ms (54.64%), Space: 7.8 MB (66.94%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,7 +1,51 @@
 class Solution {
 public:
+    
+    string minWindow(string s, string t)
+    {
+        int ns=s.length(),nt=t.length();
+        int c=0,beg=0;
+        int mn=INT_MAX,in=-1;        
+        int i;
+        unordered_map<char,int> m;
+        
+        for(i=0;i<nt;i++)
+            m[t[i]]++;
+        
+        for(i=0;i<ns;i++)
+        {
+            m[s[i]]--;
+            
+            if(m[s[i]]>=0)
+                c++;
+            
+            while(c==nt && beg<=i)
+            {
+                if(i-beg+1<mn)
+                {
+                    mn=i-beg+1;
+                    in=beg;
+                }
+                
+                m[s[beg]]++;
+                if(m[s[beg]]>0)
+                    c--;
+                
+                beg++;
+            }
+        }
+        
+        if(mn==INT_MAX)
+            return "";
+        
+        return s.substr(in,mn);
+    }
+    
+    /*
     string minWindow(string s, string t) {
-        int b=0,mn=INT_MAX,c=0,in=-1;
+        
+        int beg=0,mn=INT_MAX;
+        int c=0,in=-1;
         int ns=s.length(),nt=t.length();
         unordered_map<char,int> m1,m2;
         int i;
@@ -20,19 +64,19 @@ public:
                     c++;
             }
             
-            while(c==nt && b<=i)
+            while(c==nt && beg<=i)
             {
-                if(i-b+1<mn)
+                if(i-beg+1<mn)
                 {
-                    mn=i-b+1;
-                    in=b;
+                    mn=i-beg+1;
+                    in=beg;
                 }
-                m2[s[b]]--;
+                m2[s[beg]]--;
                 
-                if(m1.find(s[b])!=m1.end() && m2[s[b]]<m1[s[b]])
+                if(m1.find(s[beg])!=m1.end() && m2[s[beg]]<m1[s[beg]])
                     c--;
                 
-                b++;
+                beg++;
             }
         }
         
@@ -41,4 +85,5 @@ public:
         
         return s.substr(in,mn);
     }
+    */
 };
