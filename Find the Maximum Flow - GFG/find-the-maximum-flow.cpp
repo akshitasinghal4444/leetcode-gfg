e951eba02ff6ff62c38492ea693af249e2f5580e
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-05 13:29:56 +0530

    Added solution - LeetHub

--- Find the Maximum Flow - GFG/find-the-maximum-flow.cpp
+++ Find the Maximum Flow - GFG/find-the-maximum-flow.cpp
@@ -6,6 +6,75 @@ using namespace std;
 class Solution
 { 
     public:
+    
+    int bfs(int source,int sink,vector<vector<int>> &g,int n,vector<int> &parent)
+    {
+        queue<pair<int,int>> q;
+        vector<bool> vis(n,0);
+        q.push({source, INT_MAX});
+        vis[source]=1;
+        
+        while(!q.empty())
+        {
+            source=q.front().first;
+            int cap=q.front().second;
+            q.pop();
+            
+            for(int i=0;i<n;i++)
+            {
+                if(g[source][i] && !vis[i])
+                {
+                    parent[i]=source;
+                    
+                    if(i==sink)
+                        return min(cap,g[source][i]);
+                        
+                    q.push({i,min(cap,g[source][i])});
+                    vis[i]=1;
+                }
+            }
+        }
+        return 0;
+    }
+    
+    int ford_fulkerson(int source,int sink,vector<vector<int>> &g,int n)
+    {
+        int flow=0;
+        vector<int> parent(n,-1);
+        
+        int min_cap;
+        while(min_cap=bfs(source,sink,g,n,parent))
+        {
+            flow+=min_cap;
+            
+            int u,v=sink;
+            while(v!=source)
+            {
+                u=parent[v];
+                g[u][v]-=min_cap;
+                g[v][u]+=min_cap;
+                v=u;
+            }
+        }
+        
+        return flow;
+    }
+    
+    int solve(int n,int m,vector<vector<int>> e)
+    {
+        vector<vector<int>> g(n+1,vector<int>(n+1,0));
+        int i;
+        
+        for(i=0;i<m;i++)
+        {
+            g[e[i][0]][e[i][1]]+=e[i][2];
+            g[e[i][1]][e[i][0]]+=e[i][2];
+        }
+        
+        return ford_fulkerson(1,n,g,n+1);
+    }
+    
+    /*
     int BFS(vector<vector<int>> &graph,vector<int> &parent,int source,int sink,int N)
    {
        int min_cap=INT_MAX;
@@ -101,7 +170,7 @@ class Solution
        return fordFulkerson(graph,0,N-1,N);
        
    }
-
+    */
 
 
 };
