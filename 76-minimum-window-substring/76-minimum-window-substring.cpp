c9e14e7a2447542a3679e5d2a1aa12b8eb8a3ca3
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-05 18:11:14 +0530

    Time: 4 ms (98.23%), Space: 7.6 MB (81.35%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,6 +1,46 @@
 class Solution {
 public:
+    string minWindow(string s, string t)
+    {
+        int ns=s.length(),nt=t.length();
+        int c=0,beg=0;
+        int mn=INT_MAX,in=-1;        
+        int i;
+        int f[256]={0};
+        
+        for(i=0;i<nt;i++)
+            f[t[i]]++;
+        
+        for(i=0;i<ns;i++)
+        {
+            f[s[i]]--;
+            
+            if(f[s[i]]>=0)
+                c++;
+            
+            while(c==nt && beg<=i)
+            {
+                if(i-beg+1<mn)
+                {
+                    mn=i-beg+1;
+                    in=beg;
+                }
+                
+                f[s[beg]]++;
+                if(f[s[beg]]>0)
+                    c--;
+                
+                beg++;
+            }
+        }
+        
+        if(mn==INT_MAX)
+            return "";
+        
+        return s.substr(in,mn);
+    }
     
+    /*
     string minWindow(string s, string t)
     {
         int ns=s.length(),nt=t.length();
@@ -40,6 +80,7 @@ public:
         
         return s.substr(in,mn);
     }
+    */
     
     /*
     string minWindow(string s, string t) {
