9e2ef71265f08b536577309dbddb6aea19b99c82
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-06 16:29:05 +0530

    Time: 24 ms (77.21%), Space: 10.4 MB (74.83%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,6 +1,76 @@
 class Solution {
 public:
+    int threeSumMulti(vector<int>& a, int t)
+    {
+        int n=a.size();
+        int i,j,k;
+        int m=1e9+7;
+        long c=0;
+        
+        sort(a.begin(),a.end());
+        
+        for(i=0;i<n-2;i++)
+        {
+            if(a[i]>t)
+                break;
+            
+            if(i>0 && a[i]==a[i-1])
+                continue;
+            
+            j=i+1;
+            k=n-1;
+            
+            while(j<k)
+            {
+                int s=a[i]+a[j]+a[k];
+                
+                if(s<t)
+                    j++;
+                else if(s>t)
+                    k--;
+                else
+                {
+                    // cout<<a[i]<<" "<<a[j]<<" "<<a[k]<<endl;
+                    long c1=1,c2=1,c3=1;
+                    
+                    int i1=i;
+                    while(i1<n-1 && a[i1]==a[i1+1])
+                    {
+                        i1++;
+                        c1++;
+                    }
+                    
+                    while(j<n-1 && a[j]==a[j+1])
+                    {
+                        j++;
+                        c2++;
+                    }
+                    
+                    while(k>j && a[k]==a[k-1])
+                    {
+                        k--;
+                        c3++;
+                    }
+                    
+                    if(a[i]==a[j] && a[j]==a[k])
+                        c+=c1*(c1-1)*(c1-2)/6;
+                    else if(a[i]==a[j])
+                        c+=c1*(c1-1)/2*c3;
+                    else if(a[j]==a[k])
+                        c+=c1*c2*(c2-1)/2;
+                    else
+                        c+=c1*c2*c3;
+                    
+                    // cout<<c1<<" "<<c2<<" "<<c3<<" "<<c<<endl;
+                    j++;
+                    k--;
+                }
+            }
+        }
+        return c%m;
+    }
     
+    /*
     int threeSumMulti(vector<int>& a, int t) 
     {
         int n=a.size();
@@ -18,6 +88,7 @@ public:
         
         return c%m;
     }
+    */
     
     /*
     int threeSumMulti(vector<int>& a, int t) {
