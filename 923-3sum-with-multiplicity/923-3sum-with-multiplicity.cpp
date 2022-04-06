a72c15e4325a45b1cf645b06ba6d061e1c9e785d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-06 16:01:02 +0530

    Time: 1792 ms (5.11%), Space: 10.9 MB (20.41%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    
+    int threeSumMulti(vector<int>& a, int t) 
+    {
+        int n=a.size();
+        unordered_map<int,long> mp;
+        int i,j,m=1e9+7;
+        long c=0;
+        
+        for(i=0;i<n;i++)
+        {
+            c+=mp[t-a[i]];
+            
+            for(j=0;j<i;j++)
+                mp[a[i]+a[j]]++;
+        }
+        
+        return c%m;
+    }
+    
+    /*
     int threeSumMulti(vector<int>& a, int t) {
         int n=a.size();
         unordered_map<int,long> mp;
@@ -34,6 +54,8 @@ public:
                 }
             }
         }
+        
         return c;
     }
+    */
 };
