e9e46e7e643114786d3fe24d084dd46c829d84b9
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-09 12:38:35 +0530

    Time: 19 ms (67.06%), Space: 13.7 MB (71.77%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
+    vector<int> topKFrequent(vector<int>& a, int k) {
+        
+        priority_queue<pair<int,int>> q;
+        unordered_map<int,int> m;
+        vector<int> ans;
+        
+        for(int n:a)
+            m[n]++;
+        
+        for(auto it:m)
+            q.push({it.second,it.first});
+        
+        while(k--)
+        {
+            ans.push_back(q.top().second);
+            q.pop();
+        }
+        
+        return ans;
+    }
     
+    /*
     int partition(vector<int>& a, int l,int r,unordered_map<int,int> &m)
     {
         int x=rand()%(r-l+1)+l;
@@ -20,11 +41,6 @@ public:
         
         j++;
         swap(a[j],a[r]);
-        
-        // cout<<l<<" "<<r<<" "<<j<<":";
-        // for(i=l;i<=r;i++)
-        //     cout<<a[i]<<" ";
-        // cout<<endl;
         
         return j;
     }
@@ -55,14 +71,13 @@ public:
             m[n]++;
             
             if(m[n]==1)
-                ans.push_back(n);
-            
-                
+                ans.push_back(n);   
         }
         
         quickselect(ans,0,ans.size()-1,k-1,m);
         return vector<int> (ans.begin(),ans.begin()+k);
     }
+    */
     
     /*
     vector<int> topKFrequent(vector<int>& a, int k) {
