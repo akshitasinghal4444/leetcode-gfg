34595b099c892fbd5ac3b2e4b47ffc334fcebd61
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-12 13:36:55 +0530

    Time: 16 ms (99.14%), Space: 14 MB (40.40%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        const int m = grid.size();
-        const int n = grid[0].size();
-        vector<vector<int>> ans(m, vector<int>(n, 0));
-        for (int i = 0; i < m * n; ++i) {
-            int ori_y = i / n;
-            int ori_x = i % n;
-            int new_y = (ori_y + (ori_x + k) / n) % m;
-            int new_x = (ori_x + k) % n;
-            
-            ans[new_y][new_x] = grid[ori_y][ori_x];
+    vector<vector<int>> shiftGrid(vector<vector<int>>& g, int k) {
+        int i,j;
+        int m=g.size(),n=g[0].size();
+        vector<vector<int>> ans(m,vector<int>(n));
+        k=k%(m*n);
+        // cout<<k<<endl;
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                int ni,nj;
+                nj=(j+(k%n))%n;
+                ni=i+(k/n);
+                
+                if(nj<j)
+                    ni++;
+                
+                ni=ni%m;
+                ans[ni][nj]=g[i][j];
+                
+                // cout<<i<<" "<<j<<" "<<ni<<" "<<nj<<endl;
+            }
         }
         return ans;
     }
