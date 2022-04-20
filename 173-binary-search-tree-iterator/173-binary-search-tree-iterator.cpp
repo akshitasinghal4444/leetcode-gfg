51168bbb837c226e91d1f1df054aa540be720e20
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-20 11:29:09 +0530

    Time: 16 ms (99.82%), Space: 24 MB (98.10%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -9,8 +9,42 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator {
+public:
+    TreeNode *head,*t; 
+    
+    void convert(TreeNode* root)
+    {
+        if(!root)
+            return;
+        
+        convert(root->left);
+        
+        t->right=root;
+        t=t->right;
+        
+        convert(root->right);
+    }
+    
+    BSTIterator(TreeNode* root) {
+        
+        head=new TreeNode(0);
+        t=head;
+        convert(root);
+    }
+    
+    int next() {
+        head=head->right;
+        return head->val;
+    }
+    
+    bool hasNext() {
+        return (head->right!=NULL);
+    }
+};
 
 
+/*
 class BSTIterator {
 public:
     stack<TreeNode*> st;
@@ -49,6 +83,7 @@ public:
         return !st.empty();
     }
 };
+*/
 
 /**
  * Your BSTIterator object will be instantiated and called as such:
