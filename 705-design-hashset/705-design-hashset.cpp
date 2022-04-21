7292b6730da181722204e348549affc0b322cabe
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-21 10:57:31 +0530

    Time: 108 ms (76.17%), Space: 48.1 MB (37.82%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,5 +1,42 @@
 class MyHashSet {
 public:
+    int m=7919;
+    list<int> v[7919];
+    
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        
+        int x=key%m;
+        auto it=find(v[x].begin(),v[x].end(),key);
+        
+        if(it==v[x].end())
+        v[key%m].push_back(key);
+    }
+    
+    void remove(int key) {
+        
+        int x=key%m;
+        auto it=find(v[x].begin(),v[x].end(),key);
+        
+        if(it!=v[x].end())
+            v[x].erase(it);
+    }
+    
+    bool contains(int key) {
+        
+        int x=key%m;
+        auto it=find(v[x].begin(),v[x].end(),key);
+        
+        return (it!=v[x].end());
+    }
+};
+
+/*
+class MyHashSet {
+public:
     bool v[1000001]={0};
     
     MyHashSet() {
@@ -18,6 +55,7 @@ public:
         return v[key];
     }
 };
+*/
 
 /**
  * Your MyHashSet object will be instantiated and called as such:
