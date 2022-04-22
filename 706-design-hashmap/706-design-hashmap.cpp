86b5b53b144df8f9051e0f1c67c54b4d1fb67f6b
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-22 20:29:00 +0530

    Time: 147 ms (75.36%), Space: 60 MB (54.56%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,10 +1,61 @@
 class MyHashMap {
 public:
+    int m=7919;
+    list<pair<int,int>> l[7919];
+    
+    list<pair<int,int>>::iterator search(list<pair<int,int>> &li,int k)
+    {
+        for(auto it=li.begin();it!=li.end();it++)
+        {
+            if(it->first==k)
+                return it;
+        }
+        
+        return li.end();
+    }
+    
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        int i=key%m;
+        auto it=search(l[i],key);
+        
+        if(it==l[i].end())
+            l[i].push_back({key,value});
+        else
+            it->second=value;
+    }
+    
+    int get(int key) {
+        int i=key%m;
+        auto it=search(l[i],key);
+        
+        if(it==l[i].end())
+            return -1;
+        
+        return it->second;
+        
+    }
+    
+    void remove(int key) {
+        int i=key%m;
+        auto it=search(l[i],key);
+             
+        if(it!=l[i].end())
+            l[i].erase(it);
+    }
+};
+
+/*
+class MyHashMap {
+public:
     
     int m[1000001];
     
     MyHashMap() {
-        for(int i=0;i<1000000;i++)
+        for(int i=0;i<=1000000;i++)
             m[i]=-1;
     }
     
@@ -20,6 +71,7 @@ public:
         m[key]=-1;
     }
 };
+*/
 
 /**
  * Your MyHashMap object will be instantiated and called as such:
