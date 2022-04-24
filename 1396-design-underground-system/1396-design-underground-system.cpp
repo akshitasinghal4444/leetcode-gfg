dfacc052398f8516a53c3c147653fbca029238ca
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-24 11:50:48 +0530

    Time: 178 ms (69.98%), Space: 58.7 MB (73.70%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -0,0 +1,67 @@
+class UndergroundSystem {
+public:
+    unordered_map<int, pair<string, int>> checkins;
+    unordered_map<string, pair<int, int>> routes;
+    void checkIn(int id, string stationName, int t) {
+        checkins[id] = {stationName, t};
+    }
+    void checkOut(int id, string stationName, int t) {
+        auto [stn, start] = checkins[id];
+        checkins.erase(id);
+        string route = stn + "," + stationName;
+        routes[route].first++, routes[route].second += t - start;
+    }
+    double getAverageTime(string startStation, string endStation) {
+        auto& [count, sum] = routes[startStation + "," + endStation];
+        return (double)sum / count;
+    }
+};
+
+/*
+class UndergroundSystem {
+public:
+    
+    unordered_map<string,unordered_set<int>> in_station,out_station;
+    unordered_map<string,double> in_id,out_id;
+    
+    
+    UndergroundSystem() {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        in_station[stationName].insert(id);
+        in_id[to_string(id)+stationName]=t;
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        out_station[stationName].insert(id);
+        out_id[to_string(id)+stationName]=t;
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+             
+        double sum=0,n=0;
+        for(int id:in_station[startStation])
+        {
+            if(out_station[endStation].find(id)!=out_station[endStation].end())
+            {
+                double st,et;
+                st=in_id[to_string(id)+startStation];
+                et=out_id[to_string(id)+endStation];
+                sum+=et-st;
+                n++;
+            }
+        }
+        return sum/n;
+    }
+};
+*/
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
