10fd9f449faa8b9ca8c9c348b4ad464939adb213
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-04-29 13:00:01 +0530

    Time: 32 ms (53.11%), Space: 13.4 MB (81.82%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     
+    bool dfs(vector<vector<int>>& g,int in,int n,vector<int> &color,int c)
+    {
+        color[in]=c;
+
+        for(int i:g[in])
+        {
+            if((color[i]==color[in]) || (color[i]==0 && !dfs(g,i,n,color,-c)))
+                return 0;      
+        }
+        
+        return 1;
+    }
+    
+    bool isBipartite(vector<vector<int>>& g)
+    {
+        int i,n=g.size();
+        vector<int> color(n,0);
+        
+        for(i=0;i<n;i++)
+        {
+            if(color[i]==0 && !dfs(g,i,n,color,1))
+                return 0;
+        }
+        
+        return 1;
+    }
+    
+    /*
     bool bfs(vector<vector<int>>& g,int in,int n,vector<int> &color)
     {
         color[in]=1;
@@ -36,85 +64,6 @@ public:
         for(i=0;i<n;i++)
         {
             if(color[i]==0 && !bfs(g,i,n,color))
-                return 0;
-        }
-        
-        return 1;
-    }
-    
-    /*
-    bool dfs(vector<vector<int>>& g,vector<int> &color,int in,int col)
-    {
-        int nc=(col==0)?1:0;
-        color[in]=col;
-        
-        int i,n=g[in].size();
-        
-        for(i=0;i<n;i++)
-        {
-            if(color[g[in][i]]==col)
-              return 0;
-                
-            if(color[g[in][i]]==-1 && !dfs(g,color,g[in][i],nc))
-                return 0;
-        }
-        
-        return 1;
-    }
-    
-    bool isBipartite(vector<vector<int>>& g) {
-        int n=g.size();
-        int i;
-        vector<int> color(n,-1);
-        
-        for(i=0;i<n;i++)
-        {
-            if(color[i]==-1 && !dfs(g,color,i,0))
-                return 0;
-        }
-        
-        return 1;
-    }
-    */
-    
-    /*
-    bool bfs(vector<vector<int>>& g,vector<int> &color,int in)
-    {
-        queue<int> q;
-        q.push(in);
-        color[in]=0;
-        
-        while(!q.empty())
-        {
-            in=q.front();
-            q.pop();
-            
-            int i,n=g[in].size();
-            int nc=(color[in]==0)?1:0;
-            
-            for(i=0;i<n;i++)
-            {
-                if(color[g[in][i]]==color[in])
-                  return 0;
-                
-                if(color[g[in][i]]==-1)
-                {
-                    color[g[in][i]]=nc;
-                    q.push(g[in][i]);
-                }
-            }
-        }
-        return 1;
-    }
-    
-    bool isBipartite(vector<vector<int>>& g) {
-        int n=g.size();
-        int i;
-        vector<int> color(n,-1);
-        
-        for(i=0;i<n;i++)
-        {
-            if(color[i]==-1 && !bfs(g,color,i))
                 return 0;
         }
         
