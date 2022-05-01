646ef190dd5a69f604b69037aff33e3be8170bc9
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-05-01 14:02:01 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (97.67%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,5 +1,40 @@
 class Solution {
 public:
+    bool backspaceCompare(string s, string t)
+    {
+        int i,j,ns=s.length(),nt=t.length();
+        
+        i=0,j=0;
+        for(i=0;i<ns;i++)
+        {
+            if(s[i]=='#')
+                j=max(0,j-1);
+            else
+                s[j++]=s[i];
+        }
+        ns=j;
+        
+        j=0;
+        for(i=0;i<nt;i++)
+        {
+            if(t[i]=='#')
+                j=max(0,j-1);
+            else
+                t[j++]=t[i];
+        }
+        nt=j;
+        
+        if(ns!=nt)
+            return 0;
+        
+        for(i=0;i<ns;i++)
+            if(s[i]!=t[i])
+                return 0;
+        
+        return 1;
+    }
+    
+    /*
     bool backspaceCompare(string s, string t) {
         string s1,t1;
         
@@ -27,4 +62,5 @@ public:
         
         return s1==t1;
     }
+    */
 };
