cda6fc63e543d21ebfeb601a8b89664ba6ccdbe4
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-05-04 13:10:40 +0530

    Time: 4 ms (58.35%), Space: 7.6 MB (39.45%) - LeetHub

--- 284-peeking-iterator/284-peeking-iterator.cpp
+++ 284-peeking-iterator/284-peeking-iterator.cpp
@@ -18,30 +18,33 @@
  */
 
 class PeekingIterator : public Iterator {
-    int next_val; // keep a variable for storing the "next value" for use in "peek" function
-    bool iter_hasnext; // for storing the "has next value"
 public:
+    
+    vector<int> a;
+    int i=-1;
+    
 	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
-        iter_hasnext=Iterator::hasNext(); // store current has_next value
-	    if (iter_hasnext)
-            next_val = Iterator::next(); // store the "next_val" for "future" use in "peek" function
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+        
+        for(int n:nums)
+            a.push_back(n);
 	}
 	
+    // Returns the next element in the iteration without advancing the iterator.
 	int peek() {
-        return next_val; // return current next_val
+        return a[i+1];
 	}
 	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
 	int next() {
-	    int curr_next = next_val; // store the current next for returning
-        
-        iter_hasnext=Iterator::hasNext(); // change the iter_hasnext variable
-	    if (iter_hasnext) 
-            next_val = Iterator::next(); // change the next_val and move the iterator to next position
-        
-	    return curr_next; // return the previously stored current next
+	    i++;
+        return a[i];
 	}
 	
 	bool hasNext() const {
-	    return iter_hasnext; // return current iter_hashnext value
+	    return (i+1<a.size());
 	}
 };
