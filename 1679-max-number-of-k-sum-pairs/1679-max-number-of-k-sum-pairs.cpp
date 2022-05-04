c21686539dc2fbe9d98900e54f3cab91b1a1b48e
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-05-04 11:28:47 +0530

    Time: 172 ms (65.51%), Space: 58.1 MB (80.67%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,5 +1,32 @@
 class Solution {
 public:
+    
+    int maxOperations(vector<int>& a, int k)
+    {
+        sort(a.begin(),a.end());
+        int i,j,c=0;
+        
+        i=0,j=a.size()-1;
+        
+        while(i<j)
+        {
+            int s=a[i]+a[j];
+            if(s==k)
+            {
+                c++;
+                i++;
+                j--;
+            }
+            else if(s<k)
+                i++;
+            else
+                j--;
+        }
+        
+        return c;
+    }
+    
+    /*
     int maxOperations(vector<int>& a, int k) {
         
         unordered_map<int,int> m;
@@ -17,6 +44,6 @@ public:
         }
         
         return c;
-        
     }
+    */
 };
