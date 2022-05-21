c35c05ce73121da3149fa21134ac022be95b6614
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-05-21 12:16:16 +0530

    Time: 259 ms (10.71%), Space: 14.4 MB (68.59%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,49 +1,18 @@
 class Solution {
 public:
-    
-    int coinChange(vector<int>& a, int amt)
-    {
-        int n=a.size();
-        int i,j;
-        
+    int coinChange(vector<int>& a, int amt) {
+        int n=a.size(),i,j;
         vector<int> dp(amt+1,INT_MAX);
         dp[0]=0;
         
-        for(i=0;i<n;i++)
+        for(i=1;i<=amt;i++)
         {
-            for(j=1;j<=amt;j++)
+            for(j=0;j<n;j++)
             {
-                if(a[i]<=j && dp[j-a[i]]!=INT_MAX)
-                {
-                    dp[j]=min(dp[j],dp[j-a[i]]+1);
-                }
+                if(i>=a[j] && dp[i-a[j]]!=INT_MAX)
+                    dp[i]=min(dp[i],dp[i-a[j]]+1);
             }
         }
-        
-        if(dp[amt]==INT_MAX)
-            return -1;
-        
-        return dp[amt];
+        return dp[amt]!=INT_MAX?dp[amt]:-1;
     }
-    
-  /*  
-    int count(vector<int>& a,int n,int k)
-    {
-        if(k==0)
-            return 0;
-        if(n==0)
-            return INT_MAX;
-        int x=k/a[n-1];
-        return min(count(a,n-1,k),count(a,n-1,k-x*a[n-1])!=INT_MAX?x+count(a,n-1,k-x*a[n-1]):INT_MAX);
-    }
-        
-    int coinChange(vector<int>& a, int t) {
-        int n=a.size();
-        int c=count(a,n,t);
-        if(c==INT_MAX)
-            return -1;
-        return c;
-    }
-    */
-
 };
