fa8f4de59e6304c732eafb6a7baba312da7eab85
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-05-28 13:04:16 +0530

    Added solution - LeetHub

--- Party in Town - GFG/party-in-town.cpp
+++ Party in Town - GFG/party-in-town.cpp
@@ -10,105 +10,40 @@ using namespace std;
 class Solution{
 public:
 
-    int maxdis=INT_MAX;
-
-    int bfs(int u,vector<vector<int>> &a,int n)
+    int maxdis;
+    int ans=INT_MAX;
+    
+    void dfs(int in,vector<vector<int>> &a,int n,int d,vector<bool> &vis)
     {
-        queue<int> q;
-        q.push(u);
+        vis[in]=1;
+        maxdis=max(maxdis,d);
         
-        vector<bool>  vis(n+1,0);
-        vis[u]=1;
-        int dis=0;
-        
-        while(!q.empty())
-        {
-            dis++;
-            
-            if(dis>maxdis)
-            return dis;
-            
-            int s=q.size();
-            
-            while(s--)
-            {
-                u=q.front();
-                q.pop();
-                
-                for(int v:a[u])
-                {
-                    if(!vis[v])
-                    {
-                        q.push(v);
-                        vis[v]=1;
-                    }
-                }
-            }
-            
-        }
-        
-        return dis-1;
-    }
-
-    int partyHouse(int n, vector<vector<int>> &a){
-        // code here
-        int i,j;
-        
-        for(i=1;i<=n;i++)
-        {
-            int mx=bfs(i,a,n);
-            maxdis=min(maxdis,mx);
-        }
-        
-        return maxdis;
-    }
-
-    /*
-    int maxdis=INT_MAX;
-
-    void dfs(int u,vector<int> &dis,vector<vector<int>> &a)
-    {
-        if(dis[u]==maxdis)
+        if(d==ans)
         return;
         
-        for(int v:a[u])
+        for(auto i:a[in])
         {
-            if(dis[v]==INT_MAX)
-            {
-                dis[v]=dis[u]+1;
-                dfs(v,dis,a);
-            }
+            if(!vis[i])
+            dfs(i,a,n,d+1,vis);
         }
     }
-
+    
     int partyHouse(int n, vector<vector<int>> &a){
         // code here
         int i,j;
         
         for(i=1;i<=n;i++)
         {
-            vector<int> dis(n+1,INT_MAX);
-            dis[i]=0;
-            dfs(i,dis,a);
+            vector<bool> vis(n+1,0);
+ 
+            maxdis=0;
+            dfs(i,a,n,0,vis);
             
-            int mx=INT_MIN;
-            for(j=1;j<=n;j++)
-            {
-                if(dis[j]>maxdis)
-                {
-                    mx=INT_MAX;
-                    break;
-                }
-                
-                mx=max(mx,dis[j]);
-            }
-            
-            maxdis=min(maxdis,mx);
+            ans=min(ans,maxdis);
         }
         
-        return maxdis;
+        return ans;
     }
-    */
 };
 
 // { Driver Code Starts.
