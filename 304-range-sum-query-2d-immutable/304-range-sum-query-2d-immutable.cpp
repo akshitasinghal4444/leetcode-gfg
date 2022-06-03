93c8e8821baefab78d73201b91f6098860da3f6d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-06-03 09:37:48 +0530

    Time: 617 ms (46.92%), Space: 153.5 MB (5.15%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -3,6 +3,37 @@ public:
     vector<vector<int>> a;
     
     NumMatrix(vector<vector<int>>& matrix) {
+        
+        a=matrix;
+        int m=a.size(),n=a[0].size();
+        int i,j;
+        a.push_back({0});
+        
+        for(i=0;i<m;i++)
+            a[i].push_back(0);
+        
+        for(j=0;j<n;j++)
+            a[i].push_back(0);
+        
+        for(i=m-1;i>=0;i--)
+            for(j=n-1;j>=0;j--)
+                    a[i][j]+=a[i+1][j]+a[i][j+1]-a[i+1][j+1];
+    }
+    
+    int sumRegion(int r1, int c1, int r2, int c2) {
+        int sum=0;
+        sum+=a[r1][c1]-a[r1][c2+1]-a[r2+1][c1]+a[r2+1][c2+1];
+        return sum;
+    }
+};
+
+
+/*
+class NumMatrix {
+public:
+    vector<vector<int>> a;
+    
+    NumMatrix(vector<vector<int>>& matrix) {
         a=matrix;
         int m=a.size(),n=a[0].size();
         int i,j;
@@ -25,6 +56,7 @@ public:
         return sum;
     }
 };
+*/
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
