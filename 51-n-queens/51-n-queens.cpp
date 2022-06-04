c78036b34f6b424dfcc21ed5875f6b7b7eace92e
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-06-04 12:44:40 +0530

    Time: 12 ms (46.95%), Space: 7.4 MB (49.05%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -3,76 +3,67 @@ public:
     vector<vector<string>> ans;
     vector<string> t;
     
-    
-    bool isSafe(vector<vector<int>> &a,int n,int r,int c)
+    bool is_safe(vector<vector<int>> &a,int r,int c,int n)
     {
         int i,j;
         
-        //same col
-        for(i=r-1,j=c;i>=0;i--)
+        //col
+        for(i=0;i<n;i++)
         {
-            if(a[i][j]==1)
+            if(a[i][c])
                 return 0;
         }
         
         //left diagnol
         for(i=r-1,j=c-1;i>=0 && j>=0;i--,j--)
         {
-            if(a[i][j]==1)
+            if(a[i][j])
                 return 0;
         }
         
         //right diagnol
         for(i=r-1,j=c+1;i>=0 && j<n;i--,j++)
         {
-            if(a[i][j]==1)
+            if(a[i][j])
                 return 0;
         }
         
         return 1;
     }
+                 
     
     void solve(vector<vector<int>> &a,int n,int r)
     {
         if(r==n)
         {
             ans.push_back(t);
-            return ;
+            return;
         }
-        
-        int i;
+                  
         string s(n,'.');
+        int i;
         
-        // for(int i=0;i<n;i++)
-        //     s+='.';
-
         for(i=0;i<n;i++)
         {
-            if(isSafe(a,n,r,i))
+            if(is_safe(a,r,i,n))
             {
                 s[i]='Q';
-                t.push_back(s);
                 a[r][i]=1;
+                t.push_back(s);
                 
-                solve(a,n,r+1); 
+                solve(a,n,r+1);
                 
+                s[i]='.';
                 a[r][i]=0;
                 t.pop_back();
-                s[i]='.';
             }
-
         }
-        
     }
     
     vector<vector<string>> solveNQueens(int n) {
+        
         vector<vector<int>> a(n,vector<int>(n,0));
-        ans={};
-        t={};
-        
-         
         solve(a,n,0);
-        
         return ans;
     }
 };
