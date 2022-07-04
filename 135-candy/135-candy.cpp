2a3b2830b0003a3ace3f14d3c5af52d51cd1104d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-07-04 21:53:54 +0530

    Time: 35 ms (46.56%), Space: 17.7 MB (65.79%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,82 +1,14 @@
 class Solution {
 public:
-    int candy(vector<int>& a) {
-        int n=a.size();
-        int i;
-        vector<int> l(n,1);
-        int ans=0;
-        
-        for(i=1;i<n;i++)
-        {
-            if(a[i]>a[i-1])
-                l[i]=l[i-1]+1;
-        }
-        
-        ans+=l[n-1];
-        for(i=n-2;i>=0;i--)
-        {
-            if(a[i]>a[i+1])
-                l[i]=max(l[i],l[i+1]+1);
-            
-            ans+=l[i];
-        }
-        
-        return ans;
+    int candy(vector<int>& r) {
+        int n = r.size();
+        vector<int> c(n, 1);
+        for (int i = 1; i < n; i++)
+            if (r[i] > r[i - 1]) c[i] = c[i - 1] + 1;
+        for (int i = n - 2; ~i; i--)
+            if (r[i] > r[i + 1]) c[i] = max(c[i], c[i + 1] + 1);
+        int res = 0;
+        for (auto t: c) res += t;
+        return res;
     }
-    
-    /*
-    int candy(vector<int>& a) {
-        int n=a.size();
-        int i;
-        vector<int> l(n,1);
-        int ans=0;
-        
-        for(i=1;i<n;i++)
-        {
-            if(a[i]>a[i-1])
-                l[i]=l[i-1]+1;
-        }
-        
-        ans+=l[n-1];
-        int r=1;
-        for(i=n-2;i>=0;i--)
-        {
-            if(a[i]>a[i+1])
-                r=r+1;
-            else
-                r=1;
-            
-            ans+=max(l[i],r);
-        }
-        
-        return ans;
-    }
-    */
-    
-    /*
-    int candy(vector<int>& a) {
-        int n=a.size();
-        int i;
-        vector<int> l(n,1),r(n,1);
-        int ans=0;
-        
-        for(i=1;i<n;i++)
-        {
-            if(a[i]>a[i-1])
-                l[i]=l[i-1]+1;
-        }
-        
-        for(i=n-2;i>=0;i--)
-        {
-            if(a[i]>a[i+1])
-                r[i]=r[i+1]+1;
-        }
-        
-        for(i=0;i<n;i++)
-        {
-            ans+=max(l[i],r[i]);
-        }
-        return ans;
-    }
-    */
 };
