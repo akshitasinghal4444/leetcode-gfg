174ae90bb14c8d1e378905229705bc1a50b127aa
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-07-21 22:28:35 +0530

    Time: 4 ms (51.96%), Space: 7.5 MB (63.80%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -10,54 +10,13 @@
  */
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int l, int r) {
-        
-        if(!head || !head->next || l==r)
-            return head;
-        
-        int l2=l;
-        ListNode *t=head;
-        l--;
-        while(l>1)
-        {
-            t=t->next;
-            l--;
-        }
-        
-        ListNode *p,*t1,*t2;
-        
-        if(l!=0)
-        {
-            p=t;
-            t1=p->next;
-            t2=t1->next;
-        }
-        else
-        {
-            t=NULL;
-            p=NULL;
-            t1=head;
-        }
-        
-        int i=l2;
-        while(i<=r)
-        {
-            t2=t1->next;
-            t1->next=p;
-            p=t1;
-            t1=t2;
-            i++;
-        }
-        
-        if(l!=0)
-        {t->next->next=t1;
-        t->next=p;}
-        else
-        {
-            head->next=t1;
-            head=p;
-        }
-        
-        return head;
-    }
+    ListNode* reverseBetween(ListNode* head, int m, int n) 
+{
+	auto **pivot{&head->next}, **start{&head};
+
+	for(; --n && --m; pivot = &(*(start = pivot))->next); 
+	for(; n--; swap(*start, (*pivot)->next), swap(*start, *pivot)); 
+
+	return head;
+}
 };
