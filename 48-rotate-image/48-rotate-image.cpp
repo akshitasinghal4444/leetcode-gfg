b04541ceae2debf5865273f4b8dca7da77869b9c
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-03 17:39:47 +0530

    Time: 3 ms (64.45%), Space: 7 MB (92.92%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& a) {
+        int n=a.size();
+        int i,j;
+        
+        for(i=0;i<n/2;i++)
+        {
+            for(j=i;j<n-1-i;j++)
+            {
+                int t=a[i][j];
+                a[i][j]=a[n-1-j][i];
+                a[n-1-j][i]=a[n-1-i][n-1-j];
+                a[n-1-i][n-1-j]=a[j][n-1-i];                
+                a[j][n-1-i]=t;
+
+            }
+        }
+    }
+};
