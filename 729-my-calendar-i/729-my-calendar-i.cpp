b813e0a5f3c35686351041b65a654783928d94ed
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-03 14:42:55 +0530

    Time: 641 ms (5.03%), Space: 44.1 MB (6.38%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,83 +1,34 @@
 class MyCalendar {
 public:
-    map<int,int> m;
     
-    MyCalendar() {}
+    map<int,int> m;
+    MyCalendar() { }
     
     bool book(int s, int e) {
-        if(m[s]==1)
+        
+        if(m[s]>0)
             return 0;
         
+        int book=0;
         m[s]++;
         m[e]--;
-        int t=0;
-        
         for(auto it:m)
         {
-            if(it.first>e)
-                break;
+            if(it.first>=e)
+                return 1;
             
-            t+=it.second;
-            
-            if(t>1)
+            book+=it.second;
+            if(book>1)
             {
-                m[s]--;
+                m[s]--;;
                 m[e]++;
                 return 0;
             }
         }
+        
         return 1;
     }
 };
-
-/*
-class MyCalendar {
-public:
-    
-    map<int,int> m;
-    
-    MyCalendar() {    
-    }
-    
-    bool book(int s, int e) {
-        
-        auto it=m.lower_bound(e);
-        
-        if(it==m.begin() || (--it)->second<=s)
-        {
-            m[s]=e;
-            return 1;
-        }
-        return 0;
-    }
-};
-*/
-
-/*
-class MyCalendar {
-public:
-    vector<pair<int,int>> v;
-    
-    MyCalendar() {
-        
-    }
-    
-    bool book(int s, int e) {
-    
-        for(auto t:v)
-        {
-            int x=t.first,y=t.second;
-            if(s>=y || e<=x)
-                continue;
-            else
-                return 0;
-        }
-        
-        v.push_back({s,e});
-        return 1;
-    }
-};
-*/
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
