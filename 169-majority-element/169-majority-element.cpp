6eaf349edaf457ef6206d55b72fb8d6314fa476a
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-04 16:31:25 +0530

    Time: 51 ms (5.80%), Space: 19.5 MB (59.43%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,50 +1,23 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& a) 
-    {
+    int majorityElement(vector<int>& a) {
+        int i,n=a.size();
         int c=0,m=a[0];
         
-        for(int n:a)
+        for(i=0;i<n;i++)
         {
-            if(n==m)
+            if(a[i]==m)
                 c++;
             else
                 c--;
             
             if(c==0)
             {
-                m=n;
+                m=a[i];
                 c=1;
             }
         }
+        
         return m;
     }
-    
-    /*    
-    int majorityElement(vector<int>& a) {
-        int n=a.size();
-        if(n==1)
-            return a[0];
-        sort(a.begin(),a.end());
-        int c=1;
-        for(int i=1;i<n;i++)
-        {
-            if(a[i]==a[i-1])
-            {
-                c++;
-            }
-            else
-            {
-                if(c>n/2)
-                return a[i-1];
-                c=1;
-            }
-            
-        }
-        if(c>n/2)
-        return a[n-1];
-                
-        return 0;
-    }
-    */
 };
