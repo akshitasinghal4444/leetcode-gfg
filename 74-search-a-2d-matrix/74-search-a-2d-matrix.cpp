3bc2fc6c8635e59ce94d5db752838b54c5d1de73
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-04 14:55:54 +0530

    Time: 12 ms (15.11%), Space: 9.4 MB (78.54%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,110 +1,21 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& a, int t)
-    {
-        int m=a.size(),n=a[0].size();
-        int i=0,j=n-1;
+    bool searchMatrix(vector<vector<int>>& a, int t) {
+        int i,j,m=a.size(),n=a[0].size();
+        
+        i=0,j=n-1;
         
         while(i<m && j>=0)
         {
-            if(a[i][j]==t)
+            if(t==a[i][j])
                 return 1;
             
-            if(a[i][j]<t)
+            if(t>a[i][j])
                 i++;
-            else 
+            else
                 j--;
         }
         
         return 0;
     }
-    
-    /*
-    bool searchMatrix(vector<vector<int>>& a, int t)
-    {
-        int m=a.size(),n=a[0].size();
-        int i,j;
-        int l,r,mid;
-        
-        if(t<a[0][0] || t>a[m-1][n-1])
-            return 0;
-        
-        l=0,r=m-1;
-        
-        while(l<=r)
-        {
-            mid=(l+r)/2;
-            
-            if(a[mid][0]==t)
-                return 1;
-            
-            if(a[mid][0]>t)
-                r=mid-1;
-            else if(a[mid][0]<t && a[mid][n-1]>=t)
-                break;
-            else
-                l=mid+1;
-        }
-        
-        l=0,r=n-1,i=mid;
-        
-        if(t<a[i][0] || t>a[i][n-1])
-            return 0;
-        
-        while(l<=r)
-        {
-            mid=(l+r)/2;
-            
-            if(a[i][mid]==t)
-                return 1;
-            
-            if(a[i][mid]<t)
-                l=mid+1;
-            else
-                r=mid-1;
-        }
-        
-        return 0;
-    }
-    */
-    
-    /*
-    bool binSearch(vector<int> &a,int k)
-    {
-        int l=0,r=a.size()-1,mid;
-        
-        while(l<=r)
-        {
-            mid=(l+r)/2;
-            
-            if(a[mid]==k)
-                return 1;
-            
-            if(a[mid]<k)
-                l=mid+1;
-            else
-                r=mid-1;
-        }
-        return 0;
-    }
-    
-    bool searchMatrix(vector<vector<int>>& a, int t) {
-        int m=a.size();
-        int n=a[0].size();
-        int i=0;
-        while(i<m)
-        {
-            if(a[i][n-1]<t)
-                i++;
-            else if(a[i][0]>t)
-                return 0;
-            else
-                return binSearch(a[i],t);
-        }
-        return 0;
-    }
-    */
-    
-    
-
 };
