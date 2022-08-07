81b44e91ed8bad4ec4ebbc2ba78ab1333dd4e863
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-07 13:43:06 +0530

    Time: 60 ms (45.15%), Space: 71.5 MB (51.04%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,144 +10,33 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
-    {
-        ListNode *t,*h;
-        t=new ListNode(0);
-        h=t;
-        int sum=0;
-        
-        while(l1 || l2 || sum)
-        {
-            int a=0,b=0;
-            
-            if(l1)
-            {
-                a=l1->val;
-                l1=l1->next;
-            }
-            
-            if(l2)
-            {
-                b=l2->val;
-                l2=l2->next;
-            }
-            
-            sum+=a+b;
-            t->next=new ListNode(sum%10);
-            t=t->next;
-            sum=sum/10;
-        }
-        return h->next;
-    }
-    
-    /*
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
-    {
-        ListNode* ans=new ListNode(0);
-        ListNode* h=ans;
-        int sum=0;
-        
-        while(l1 || l2 || sum)
-        {
-            if(l1)
-            {
-                sum+=l1->val;
-                l1=l1->next;
-            }
-            if(l2)
-            {
-                sum+=l2->val;
-                l2=l2->next;
-            }
-            ListNode* t=new ListNode(sum%10);
-            h->next=t;
-            sum=sum/10;
-            h=h->next;
-        }
-        return ans->next;
-    }
-    */
-    
-    /*
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans=new ListNode(0);
-        ListNode* h=ans;
-        int sum=0;
-        while(l1 && l2)
+        ListNode *l3=new ListNode(0),*t=l3;
+        int c=0;
+        
+        while(l1 || l2 || c)
         {
+            int x=0;
             
-            sum+=l1->val+l2->val;
-            if(sum>=0 && sum<=9)
+            if(l1)
             {
-                ListNode* t=new ListNode(sum);
-                h->next=t;
-                sum=0;
+                x+=l1->val;
+                l1=l1->next;
             }
-            else
+            
+            if(l2)
             {
-                ListNode* t=new ListNode(sum%10);
-                h->next=t;
-                sum=sum/10;
+                x+=l2->val;
+                l2=l2->next;
             }
-            l1=l1->next;
-            l2=l2->next;
-            h=h->next;
+            
+            x+=c;
+            
+            t->next=new ListNode(x%10);
+            t=t->next;
+            c=x/10;
         }
         
-        while(l1)
-        {
-            sum+=l1->val;
-            if(sum>=0 && sum<=9)
-            {
-                ListNode* t=new ListNode(sum);
-                h->next=t;
-                sum=0;
-            }
-            else
-            {
-                ListNode* t=new ListNode(sum%10);
-                h->next=t;
-                sum=sum/10;
-            }
-            l1=l1->next;
-            h=h->next;
-        }
-        while(l2)
-        {
-            sum+=l2->val;
-            if(sum>=0 && sum<=9)
-            {
-                ListNode* t=new ListNode(sum);
-                h->next=t;
-                sum=0;
-            }
-            else
-            {
-                ListNode* t=new ListNode(sum%10);
-                h->next=t;
-                sum=sum/10;
-            }
-            l2=l2->next;
-            h=h->next;
-        }
-        while(sum)
-        {
-            if(sum>=0 && sum<=9)
-            {
-                ListNode* t=new ListNode(sum);
-                h->next=t;
-                sum=0;
-            }
-            else
-            {
-                ListNode* t=new ListNode(sum%10);
-                h->next=t;
-                sum=sum/10;
-            }
-            h=h->next;
-        }
-        return ans->next;
+        return l3->next;
     }
-    */
 };
