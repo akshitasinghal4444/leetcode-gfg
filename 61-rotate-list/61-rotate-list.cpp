a43ab3a3a68f4a7a8bbc98a1af241e4967142fa3
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-10 14:33:51 +0530

    Time: 17 ms (24.74%), Space: 11.7 MB (54.95%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -8,79 +8,48 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(!head)
-            return head;
-        
-        ListNode *t;
-        int l=1;
-        t=head;
-        
-        while(t->next)
-        {
-            l++;
-            t=t->next;
-        }
-        
-        k=k%l;
-        if(k==0)
-            return head;
-        
-        t->next=head;
-        t=head;
-        
-        for(int i=0;i<(l-k-1);i++)
-            t=t->next;
-        
-        head=t->next;
-        t->next=NULL;
-
-        return head;
-    }
-    
-    
-    /*
     int len(ListNode* head)
     {
         int l=0;
         while(head)
         {
-            head=head->next;
             l++;
+            head=head->next;
         }
+        
         return l;
     }
     
     ListNode* rotateRight(ListNode* head, int k) {
-        if(!head)
+        if(!head || !head->next)
             return head;
         
         int l=len(head);
-        
         k=k%l;
+        
         if(k==0)
             return head;
         
-        ListNode *f,*s;
-        f=s=head;
-        int i=0;
-        while(i++<k)
+        int n=k;
+        ListNode *t1,*t2;
+        t1=t2=head;
+        while(k--)
+            t1=t1->next;
+        
+        while(t1->next)
         {
-            f=f->next;
+            t1=t1->next;
+            t2=t2->next;
         }
-        while(f->next)
-        {
-            f=f->next;
-            s=s->next;
-        }
-        f->next=head;
-        head=s->next;
-        s->next=NULL;
-
+        
+        t1->next=head;
+        head=t2->next;
+        t2->next=NULL;
+        
         return head;
     }
-    */
 };
