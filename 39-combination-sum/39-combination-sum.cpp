30e1605f26386439c6d040482639ee1107c39905
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-17 12:02:34 +0530

    Time: 40 ms (33.05%), Space: 20 MB (31.73%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,59 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> v;
-    
-    void helper(vector<int>& a, int in,int sum,int n,int t)
-    {
-        if(sum==t)
-        {
-            ans.push_back(v);
-            return;
-        }
-        
-        if(in==n)
-            return;
-        
-        helper(a,in+1,sum,n,t);
-        
-        if(sum+a[in]<=t)
-        {
-            v.push_back(a[in]);
-            helper(a,in,sum+a[in],n,t);
-            v.pop_back();
-        }
-    }
-    
-    vector<vector<int>> combinationSum(vector<int>& a, int t)
-    {
-        int n=a.size();
-        ans={};
-        v={};
-        
-        helper(a,0,0,n,t);
-        
-        return ans;
-    }
-    
-    /*
-    vector<vector<int>> combinationSum(vector<int>& a, int t)
-    {
-        int n=a.size();
-        int i,j;
-        vector<vector<int>> dp[t+1];
-        dp[0]={};
+    vector<vector<int>> combinationSum(vector<int>& a, int t) {
+        int i,j,n=a.size();
+        vector<vector<int>> ans;
+        vector<vector<vector<int>>> dp(t+1);
+        dp[0].push_back({});
         
         for(i=0;i<n;i++)
         {
             for(j=1;j<=t;j++)
             {
-                if(a[i]==j)
+                if(a[i]<=j)
                 {
-                    dp[j].push_back({a[i]});
-                }
-                else if(a[i]<j)
-                {
-                    for(vector<int> v:dp[j-a[i]])
+                    for(auto v:dp[j-a[i]])
                     {
                         v.push_back(a[i]);
                         dp[j].push_back(v);
@@ -61,7 +20,7 @@ public:
                 }
             }
         }
-        return dp[t];
+                                  
+        return dp[t]; 
     }
-    */
 };
