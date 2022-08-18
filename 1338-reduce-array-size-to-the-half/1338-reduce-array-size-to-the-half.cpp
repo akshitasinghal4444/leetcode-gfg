26b7a6fa89bb0eab9f1f2f8d1246246e3b6b57aa
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-18 11:25:16 +0530

    Time: 287 ms (69.75%), Space: 78.4 MB (48.79%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -2,17 +2,40 @@ class Solution {
 public:
     int minSetSize(vector<int>& a) {
         int i,n=a.size();
+        int x=0,c=0,k=0;
         unordered_map<int,int> f;
+        vector<int> v;
         
         for(i=0;i<n;i++)
             f[a[i]]++;
         
+        for(auto it:f)
+            v.push_back(it.second);
+        
+        sort(v.begin(),v.end(),greater<int>());
+        
+        while(x<n/2)
+        {
+            x+=v[k++];
+            c++;
+        }
+        
+        return c;
+    }
+    
+    /*
+    int minSetSize(vector<int>& a) {
+        int i,n=a.size();
+        int x=0,c=0;
+        unordered_map<int,int> f;
         priority_queue<int> q;
+        
+        for(i=0;i<n;i++)
+            f[a[i]]++;
         
         for(auto it:f)
             q.push(it.second);
         
-        int x=0,c=0;
         while(x<n/2)
         {
             x+=q.top();
@@ -22,4 +45,5 @@ public:
         
         return c;
     }
+    */
 };
