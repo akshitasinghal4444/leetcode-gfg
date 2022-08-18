0a20728695caad17f26d44557cb87c83b21a2f1a
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-18 13:11:02 +0530

    Time: 43 ms (78.73%), Space: 18.2 MB (16.19%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,6 +1,66 @@
 class Solution {
 public:
     
+    void bfs(vector<vector<char>>& g,int m,int n,int i,int j)
+    {
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        g[i][j]='0';
+        
+        
+        while(!q.empty())
+        {
+            i=q.front().first;
+            j=q.front().second;
+            q.pop();
+            
+            if(i-1>=0 && g[i-1][j]=='1')
+            {
+                g[i-1][j]='0';
+                q.push({i-1,j});
+            }
+        
+            if(i+1<m && g[i+1][j]=='1')
+            {
+                g[i+1][j]='0';
+                q.push({i+1,j});
+            }
+        
+            if(j-1>=0 && g[i][j-1]=='1')
+            {
+                g[i][j-1]='0';
+                q.push({i,j-1});
+            }
+        
+            if(j+1<n && g[i][j+1]=='1')
+            {
+                g[i][j+1]='0';
+                q.push({i,j+1});
+            }
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& g) {
+        int m=g.size(),n=g[0].size();
+        int i,j;
+        int ans=0;
+        
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(g[i][j]=='1')
+                {
+                    ans++;
+                    bfs(g,m,n,i,j);
+                }                 
+            }
+        }
+        
+        return ans;
+    }
+    
+    /*
     void dfs(vector<vector<char>>& g,int m,int n,int i,int j)
     {
         g[i][j]='0';
@@ -37,4 +97,5 @@ public:
         
         return ans;
     }
+    */
 };
