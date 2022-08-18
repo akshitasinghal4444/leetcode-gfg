4806ca064617a5ee17a9a4b3358bd47dcedc7dae
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-18 14:11:35 +0530

    Time: 7 ms (40.40%), Space: 8 MB (27.32%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -3,31 +3,28 @@ public:
     vector<vector<int>> ans;
     vector<int> t;
     
-    void permute(vector<int>& a,int n,int st)
+    void permute(vector<int>& a,int n,int in)
     {
-        if(st==n)
+        if(in==n)
         {
             ans.push_back(t);
-            return ;
+            return;
         }
-        int i;
-        for(i=st;i<n;i++)
+        
+        for(int i=in;i<n;i++)
         {
-            swap(a[i],a[st]);
-            t.push_back(a[st]);
-            permute(a,n,st+1);
+            swap(a[i],a[in]);
+            t.push_back(a[in]);
+            
+            permute(a,n,in+1);
+            
+            swap(a[i],a[in]);
             t.pop_back();
-            swap(a[i],a[st]);
         }
     }
     
     vector<vector<int>> permute(vector<int>& a) {
-        int n=a.size();
-        ans={};
-        t={};
-        
-        permute(a,n,0);
-        
+        permute(a,a.size(),0);
         return ans;
     }
 };
