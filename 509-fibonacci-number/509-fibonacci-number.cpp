1c6980404902561409e312474da64ae7e8b32c32
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-19 13:21:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.80%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
-	double goldenRatio = (1 + sqrt(5)) / 2;
-	return round(pow(goldenRatio, n) / sqrt(5));
-}
+        
+        if(n<=1)
+            return n;
+        
+        int f0=0,f1=1,f;
+        
+        for(int i=2;i<=n;i++)
+        {
+            f=f0+f1;
+            f0=f1;
+            f1=f;
+        }
+        
+        return f;
+    }
 };
