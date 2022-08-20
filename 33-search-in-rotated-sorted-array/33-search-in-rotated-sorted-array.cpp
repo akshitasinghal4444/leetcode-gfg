e514f255575ba8b9db3ba278b19327e662a3f6a7
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-20 21:00:33 +0530

    Time: 7 ms (53.97%), Space: 11.1 MB (75.25%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int search(vector<int>& a, int t)
-    {
+    int search(vector<int>& a, int t) {
         int l=0,r=a.size()-1;
         
         while(l<=r)
@@ -13,19 +12,20 @@ public:
             
             if(a[mid]<=a[r])
             {
-                if(t>=a[mid] && t<=a[r])
+                if(t>a[mid] && t<=a[r])
                     l=mid+1;
                 else
                     r=mid-1;
             }
             else
             {
-                if(t>=a[l] && t<=a[mid])
+                if(t>=a[l] && t<a[mid])
                     r=mid-1;
                 else
                     l=mid+1;
             }
         }
+        
         return -1;
     }
 };
