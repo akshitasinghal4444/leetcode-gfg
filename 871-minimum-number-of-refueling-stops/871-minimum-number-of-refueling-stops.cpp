f3abd7dc464ca954b2a2c6ff215f2c12e436c6fe
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-20 13:18:21 +0530

    Time: 33 ms (83.69%), Space: 16.2 MB (80.69%) - LeetHub

---
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minRefuelStops(int t, int fuel, vector<vector<int>>& a) {
+        priority_queue<int> pq;
+        int pos=fuel;
+        int i=0,n=a.size();
+        int ans=0;
+        
+        while(pos<t && (i<n || !pq.empty()))
+        {
+            ans++;
+            while(i<n && a[i][0]<=pos)
+            {
+                pq.push(a[i][1]);
+                i++;
+            }
+            
+            if(pq.empty())
+                break;
+            
+            fuel=pq.top();
+            pq.pop();
+            pos+=fuel;
+        }
+        
+        if(pos>=t)
+            return ans;
+        
+        return -1;
+    }
+};
