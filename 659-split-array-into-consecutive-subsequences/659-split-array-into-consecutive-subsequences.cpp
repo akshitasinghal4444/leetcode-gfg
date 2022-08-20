3f4a60f664ca6bdb70cb3759a780474f22271949
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-20 12:23:51 +0530

    Time: 131 ms (72.37%), Space: 58.4 MB (41.56%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -1,5 +1,42 @@
 class Solution {
 public:
+    bool isPossible(vector<int>& a)
+    {
+        unordered_map<int,int> f,req;
+        int i,n=a.size();
+        
+        for(i=0;i<n;i++)
+            f[a[i]]++;
+        
+        for(i=0;i<n;i++)
+        {
+            if(f[a[i]]==0)
+                continue;
+            
+            if(req[a[i]]>0)
+            {
+                req[a[i]]--;
+                f[a[i]]--;
+                
+                req[a[i]+1]++;
+            }
+            else
+            {
+                if(f[a[i]+1]==0 || f[a[i]+2]==0)
+                    return 0;
+                
+                f[a[i]]--;
+                f[a[i]+1]--;
+                f[a[i]+2]--;
+                
+                req[a[i]+3]++;
+            }
+        }
+        
+        return 1;
+    }
+    
+    /*
     bool isPossible(vector<int>& nums) {
         vector<int> cnt (2001, 0);
         for (int& n : nums) cnt[n + 1000]++;
@@ -23,4 +60,5 @@ public:
         
         return true;
     }
+    */
 };
