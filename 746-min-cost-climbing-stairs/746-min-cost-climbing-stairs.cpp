c4518a34d0a28ef9e0bdfe5870dfa2f9e661ce48
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-20 11:30:06 +0530

    Time: 14 ms (32.57%), Space: 13.7 MB (48.96%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -3,6 +3,16 @@ public:
     int minCostClimbingStairs(vector<int>& a) {
         int n=a.size(),i;
         
+        for(i=n-3;i>=0;i--)
+            a[i]+=min(a[i+1],a[i+2]);
+        
+        return min(a[0],a[1]);
+    }
+    
+    /*
+    int minCostClimbingStairs(vector<int>& a) {
+        int n=a.size(),i;
+        
         int dp[n+1];
         dp[n]=0;
         dp[n-1]=a[n-1];
@@ -12,4 +22,5 @@ public:
         
         return min(dp[0],dp[1]);
     }
+    */
 };
