4325427386743c6b326d6a24afc6e33a47aab9cd
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-20 11:49:51 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (91.15%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -4,19 +4,12 @@ public:
         int dp[m][n];
         int i,j;
         
-        dp[m-1][n-1]=1;
-        
         for(i=m-1;i>=0;i--)
         {
             for(j=n-1;j>=0;j--)
             {
-                if(i==m-1 && j==n-1)
-                    continue;
-                
-                if(i==m-1)
-                    dp[i][j]=dp[i][j+1];
-                else if(j==n-1)
-                    dp[i][j]=dp[i+1][j];
+                if(i==m-1 || j==n-1)
+                    dp[i][j]=1;
                 else
                     dp[i][j]=dp[i+1][j]+dp[i][j+1];
             }
