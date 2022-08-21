c6c7c270bb39be3b8cfd8f22b2438e5d40fd1323
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-21 11:26:23 +0530

    Time: 20 ms (69.22%), Space: 8.8 MB (39.63%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -2,6 +2,37 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         int ns=s.length(),np=p.length();
+        vector<int> fs(26,0),fp(26,0);
+        int i,j;
+        vector<int> ans;
+        
+        if(ns<np)
+            return ans;
+        
+        for(i=0;i<np;i++)
+            fp[p[i]-'a']++;
+        
+        for(i=0;i<np;i++)
+            fs[s[i]-'a']++;
+        
+        if(fs==fp)
+            ans.push_back(0);
+        
+        for(;i<ns;i++)
+        {
+            fs[s[i]-'a']++;
+            fs[s[i-np]-'a']--;
+            
+            if(fs==fp)
+                ans.push_back(i-np+1);
+        }
+        
+        return ans;
+    }
+    
+    /*
+    vector<int> findAnagrams(string s, string p) {
+        int ns=s.length(),np=p.length();
         int fs[26]={0},fp[26]={0};
         int i,j;
         vector<int> ans;
@@ -49,4 +80,5 @@ public:
         
         return ans;
     }
+    */
 };
