48c4c70ea98b4ec5684090b707596d635e54a725
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-22 12:39:35 +0530

    Time: 4 ms (40.16%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -5,12 +5,30 @@ public:
         if(n<=0)
             return 0;
         
+        while(n>1)
+        {
+            if(n%4!=0)
+                return 0;
+            
+            n=n/4;
+        }
+        
+        return 1;    
+    }
+    
+    /*
+    bool isPowerOfFour(int n)
+    {
+        if(n<=0)
+            return 0;
+        
         long n1=1;
         while(n1<n)
             n1*=4;
         
         return n1==n;    
     }
+    */
     
     /*
     bool isPowerOfFour(int n) {
