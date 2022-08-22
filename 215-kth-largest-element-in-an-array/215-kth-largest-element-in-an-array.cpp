0e41b95b0dd107970340ee5f3cdec3ace2153f75
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-22 21:04:37 +0530

    Time: 121 ms (30.47%), Space: 45.6 MB (19.73%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,5 +1,47 @@
 class Solution {
 public:
+    int partition(vector<int>& a,int l,int r)
+    {
+        int x=rand()%(r-l+1)+l;
+        swap(a[x],a[r]);
+        
+        int i=l,j=l;
+        
+        while(i<r)
+        {
+            if(a[i]<=a[r])
+                swap(a[i],a[j++]);
+            
+            i++;
+        }
+        
+        // cout<<l<<" "<<r<<" "<<a[r]<<" "<<j<<endl;
+        swap(a[j],a[r]);
+        return j;
+    }
+    
+    int quickselect(vector<int>& a, int k,int l,int r)
+    {
+        if(l<=r)
+        {
+            int pivot=partition(a,l,r);
+            if(pivot==k)
+                return a[k];
+            
+            if(pivot<k)
+                return quickselect(a,k,pivot+1,r);
+            else
+                return quickselect(a,k,l,pivot-1);
+        }
+        
+        return -1;
+    }
+    
+    int findKthLargest(vector<int>& a, int k)
+    {
+        return quickselect(a,a.size()-k,0,a.size()-1);
+    }
+    
     /*
     int findKthLargest(vector<int>& a, int k) {
         sort(a.begin(),a.end(),greater<int>());
@@ -7,6 +49,7 @@ public:
     }
     */
     
+    /*
     int findKthLargest(vector<int>& a, int k)
     {
         priority_queue<int,vector<int>,greater<int>> pq;
@@ -18,9 +61,7 @@ public:
                 pq.pop();
         }
         
-//         while(pq.size()>1)
-//             pq.pop();
-        
         return pq.top();
     }
+    */
 };
