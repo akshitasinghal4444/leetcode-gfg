6aa8a25b404863581ac771c14cc3b586e1dec55b
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-23 17:43:48 +0530

    Time: 31 ms (31.11%), Space: 13.5 MB (89.20%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,109 +1,26 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& a, int k) {
-        
-        priority_queue<pair<int,int>> q;
         unordered_map<int,int> m;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         vector<int> ans;
         
-        for(int n:a)
-            m[n]++;
-        
-        for(auto it:m)
-            q.push({it.second,it.first});
-        
-        while(k--)
-        {
-            ans.push_back(q.top().second);
-            q.pop();
-        }
-        
-        return ans;
-    }
-    
-    /*
-    int partition(vector<int>& a, int l,int r,unordered_map<int,int> &m)
-    {
-        int x=rand()%(r-l+1)+l;
-        swap(a[x],a[r]);
-        
-        int i,j=l-1;
-        
-        for(i=l;i<r;i++)
-        {
-            if(m[a[i]]>m[a[r]])
-            {
-                j++;
-                if(i!=j)
-                    swap(a[i],a[j]);
-            }
-        }
-        
-        j++;
-        swap(a[j],a[r]);
-        
-        return j;
-    }
-    
-    void quickselect(vector<int>& a, int l,int r,int k,unordered_map<int,int> &m)
-    {
-        if(l<=r)
-        {
-            int pivot=partition(a,l,r,m);
-            
-            if(pivot==k)
-                return;
-            
-            if(pivot<k)
-                quickselect(a,pivot+1,r,k,m);
-            else
-                quickselect(a,l,pivot-1,k,m);
-        }
-    }
-    
-    vector<int> topKFrequent(vector<int>& a, int k) 
-    {
-        unordered_map<int,int> m;
-        vector<int> ans;
-        
-        for(int n:a)
-        {
-            m[n]++;
-            
-            if(m[n]==1)
-                ans.push_back(n);   
-        }
-        
-        quickselect(ans,0,ans.size()-1,k-1,m);
-        return vector<int> (ans.begin(),ans.begin()+k);
-    }
-    */
-    
-    /*
-    vector<int> topKFrequent(vector<int>& a, int k) {
-        
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
-        unordered_map<int,int> m;
-        vector<int> ans;
-        
-        for(int n:a)
-            m[n]++;
+        for(int x:a)
+            m[x]++;
         
         for(auto it:m)
         {
-            q.push({it.second,it.first});
-            
-            if(q.size()>k)
-                q.pop();
+            pq.push({it.second,it.first});
+            if(pq.size()>k)
+                pq.pop();
         }
         
-        while(!q.empty())
+        while(pq.size())
         {
-            ans.push_back(q.top().second);
-            q.pop();
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
-        
+            
         return ans;
     }
-    */
 };
