7687a02a6bd598d2b159f8d90559bd5bf7dece2e
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-23 11:33:49 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (89.87%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,5 +1,83 @@
 class Solution {
 public:
+    bool backspaceCompare(string s, string t)
+    {
+        int ns=s.length(),nt=t.length();
+        int i=ns-1,j=nt-1;
+        int cs=0,ct=0;
+        
+        while(i>=0 && j>=0)
+        {
+            if(s[i]=='#')
+            {
+                cs++;
+                i--;
+                continue;
+            }
+            else if(cs)
+            {
+                cs--;
+                i--;
+                continue;
+            }         
+            
+            if(t[j]=='#')
+            {
+                ct++;
+                j--;
+                continue;
+            }
+            else if(ct)
+            {
+                ct--;
+                j--;
+                continue;
+            }   
+            
+            if(s[i--]!=t[j--])
+                return 0;
+        }
+        
+        while(i>=0)
+        {
+            if(s[i]=='#')
+            {
+                cs++;
+                i--;
+                continue;
+            }
+            else if(cs)
+            {
+                cs--;
+                i--;
+                continue;
+            }    
+            else
+                break;
+        }
+        
+        while(j>=0)
+        {
+            if(t[j]=='#')
+            {
+                ct++;
+                j--;
+                continue;
+            }
+            else if(ct)
+            {
+                ct--;
+                j--;
+                continue;
+            }    
+            else
+                break;
+        }
+        
+        return i==-1 && j==-1;
+    }
+    
+    /*
     bool backspaceCompare(string s, string t) {
         string s1="",s2="";
         
@@ -27,4 +105,5 @@ public:
         
         return s1==s2;
     }
+    */
 };
