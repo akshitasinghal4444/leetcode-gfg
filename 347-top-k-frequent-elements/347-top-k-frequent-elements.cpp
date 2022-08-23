e378357743590dd2436d2a57ca593a765c5a3b94
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-23 17:56:01 +0530

    Time: 25 ms (54.22%), Space: 13.7 MB (72.48%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,5 +1,56 @@
 class Solution {
 public:
+    int partition(vector<int> &a,unordered_map<int,int> &m,int l,int r)
+    {
+        int x=rand()%(r-l+1)+l;
+        swap(a[x],a[r]);
+        
+        int j=l;
+        
+        while(l<r)
+        {
+            if(m[a[l]]>=m[a[r]])
+                swap(a[l],a[j++]);
+            l++;
+        }
+        
+        swap(a[r],a[j]);
+        return j;
+    }
+    
+    void quickSelect(vector<int> &a,unordered_map<int,int> &m,int l,int r,int k)
+    {
+        if(l>r)
+            return;
+        
+        int p=partition(a,m,l,r);
+        
+        if(p==k)
+            return;
+        
+        if(p<k)
+            return quickSelect(a,m,p+1,r,k);
+        
+        return quickSelect(a,m,l,p-1,k);
+    }
+    
+    vector<int> topKFrequent(vector<int>& a, int k)
+    {
+        unordered_map<int,int> m;
+        vector<int> ans;
+        
+        for(int x:a)
+            m[x]++;
+        
+        for(auto it:m)
+            ans.push_back(it.first);
+        
+        quickSelect(ans,m,0,ans.size()-1,k-1);
+        
+        return vector<int> (ans.begin(),ans.begin()+k);
+    }
+    
+    /*
     vector<int> topKFrequent(vector<int>& a, int k) {
         unordered_map<int,int> m;
         priority_queue<pair<int,int>> pq;
@@ -20,6 +71,7 @@ public:
             
         return ans;
     }
+    */
     
     /*
     vector<int> topKFrequent(vector<int>& a, int k) {
