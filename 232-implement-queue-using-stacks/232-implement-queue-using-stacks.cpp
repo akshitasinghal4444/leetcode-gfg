e406135153772f801f95d80362368ac388c51ea4
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-24 17:28:34 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (99.20%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,97 +1,18 @@
 class MyQueue {
 public:
-    stack<int> s;
-    
-    MyQueue() {
-        
-    }
-    
-    void push(int x) {
-        
-        if(s.empty())
-        {
-            s.push(x);
-            return;
-        }
-        
-        int t=s.top();
-        s.pop();
-        push(x);
-        s.push(t);
-    }
-    
-    int pop() {
-        int x=s.top();
-        s.pop();
-        return x;
-    }
-    
-    int peek() {
-        return s.top();
-    }
-    
-    bool empty() {
-        return s.empty();
-    }
-};
-
-/*
-class MyQueue {
-public:
     
     stack<int> s1,s2;
     
-    MyQueue() {
-        
-    }
-    
-    void push(int x) {
-        while(!s1.empty())
-        {
-            s2.push(s1.top());
-            s1.pop();
-        }
-        
-        s1.push(x);
-        
-        while(!s2.empty())
-        {
-            s1.push(s2.top());
-            s2.pop();
-        }
-    }
-    
-    int pop() {
-        int x=s1.top();
-        s1.pop();
-        return x;
-    }
-    
-    int peek() {
-        return s1.top();
-    }
-    
-    bool empty() {
-        return s1.empty();
-    }
-};
-*/
-
-/*
-class MyQueue {
-public:
-    
-    stack<int> s1,s2;
-    
-    MyQueue() {
-        
-    }
+    MyQueue() {}
     
     void push(int x) {
         s1.push(x);
     }
     
     int pop() {
+        if(s1.empty())
+            return -1;
+        
         while(!s1.empty())
         {
             s2.push(s1.top());
@@ -111,6 +32,9 @@ public:
     }
     
     int peek() {
+        if(s1.empty())
+            return -1;
+        
         while(!s1.empty())
         {
             s2.push(s1.top());
@@ -118,7 +42,6 @@ public:
         }
         
         int x=s2.top();
-        // s2.pop();
         
         while(!s2.empty())
         {
@@ -133,7 +56,7 @@ public:
         return s1.empty();
     }
 };
-*/
+
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue* obj = new MyQueue();
