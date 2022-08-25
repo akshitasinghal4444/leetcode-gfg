71b2d7f5dd8825a5de7003cef80662e158d9c108
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-25 13:22:31 +0530

    Time: 7 ms (21.22%), Space: 5.9 MB (64.83%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -15,6 +15,30 @@ public:
     }
     
     bool isHappy(int n) {
+        int x=calc(n);
+        
+        while(x!=1 && x!=89)
+            x=calc(x);
+        
+        return x==1;
+    }
+    
+    /*
+    int calc(int n)
+    {
+        int ans=0;
+        
+        while(n>0)
+        {
+            int x=n%10;
+            ans+=x*x;
+            n=n/10;
+        }
+        
+        return ans;
+    }
+    
+    bool isHappy(int n) {
         unordered_set<int> s;
         int x=calc(n);
         
@@ -30,4 +54,5 @@ public:
         
         return 1;
     }
+    */
 };
