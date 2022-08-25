c94b892bfb251cfee62a6e072bba6d80f56ee156
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-25 14:10:51 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (30.39%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -2,8 +2,48 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& a) {
         vector<int> ans;
-        int m=a.size(),n=a[0].size();
-        int l=0,r=n-1,t=0,b=m-1,i;
+        int l=0,r=a[0].size()-1,t=0,b=a.size()-1;
+        int i,dir=1;
+        
+        while(l<=r && t<=b)
+        {
+            
+            if(dir==1)
+            {   for(i=l;i<=r;i++)
+                    ans.push_back(a[t][i]);
+                t++;
+                dir++;
+            }
+            else if(dir==2)
+            {    
+                for(i=t;i<=b;i++)
+                    ans.push_back(a[i][r]);
+                r--;
+                dir++;
+            }
+            else if(dir==3)
+            { 
+                for(i=r;i>=l;i--)
+                    ans.push_back(a[b][i]);
+                b--;
+                dir++;
+            }
+            else if(dir==4)
+            { 
+                for(i=b;i>=t;i--)
+                    ans.push_back(a[i][l]);
+                l++;
+                dir=1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    /*
+    vector<int> spiralOrder(vector<vector<int>>& a) {
+        vector<int> ans;
+        int l=0,r=a[0].size()-1,t=0,b=a.size()-1,i;
         
         while(l<=r && t<=b)
         {
@@ -29,4 +69,5 @@ public:
         
         return ans;
     }
+    */
 };
