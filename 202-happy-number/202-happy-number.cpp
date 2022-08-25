b5d006d4311bbd12b812ec4c145c38a6aa5dfde9
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-25 13:20:36 +0530

    Time: 4 ms (47.31%), Space: 6.2 MB (43.96%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int calc(int n)
+    {
+        int ans=0;
+        
+        while(n>0)
+        {
+            int x=n%10;
+            ans+=x*x;
+            n=n/10;
+        }
+        
+        return ans;
+    }
+    
+    bool isHappy(int n) {
+        unordered_set<int> s;
+        int x=calc(n);
+        
+        while(x!=1)
+        {
+            if(s.find(x)!=s.end())
+                return 0;
+            
+            s.insert(x);
+            
+            x=calc(x);
+        }
+        
+        return 1;
+    }
+};
