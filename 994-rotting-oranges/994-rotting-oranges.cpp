0c08a7ced843ed17a766326563769b719b50b73e
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-25 20:40:35 +0530

    Time: 4 ms (92.51%), Space: 13.1 MB (47.67%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,74 +1,69 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& a)
-    {
-        int m,n,ans=0;
-        m=a.size();
-        n=a[0].size();
-        
+    int orangesRotting(vector<vector<int>>& g) {
+        int m=g.size(),n=g[0].size();
+        int i,j;
+        int f=0,c=0,ans=0;
         queue<pair<int,int>> q;
         
-        int i,j,f=0;
         for(i=0;i<m;i++)
         {
             for(j=0;j<n;j++)
             {
-                if(a[i][j]==2)
-                {
-                    q.push({i,j});
-                    a[i][j]=0;
-                }
-                else if(a[i][j]==1)
+                if(g[i][j]==1)
                     f++;
+                else if(g[i][j]==2)
+                    q.push({i,j});
             }
         }
         
-        if(q.empty() || f==0)
-            return f>0?-1:0;
-            
+        if(f==0)
+            return 0;
+        
         while(!q.empty())
         {
             ans++;
             int s=q.size();
+            
             while(s--)
             {
                 i=q.front().first;
                 j=q.front().second;
                 q.pop();
                 
-                if(i-1>=0 && a[i-1][j]==1)
+                if(i-1>=0 && g[i-1][j]==1)
                 {
                     q.push({i-1,j});
-                    a[i-1][j]=0;
+                    g[i-1][j]=2;
                     f--;
                 }
                 
-                if(j+1<n && a[i][j+1]==1)
-                {
-                    q.push({i,j+1});
-                    a[i][j+1]=0;
-                    f--;
-                }
-                
-                if(i+1<m && a[i+1][j]==1)
+                if(i+1<m && g[i+1][j]==1)
                 {
                     q.push({i+1,j});
-                    a[i+1][j]=0;
+                    g[i+1][j]=2;
                     f--;
                 }
                 
-                if(j-1>=0 && a[i][j-1]==1)
+                if(j-1>=0 && g[i][j-1]==1)
                 {
                     q.push({i,j-1});
-                    a[i][j-1]=0;
+                    g[i][j-1]=2;
                     f--;
                 }
+                
+                if(j+1<n && g[i][j+1]==1)
+                {
+                    q.push({i,j+1});
+                    g[i][j+1]=2;
+                    f--;
+                }
+                
+                if(f==0)
+                return ans;
             }
         }
         
-        if(f==0)
-        return ans-1;
-        else
-            return -1;
+        return -1;
     }
 };
