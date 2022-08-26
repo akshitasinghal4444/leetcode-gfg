096a845a0a4af528c7408d161fdee3923104e479
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-26 11:25:44 +0530

    Time: 7 ms (56.28%), Space: 15.4 MB (6.16%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,55 +1,27 @@
 class Solution {
 public:
-    /*
-    string longestCommonPrefix(vector<string>& str)
-    {
-        sort(str.begin(),str.end());
-        string s1=str[0];
-        string s2=str.back();
-        int n1=s1.length(),n2=s2.length();
-        
-        string ans="";
-        
-        for(int i=0;i<n1 && i<n2;i++)
-        {
-            if(s1[i]==s2[i])
-                ans+=s1[i];
-            else
-                break;
-        }
-        return ans;
-    }
-    */
-    
     string longestCommonPrefix(vector<string>& str) {
+        int i=0;
         string ans="";
-        bool f=0;
-        int i=0,l=str[0].length();
-        char ch;
+        char c;
+        int n=str.size();
         
-        while(f==0)
+        while(1)
         {
-            if(i<l)
-                ch=str[0][i];
+            if(i<str[0].length())
+                c=str[0][i];
             else
-                break;
+                return ans;
             
             for(string s:str)
             {
-                if(i<s.length() && ch==s[i])
-                    continue;
-                else
-                {
-                    f=1;
-                    break;
-                } 
+                if(i>s.length() || s[i]!=c)
+                    return ans;
             }
             
-            if(f==0)
-                ans+=ch;
             i++;
+            ans+=c;
         }
         return ans;
     }
-    
 };
