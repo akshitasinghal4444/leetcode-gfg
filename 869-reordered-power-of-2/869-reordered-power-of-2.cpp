4990d0b845914c6de601b39e5d47930f5ec017f1
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-26 14:28:40 +0530

    Time: 6 ms (50.57%), Space: 6.1 MB (68.93%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    bool reorderedPowerOf2(int n) {
+        if(n==1)
+            return 1;
+        
+        string s1=to_string(n);
+        sort(s1.begin(),s1.end());
+        
+        long x=1,i;
+        for(i=0;i<31;i++)
+        {
+            x*=2;
+            string s2=to_string(x);
+            sort(s2.begin(),s2.end());
+            
+            if(s1==s2)
+                return 1;
+        }
+        
+        return 0;
+    }
+    
+    /*
     void count(long n, vector<int> &f)
     {
         while(n>0)
@@ -29,4 +51,5 @@ public:
         
         return 0;
     }
+    */
 };
