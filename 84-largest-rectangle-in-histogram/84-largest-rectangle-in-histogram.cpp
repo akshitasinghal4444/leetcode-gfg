986aa75758d155729c15282d60c5df73be786e71
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-26 18:02:36 +0530

    Time: 297 ms (47.63%), Space: 85 MB (30.06%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,117 +1,38 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& a)
-    {
-        int n=a.size();
+    int largestRectangleArea(vector<int>& a) {
+        int i,n=a.size();
         int ans=0;
-        int i;
-        
-        stack<int> s;
-        a.push_back(0);
-        
-        for(i=0;i<=n;i++)
-        {
-            while(!s.empty() && a[s.top()]>a[i])
-            {
-                int j=s.top();
-                s.pop();
-                
-                int w;
-                if(s.empty())
-                    w=i;
-                else
-                    w=(i-s.top()-1);
-                
-                ans=max(ans,w*a[j]);
-            }
-            
-            s.push(i);
-        }
-        
-        return ans;
-    }
-    
-    /*
-    int largestRectangleArea(vector<int>& a)
-    {
-        int n=a.size();
-        int i,j;
-        
-        vector<int> lmin(n),rmin(n);
-        
-        lmin[0]=-1;
-        for(i=1;i<n;i++)
-        {
-            j=i-1;
-            while(j>=0 && a[j]>=a[i])
-                j=lmin[j];
-            
-            lmin[i]=j;
-        }
-        
-        rmin[n-1]=n;
-        for(i=n-2;i>=0;i--)
-        {
-            j=i+1;
-            while(j<n && a[j]>=a[i])
-                j=rmin[j];
-            
-            rmin[i]=j;
-        }
-        
-        int ans=0;
-        for(i=0;i<n;i++)
-            ans=max(ans,(rmin[i]-lmin[i]-1)*a[i]);
-        
-        return ans;
-    }
-    */
-    
-    /*
-    int largestRectangleArea(vector<int>& a)
-    {
-        int n=a.size();
-        int i;
-        
-        stack<int> s;
-        vector<int> lmin(n),rmin(n);
+        vector<int> r(n,n),l(n,-1);
+        stack<int> st;
         
         for(i=0;i<n;i++)
         {
-            while(!s.empty() && a[s.top()]>a[i])
+            while(!st.empty() && a[st.top()]>a[i])
             {
-                rmin[s.top()]=i;
-                s.pop();
+                r[st.top()]=i;
+                st.pop();
             }
-            s.push(i);
+            
+            st.push(i);
         }
-        
-        while(!s.empty())
-        {
-            rmin[s.top()]=n;
-            s.pop();
-        }
+        while(!st.empty())
+            st.pop();
         
         for(i=n-1;i>=0;i--)
         {
-            while(!s.empty() && a[s.top()]>a[i])
+            while(!st.empty() && a[st.top()]>a[i])
             {
-                lmin[s.top()]=i;
-                s.pop();
+                l[st.top()]=i;
+                st.pop();
             }
-            s.push(i);
-        }
-        while(!s.empty())
-        {
-            lmin[s.top()]=-1;
-            s.pop();
+            
+            st.push(i);
         }
         
-        int ans=0;
         for(i=0;i<n;i++)
-            ans=max(ans,(rmin[i]-lmin[i]-1)*a[i]);
+        ans=max(ans,a[i]*(r[i]-l[i]-1));
         
         return ans;
     }
-    */
 };
