5fa7f03ded8218265cf72563b3d1d6c66b779550
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-26 12:05:57 +0530

    Time: 15 ms (44.46%), Space: 6.7 MB (62.08%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -1,5 +1,38 @@
 class Solution {
 public:
+    string multiply(string n1, string n2) 
+    {
+        int l1=n1.length(),l2=n2.length();
+        vector<int> res(l1+l2,0);
+        string ans="";
+        int i,j;
+        
+        for(i=l2-1;i>=0;i--)
+        {
+            for(j=l1-1;j>=0;j--)
+            {
+                res[i+j+1]+=((n2[i]-'0')*(n1[j]-'0'));
+                res[i+j]+=res[i+j+1]/10;
+                res[i+j+1]%=10;
+            }
+        }
+        
+        for(i=0;i<l1+l2;i++)
+        {
+            if(res[i]!=0)
+                break;
+        }
+        
+        for(i;i<l1+l2;i++)
+            ans+=to_string(res[i]);
+        
+        if(ans.empty())
+            return "0";
+        
+        return ans;
+    }
+    
+    /*
     string add(string n1,string n2)
     {
         int i=n1.length()-1,j=n2.length()-1,c=0;
@@ -55,11 +88,11 @@ public:
             for(j=0;j<k;j++)
                 t.push_back('0');
             
-            // cout<<t<<endl;
             ans=add(ans,t);
             k++;
         }
         
         return ans;
     }
+    */
 };
