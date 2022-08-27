8efcd215f9e9f2c7454181b5c8bc5d1eec2d3c0c
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-27 14:04:01 +0530

    Time: 9 ms (54.59%), Space: 7.2 MB (77.11%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        string t="",ans="";
+        
+        for(char c:s)
+        {
+            if(c==' ')
+            {
+                if(!t.empty())
+                {
+                    reverse(t.begin(),t.end());
+                    ans+=t;
+                    ans+=" ";
+                }
+                t="";
+            }
+            else
+                t+=c;
+        }
+        
+        if(!t.empty())
+        {
+            reverse(t.begin(),t.end());
+            ans+=t;
+            ans+=" ";
+        }
+        
+        ans.pop_back();
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+    }
+    
+    /*
+    string reverseWords(string s) {
         stack<string> st;
         string t="",ans="";
         
@@ -29,4 +62,5 @@ public:
         ans.pop_back();
         return ans;
     }
+    */
 };
