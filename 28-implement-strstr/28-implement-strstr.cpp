e8e8d9585941d6fe4008bf304fc023c64df09fee
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-29 15:54:08 +0530

    Time: 4 ms (32.19%), Space: 6.3 MB (25.01%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,5 +1,61 @@
 class Solution {
 public:
+    
+    int strStr(string h, string n)
+    {
+        int i,j,l,r;
+        int nn=n.length(),nh=h.length(); 
+        
+        if(nn==0)
+            return 0;
+        
+        if(nh<nn)
+            return -1;
+        
+        string s=n+"."+h;
+        int ns=nn+nh+1;
+        int z[ns];
+        
+        z[0]=0;
+        l=r=0;
+        
+        for(i=1;i<ns;i++)
+        {
+            if(i>r)
+            {
+                l=r=i;
+                
+                while(r<ns && s[r]==s[r-l])
+                    r++;
+                
+                r--;
+                z[i]=r-l+1;
+            }
+            else
+            {
+                if(i+z[i-l]<=r)
+                    z[i]=z[i-l];
+                else
+                {
+                    l=i;
+                    
+                    while(r<ns && s[r]==s[r-l])
+                    r++;
+                
+                    r--;
+                    z[i]=r-l+1;
+                }
+            }
+            
+            if(z[i]==nn)
+                return i-nn-1;
+        }
+        
+        return -1;
+    }
+    
+    /*
+    //Rabin Karp
     bool comp(string h,string n,int in,int nn)
     {
         int i;
@@ -47,4 +103,5 @@ public:
         return -1;
         
     }
+    */
 };
