8576ced4dcd526f19854b9ed53c1189055c8fd25
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-29 20:25:06 +0530

    Time: 3 ms (93.42%), Space: 6.7 MB (43.32%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,62 +1,56 @@
 class Solution {
 public:
-    string countAndSay(int n)
-    {   
-        string s="1";
-        
-        while(--n)
-        {
-            string ans="";
-            int c=0;
-            char ch=s[0];
-            
-            for(char x:s)
-            {
-                if(x==ch)
-                    c++;
-                else
-                {
-                    ans+=(c+'0');
-                    ans+=ch;
-                
-                    ch=x;
-                    c=1;
-                }
-            }
-            ans+=(c+'0');
-            ans+=ch;
-            s=ans;
-        }
-        return s;
-    }
-    
-    /*
     string countAndSay(int n) {
-        
         if(n==1)
             return "1";
         
-        string s=countAndSay(n-1),ans="";
-        int c=0;
-        char ch=s[0];
-            
-        for(char x:s)
+        string s=countAndSay(n-1);
+        int ns=s.length();
+        int i,c=1;
+        string ans="";
+        
+        for(i=1;i<ns;i++)
         {
-            if(x==ch)
+            if(s[i]==s[i-1])
                 c++;
             else
             {
-                ans+=(c+'0');
-                ans+=ch;
-                
-                ch=x;
+                ans+=to_string(c);
+                ans+=s[i-1];
                 c=1;
             }
         }
-        ans+=(c+'0');
-        ans+=ch;
+        ans+=to_string(c);
+        ans+=s[i-1];
         
         return ans;
     }
-    */
+    
+//     string countAndSay(int n) {
+        
+//         if(n==1)
+//             return "1";
+        
+//         string s=countAndSay(n-1),ans="";
+//         int c=0;
+//         char ch=s[0];
+            
+//         for(char x:s)
+//         {
+//             if(x==ch)
+//                 c++;
+//             else
+//             {
+//                 ans+=(c+'0');
+//                 ans+=ch;
+                
+//                 ch=x;
+//                 c=1;
+//             }
+//         }
+//         ans+=(c+'0');
+//         ans+=ch;
+        
+//         return ans;
+//     }
 };
