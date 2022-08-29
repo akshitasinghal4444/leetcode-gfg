fd6ddb2fd77d7daba3cfa46a6ab0f6ba00476f09
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-29 14:14:31 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (90.20%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,167 +1,50 @@
 class Solution {
 public:
-    int strStr(string h, string n)
+    bool comp(string h,string n,int in,int nn)
     {
-        if(n=="")
-            return 0;
-        
-        int nh=h.length(),nn=n.length();
-        if(nn>nh)
-            return -1;
-        
-        string s=n+"."+h;
-        int len=nh+nn+1;
-        int z[len];
-        z[0]=0;
-        
-        int l=0,r=0;
         int i;
-        
-        for(i=1;i<len;i++)
+        for(i=0;i<nn;i++)
         {
-            if(i>r)
-            {
-                l=r=i;
-                
-                while(r<len && s[r]==s[r-l])
-                    r++;
-                
-                z[i]=r-l;
-                r--;
-            }
-            else
-            {
-                int j=i-l;
-                
-                if(z[j]+i<=r)
-                    z[i]=z[j];
-                else
-                {
-                    l=i;
-                    while(r<len && s[r]==s[r-l])
-                    r++;
-                
-                    z[i]=r-l;
-                    r--;
-                }
-            }
-            
-            if(z[i]==nn)
-                return i-nn-1;
-        }
-        
-        return -1;
-    }
-    
-    
-    /*
-    void createlps(string &n,int nn,int *lps)
-    {
-        int i,j;
-        i=-1;
-        j=1;
-        lps[0]=-1;
-        
-        while(j<nn)
-        {
-            if(n[j]==n[i+1])
-            {
-                lps[j]=i+1;
-                i++;
-                j++;
-            }
-            else if(i==-1)
-            {
-                lps[j]=-1;
-                j++;
-            }
-            else
-            {
-                i=lps[i];
-            }
-        }
-    }
-    
-    int strStr(string h, string n)
-    {
-        if(n=="")
-            return 0;
-        
-        int nh=h.length(),nn=n.length();
-        if(nn>nh)
-            return -1;
-        
-        int lps[nn];
-        createlps(n,nn,lps);
-        
-        int i,j=-1;
-        
-        for(i=0;i<nh;i++)
-        {
-            if(h[i]==n[j+1])
-            {
-                j++;
-            }
-            else if(j!=-1)
-            {
-                j=lps[j];
-                i--;
-            }
-            
-            if(j==nn-1)
-                return i-nn+1;
-        }
-
-        return -1;
-    }
-    */
-    
-    /*
-    bool comp(string &s1,string &s2,int in,int n)
-    {
-        for(int i=0;i<n;i++)
-        {
-            if(s1[in+i]!=s2[i])
+            if(n[i]!=h[i+in])
                 return 0;
         }
+        
         return 1;
     }
     
     int strStr(string h, string n) {
+        int i,nh=h.length(),nn=n.length();
+        int hh=0,hn=0;
+        int base=26,prime=101,mul=1;
         
-        if(n=="")
+        if(nn==0)
             return 0;
         
-        int nh=h.length(),nn=n.length();
-        if(nn>nh)
+        if(nh<nn)
             return -1;
         
-        int p=26,m=101;
-        int hh=0,hn=0;
-        int i,j,d=1;
-        
         for(i=0;i<nn-1;i++)
-            d=(d*p)%m;
+            mul=(mul*base)%prime;
         
         for(i=0;i<nn;i++)
         {
-            hh=((hh*p)%m+(h[i]))%m;
-            hn=((hn*p)%m+(n[i]))%m;
+            hh=((hh*base)%prime+h[i])%prime;
+            hn=((hn*base)%prime+n[i])%prime;            
         }
-                
+
         if(hh==hn && comp(h,n,0,nn))
             return 0;
         
         for(i;i<nh;i++)
-        {   
-            hh=(hh-(d*(h[i-nn]))%m)%m;
-            hh=(hh*p)%m;
-            hh=(hh+(h[i]))%m;
-
+        {
+            hh=(hh-(h[i-nn]*mul)%prime)%prime;
+            hh=((hh*base)%prime+h[i])%prime;
+            
             if(hh==hn && comp(h,n,i-nn+1,nn))
-            return i-nn+1;
+                return i-nn+1;
         }
+        
         return -1;
+        
     }
-    */
 };
