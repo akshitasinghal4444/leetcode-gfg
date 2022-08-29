4057a34cb598385e411788f2496182f0078d97b8
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-29 20:35:16 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (28.15%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     int compareVersion(string v1, string v2) {
-        
         int n1=v1.length(),n2=v2.length();
         int i=0,j=0;
-        string t1="0",t2="0";
         
         while(i<n1 || j<n2)
         {
+            string s1="0",s2="0";
+            int x1,x2;
+            
             while(i<n1 && v1[i]!='.')
-            {
-                t1+=v1[i];
-                i++;
-            }
+                s1+=v1[i++];
+            i++;
             
             while(j<n2 && v2[j]!='.')
-            {
-                t2+=v2[j];
-                j++;
-            }
-            
-            // if(t1=="")
-            //     t1="0";
-            // if(t2=="")
-            //     t2="0";
-            
-            if(stoi(t1)<stoi(t2))
-                return -1;
-            else if(stoi(t1)>stoi(t2))
-                return 1;
-            
-            t1="0";
-            t2="0";
-            i++; 
+                s2+=v2[j++];
             j++;
+            
+            x1=stoi(s1),x2=stoi(s2);
+            
+            if(x1<x2)
+                return -1;
+            else if(x2<x1)
+                return 1;
         }
+        
         return 0;
     }
 };
