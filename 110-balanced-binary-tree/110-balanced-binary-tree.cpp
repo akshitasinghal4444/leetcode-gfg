7797bc2f8280c283f04662c5998884c1201f10c8
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-30 13:45:27 +0530

    Time: 27 ms (29.01%), Space: 20.9 MB (80.91%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,103 +11,31 @@
  */
 class Solution {
 public:
+    bool bal;
+    
     int height(TreeNode* root)
     {
         if(!root)
             return 0;
         
-        int l,r;
-        l=height(root->left);
-        
-        if(l!=-1)
-        r=height(root->right);
-        
-        if(l==-1 || r==-1 || abs(l-r)>1)
+        if(!bal)
             return -1;
+                
+        int l=height(root->left);
+        int r=height(root->right);
         
-        return max(l,r)+1;
-    }
-    
-     bool isBalanced(TreeNode* root)
-     {
-         if(!root)
-             return 1;
-         
-         return height(root)!=-1;
-     }
-    
-    /*
-    bool bal;
-    
-    int check(TreeNode* root)
-    {
-        if(!root)
-            return 0;
-        
-        int l,r;
-        l=check(root->left);
-        r=check(root->right);
-        
-        if(abs(l-r)>1)
+        if(abs(r-l)>1)
             bal=0;
         
-        return max(l,r)+1;
+        return 1+max(l,r);
     }
     
-    bool isBalanced(TreeNode* root)
-    {
+    bool isBalanced(TreeNode* root) {
         if(!root)
             return 1;
         
         bal=1;
-        
-        int h=check(root);
-        
+        int x=height(root);
         return bal;
     }
-    */
-    
-    /*
-    unordered_map<TreeNode*,int> m;
-    
-    int height(TreeNode* root)
-    {
-        if(!root)
-            return 0;
-        
-        if(m.find(root)==m.end())
-            m[root]=max(height(root->left),height(root->right))+1;
-        
-        return m[root];
-    }
-    
-    bool isBalanced(TreeNode* root) {
-        
-        if(!root)
-            return 1;
-        
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        while(!q.empty())
-        {
-            root=q.front();
-            q.pop();
-            
-            if(root->left)
-                q.push(root->left);
-            if(root->right)
-                q.push(root->right);
-            
-            int l=height(root->left);
-            int r=height(root->right);
-            
-            if(abs(l-r)<=1)
-                continue;
-            else
-                return 0;   
-        }
-        return 1;
-    }
-    */
 };
