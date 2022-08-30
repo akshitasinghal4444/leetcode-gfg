903fd6b607e4db1e1eeceb5bd9d17a9db00af5ba
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-30 16:23:21 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (35.72%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,125 +11,21 @@
  */
 class Solution {
 public:
-    vector<int> postorderTraversal(TreeNode* root)
-    {
-        if(!root)
-            return {};
-        
-        stack<TreeNode*> s1,s2;
-        vector<int> ans;
-        
-        s1.push(root);
-        
-        while(!s1.empty())
-        {
-            root=s1.top();
-            s1.pop();
-            
-            if(root->left)
-                s1.push(root->left);
-            if(root->right)
-                s1.push(root->right);
-            
-            s2.push(root);
-        }
-        
-        while(!s2.empty())
-        {
-            root=s2.top();
-            s2.pop();
-            ans.push_back(root->val);
-        }
-        return ans;
-    }
-    
-    /*
-    vector<int> postorderTraversal(TreeNode* root)
-    {
-        if(!root)
-            return {};
-        
-        vector<int> ans;
-        stack<TreeNode*> s;
-        s.push(root);
-        
-        while(!s.empty())
-        {
-            TreeNode *n=s.top();
-            
-            if(n==NULL)
-            {
-                s.pop();
-                ans.push_back(s.top()->val);
-                s.pop();
-            }
-            else
-            {
-                s.push(NULL);
-                
-                if(n->right)
-                    s.push(n->right);
-                
-                if(n->left)
-                    s.push(n->left);
-            }
-        }
-        return ans;
-    }
-    */
-    
-    /*
-    vector<int> postorderTraversal(TreeNode* root)
-    {
-        if(!root)
-            return {};
-        
-        vector<int> ans;
-        stack<TreeNode*> s1,s2;
-        
-        s1.push(root);
-        
-        while(!s1.empty())
-        {
-            TreeNode *n=s1.top();
-            s1.pop();
-            s2.push(n);
-            
-            if(n->left)
-                s1.push(n->left);
-            
-            if(n->right)
-                s1.push(n->right);
-        }
-        
-        while(!s2.empty())
-        {
-            TreeNode *n=s2.top();
-            s2.pop();
-            ans.push_back(n->val);
-        }
-        
-        return ans;
-    }
-    */
-    
-    /*
     vector<int> ans;
     
     void postorder(TreeNode* root)
     {
         if(!root)
-            return ;
+            return;
         
         postorder(root->left);
         postorder(root->right);
+        
         ans.push_back(root->val);
     }
     
     vector<int> postorderTraversal(TreeNode* root) {
-        ans={};
         postorder(root);
         return ans;
     }
-    */
 };
