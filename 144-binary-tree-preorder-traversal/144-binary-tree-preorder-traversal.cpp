dfc2e099a2721b0d0202d067b7ecd1124d9561ac
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-30 15:52:55 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (13.59%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -11,77 +11,12 @@
  */
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root)
-    {
-        if(!root)
-            return {};
-        
-        TreeNode *curr=root;
-        vector<int> ans;
-    
-        while(curr)
-        {
-            if(!curr->left)
-            {
-                ans.push_back(curr->val);
-                curr=curr->right;
-            }
-            else
-            {
-                TreeNode *t=curr->left;
-                
-                while(t->right && t->right!=curr)
-                    t=t->right;
-                
-                if(!t->right)
-                {
-                    ans.push_back(curr->val);
-                    t->right=curr;
-                    curr=curr->left;
-                }
-                else
-                {
-                    t->right=NULL;
-                    curr=curr->right;
-                }
-            }
-        }
-        return ans;
-    }
-    
-    /*
-    vector<int> preorderTraversal(TreeNode* root)
-    {
-        if(!root)
-            return {};
-        
-        vector<int> ans;
-        stack<TreeNode*> s;
-        s.push(root);
-        
-        while(!s.empty())
-        {
-            TreeNode *n=s.top();
-            s.pop();
-            ans.push_back(n->val);
-            
-            if(n->right)
-                s.push(n->right);
-            if(n->left)
-                s.push(n->left);
-        }
-        
-        return ans;
-    }
-    */
-    
-    /*
     vector<int> ans;
     
     void preorder(TreeNode* root)
     {
         if(!root)
-            return ;
+            return;
         
         ans.push_back(root->val);
         preorder(root->left);
@@ -89,9 +24,7 @@ public:
     }
     
     vector<int> preorderTraversal(TreeNode* root) {
-        ans={};
         preorder(root);
         return ans;
     }
-    */
 };
