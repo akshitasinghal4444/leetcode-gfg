91e1505015c397e2a620fc2d61db80d28568ad3c
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-08-30 18:44:34 +0530

    Time: 3 ms (93.83%), Space: 12.1 MB (88.54%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,78 +11,52 @@
  */
 class Solution {
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root)
-    {
+    
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
         if(!root)
             return {};
         
-        map<int,vector<pair<int,int>>> m;       // vertical index : (level,value)
-        queue<pair<TreeNode*,int>> q;           // node,vertical index
-        
-        int l=0;
+        vector<vector<int>> ans;
+        map<int,vector<pair<int,int>>> m;
+        queue<pair<TreeNode* ,int>> q;
+        int level=0;
         
         q.push({root,0});
         
         while(!q.empty())
         {
             int s=q.size();
-            l++;
             
             while(s--)
             {
                 root=q.front().first;
-                int i=q.front().second;
-                q.pop();
+            int in=q.front().second;
+            q.pop();
             
-                m[i].push_back({l,root->val});
+            m[in].push_back({level,root->val});
             
-                if(root->left)
-                    q.push({root->left,i-1});
-                
-                if(root->right)
-                    q.push({root->right,i+1});
+            if(root->left)
+                q.push({root->left,in-1});
+            
+            if(root->right)
+                q.push({root->right,in+1});
             }
-            
+                            
+            level++;
         }
-        vector<vector<int>> ans;
         
-        for(auto i:m)
+        for(auto it:m)
         {
-            sort(i.second.begin(),i.second.end());
+            sort(it.second.begin(),it.second.end());
             vector<int> t;
             
-            for(auto x:i.second)
-            t.push_back(x.second);
+            for(auto p:it.second)
+            t.push_back(p.second);
             
             ans.push_back(t);
         }
+            
         
         return ans;
     }
-    
-    /*
-    void level(TreeNode* root,int x,int y,map<int,multiset<pair<int,int>>> &m)
-    {
-        if(!root)
-            return;
-        m[y].insert({x,root->val});
-        level(root->left,x+1,y-1,m);
-        level(root->right,x+1,y+1,m);
-    }
-    
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>> ans;
-        map<int,multiset<pair<int,int>>> m;
-        level(root,0,0,m);
-        for(auto it=m.begin();it!=m.end();it++)
-        {
-            vector<int> t;
-            for(auto x : it->second)
-                t.push_back(x.second);
-            ans.push_back(t);
-        }
-            
-        return ans;
-    }
-    */
 };
