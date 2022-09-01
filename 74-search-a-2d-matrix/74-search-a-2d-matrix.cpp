146bd1b9b67bc3d7992b94f390345cebc2b2e19a
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-01 11:55:26 +0530

    Time: 14 ms (7.40%), Space: 9.6 MB (51.39%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,5 +1,52 @@
 class Solution {
 public:
+    bool searchMatrix(vector<vector<int>>& a, int t)
+    {
+        int m=a.size(),n=a[0].size();
+        int l,r,i,j,mid;
+        
+        l=0,r=m-1;
+        
+        while(l<=r)
+        {
+            if(t<a[l][0] || t>a[r][n-1])
+                return 0;
+            
+            mid=(l+r)/2;
+            
+            if(t>=a[mid][0] && t<=a[mid][n-1])
+                break;
+            
+            if(t<a[mid][0])
+                r=mid-1;
+            else
+                l=mid+1;
+        }
+        
+        if(l>r)
+            return 0;
+        
+        i=mid;
+        l=0,r=n-1;
+        // cout<<l<<" "<<r<<endl;
+        while(l<=r)
+        {
+            mid=(l+r)/2;
+            
+            // cout<<mid<<endl;
+            if(t==a[i][mid])
+                return 1;
+            
+            if(t>a[i][mid])
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+        
+        return 0;
+    }
+    
+    /*
     bool searchMatrix(vector<vector<int>>& a, int t) {
         
         int m=a.size(),n=a[0].size();
@@ -18,4 +65,5 @@ public:
         
         return 0;
     }
+    */
 };
