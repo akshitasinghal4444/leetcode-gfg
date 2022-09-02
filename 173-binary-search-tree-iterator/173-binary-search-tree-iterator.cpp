192ba235383948c8c7143e7d2c0ddfdd5f500679
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-02 13:15:11 +0530

    Time: 34 ms (83.54%), Space: 24.3 MB (22.66%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -11,79 +11,37 @@
  */
 class BSTIterator {
 public:
-    TreeNode *head,*t; 
     
-    void convert(TreeNode* root)
+    vector<int> a;
+    int i,n;
+    
+    void inorder(TreeNode* root)
     {
         if(!root)
             return;
         
-        convert(root->left);
-        
-        t->right=root;
-        t=t->right;
-        
-        convert(root->right);
+        inorder(root->left);
+        a.push_back(root->val);
+        inorder(root->right);
     }
     
     BSTIterator(TreeNode* root) {
-        
-        head=new TreeNode(0);
-        t=head;
-        convert(root);
+        inorder(root);
+        i=-1;
+        n=a.size();
     }
     
     int next() {
-        head=head->right;
-        return head->val;
+        if(++i>=n)
+            return -1;
+        
+        return a[i];
     }
     
     bool hasNext() {
-        return (head->right!=NULL);
+        return (i+1<n);
     }
 };
-
-
-/*
-class BSTIterator {
-public:
-    stack<TreeNode*> st;
-    
-    BSTIterator(TreeNode* root) {
-        st.push(root);
-        
-        while(root->left)
-        {
-            st.push(root->left);
-            root=root->left;
-        }
-    }
-    
-    int next() {
-        TreeNode *root=st.top();
-        st.pop();
-        
-        int ans=root->val;
-        
-        if(root->right)
-        {
-            root=root->right;
-            st.push(root);
-            while(root->left)
-            {
-                st.push(root->left);
-                root=root->left;
-            }
-        }
-        
-        return ans;
-    }
-    
-    bool hasNext() {
-        return !st.empty();
-    }
-};
-*/
 
 /**
  * Your BSTIterator object will be instantiated and called as such:
