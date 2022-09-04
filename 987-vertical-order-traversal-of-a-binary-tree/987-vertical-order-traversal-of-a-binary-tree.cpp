58d42651536e864af92de512023aae8bd6b90aa0
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-04 11:26:51 +0530

    Time: 3 ms (93.93%), Space: 14.6 MB (13.71%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,7 +11,41 @@
  */
 class Solution {
 public:
+    map<int,multiset<pair<int,int>>> m;
     
+    void traverse(TreeNode* root,int col,int level)
+    {
+        if(!root)
+            return;
+        
+        m[col].insert({level,root->val});
+        
+        traverse(root->left,col-1,level+1);
+        traverse(root->right,col+1,level+1);
+    }
+    
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        if(!root)
+            return {};
+        
+        vector<vector<int>> ans;             
+        traverse(root,0,0);
+        
+        for(auto it:m)
+        {
+            vector<int> t;
+            
+            for(auto p:it.second)
+            t.push_back(p.second);
+            
+            ans.push_back(t);
+        }
+            
+        
+        return ans;
+    }
+    
+    /*
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         if(!root)
             return {};
@@ -30,17 +64,17 @@ public:
             while(s--)
             {
                 root=q.front().first;
-            int in=q.front().second;
-            q.pop();
+                int in=q.front().second;
+                q.pop();
             
-            m[in].push_back({level,root->val});
+                m[in].push_back({level,root->val});
             
-            if(root->left)
-                q.push({root->left,in-1});
+                if(root->left)
+                    q.push({root->left,in-1});
             
-            if(root->right)
-                q.push({root->right,in+1});
-            }
+                if(root->right)
+                    q.push({root->right,in+1});
+             }
                             
             level++;
         }
@@ -59,4 +93,5 @@ public:
         
         return ans;
     }
+    */
 };
