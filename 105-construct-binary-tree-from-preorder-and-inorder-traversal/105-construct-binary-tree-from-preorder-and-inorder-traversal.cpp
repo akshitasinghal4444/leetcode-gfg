fe425f5ffa22f58fd2267736a9aed9ae07642992
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-04 18:37:36 +0530

    Time: 60 ms (26.42%), Space: 26 MB (77.18%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,58 +11,37 @@
  */
 class Solution {
 public:
-    unordered_map<int,int> m;
-    int i;
+    int j;
     
-    TreeNode* buildTree(vector<int>& pre,vector<int>& in,int l,int r)
+    int find(vector<int> &in,int n,int l,int r)
+    {
+        while(l<=r)
+        {
+            if(in[l]==n)
+                return l;
+            
+            l++;
+        }
+        
+        return -1;
+    }
+    
+    TreeNode* build(vector<int>& pre, vector<int>& in,int l,int r)
     {
         if(l>r)
             return NULL;
         
-        TreeNode *root=new TreeNode(pre[i]);
-        int mid=m[pre[i++]];
+        TreeNode *root=new TreeNode(pre[j]);
+        int mid=find(in,pre[j++],l,r);
         
-        root->left=buildTree(pre,in,l,mid-1);
-        root->right=buildTree(pre,in,mid+1,r);
-        
-        return root;
-    }
-    
-    
-    TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
-        
-        int n=in.size();
-        for(int i=0;i<n;i++)
-            m[in[i]]=i;
-        
-        i=0;
-        return buildTree(pre,in,0,n-1);
-    }
-    
-    /*
-    unordered_map<int,int> m;
-    TreeNode* buildTree(vector<int>& pre,int l1,int r1,vector<int>& in,int l2,int r2)
-    {
-        if(l1>r1)
-            return NULL;
-        
-        TreeNode *root=new TreeNode(pre[l1]);
-        int i=m[pre[l1]]-l2;
-        
-        root->left=buildTree(pre,l1+1,l1+i,in,l2,l2+i-1);
-        root->right=buildTree(pre,l1+i+1,r1,in,l2+i+1,r2);
+        root->left=build(pre,in,l,mid-1);
+        root->right=build(pre,in,mid+1,r);
         
         return root;
     }
     
-    
     TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
-        
-        int n=in.size();
-        for(int i=0;i<n;i++)
-            m[in[i]]=i;
-        
-        return buildTree(pre,0,pre.size()-1,in,0,n-1);
+        j=0;
+        return build(pre,in,0,in.size()-1);        
     }
-    */
 };
