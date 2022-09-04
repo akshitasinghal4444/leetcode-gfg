acc8a681d0532eec7414032f9478f4fb269f294c
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-04 18:39:52 +0530

    Time: 31 ms (63.90%), Space: 26.3 MB (53.39%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,6 +12,34 @@
 class Solution {
 public:
     int j;
+    unordered_map<int,int> m;
+    
+    TreeNode* build(vector<int>& pre, vector<int>& in,int l,int r)
+    {
+        if(l>r)
+            return NULL;
+        
+        TreeNode *root=new TreeNode(pre[j]);
+        int mid=m[pre[j++]];
+        
+        root->left=build(pre,in,l,mid-1);
+        root->right=build(pre,in,mid+1,r);
+        
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
+        j=0;
+        int i,n=in.size();
+        
+        for(i=0;i<n;i++)
+            m[in[i]]=i;
+        
+        return build(pre,in,0,n-1);        
+    }
+    
+    /*
+    int j;
     
     int find(vector<int> &in,int n,int l,int r)
     {
@@ -44,4 +72,5 @@ public:
         j=0;
         return build(pre,in,0,in.size()-1);        
     }
+    */
 };
