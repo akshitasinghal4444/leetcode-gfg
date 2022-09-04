2a2b6047ec2832b5abad01af8432a7ab3388b4c7
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-04 16:55:36 +0530

    Time: 47 ms (23.80%), Space: 13.8 MB (69.78%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,14 +1,53 @@
 class Solution {
 public:
+    vector<int> findOrder(int n, vector<vector<int>>& a)
+    {
+        vector<int> g[n];
+        vector<int> indegree(n,0);
+        vector<int> ans;
+        int i,j;
+        
+        for(auto v:a)
+        {
+            g[v[1]].push_back(v[0]);
+            indegree[v[0]]++;
+        }
+        
+        for(i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+                ans.push_back(i);
+        }
+        
+        j=0;
+        while(j<ans.size())
+        {
+            for(int x:g[ans[j]])
+            {
+                indegree[x]--;
+                if(indegree[x]==0)
+                    ans.push_back(x);
+            }
+            
+            j++;
+        }
+        
+        if(ans.size()==n)
+            return ans;
+        
+        return {};
+    }
+    
+    /*
     vector<int> ans;
     
-    bool dfs(vector<int> *g,int n,vector<int> &vis,int in)
+    bool topo_sort(vector<int> *g,int n,vector<int> &vis,int in)
     {
         vis[in]=1;
         
         for(int x:g[in])
         {
-            if(!vis[x] && !dfs(g,n,vis,x))
+            if(!vis[x] && !topo_sort(g,n,vis,x))
                 return 0;
             
             if(vis[x]==1)
@@ -30,11 +69,12 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            if(!vis[i] && !dfs(g,n,vis,i))
+            if(!vis[i] && !topo_sort(g,n,vis,i))
                 return {};
         }
         
         reverse(ans.begin(),ans.end());
         return ans;
     }
+    */
 };
