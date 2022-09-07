9065a1efaff11098b97002af3c2f7b9961a3f20f
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-07 17:04:10 +0530

    Time: 15 ms (83.48%), Space: 7.5 MB (98.40%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    string minWindow(string s, string t)
-    {
+    string minWindow(string s, string t) {
         int ns=s.length(),nt=t.length();
-        int c=0,beg=0;
-        int mn=INT_MAX,in=-1;        
-        int i;
         int f[256]={0};
+        int i,j=-1;
+        int st=0;
+        int len=INT_MAX,c=0;
         
         for(i=0;i<nt;i++)
             f[t[i]]++;
@@ -16,115 +15,28 @@ public:
             f[s[i]]--;
             
             if(f[s[i]]>=0)
-                c++;
+            c++;
             
-            while(c==nt && beg<=i)
+            while(c==nt && st<=i)
             {
-                if(i-beg+1<mn)
+                if(i-st+1<len)
                 {
-                    mn=i-beg+1;
-                    in=beg;
+                    len=i-st+1;
+                    j=st;
                 }
                 
-                f[s[beg]]++;
-                if(f[s[beg]]>0)
+                f[s[st]]++;
+                
+                if(f[s[st]]>0)
                     c--;
                 
-                beg++;
+                st++;
             }
         }
         
-        if(mn==INT_MAX)
+        if(len==INT_MAX)
             return "";
         
-        return s.substr(in,mn);
+        return s.substr(j,len);
     }
-    
-    /*
-    string minWindow(string s, string t)
-    {
-        int ns=s.length(),nt=t.length();
-        int c=0,beg=0;
-        int mn=INT_MAX,in=-1;        
-        int i;
-        unordered_map<char,int> m;
-        
-        for(i=0;i<nt;i++)
-            m[t[i]]++;
-        
-        for(i=0;i<ns;i++)
-        {
-            m[s[i]]--;
-            
-            if(m[s[i]]>=0)
-                c++;
-            
-            while(c==nt && beg<=i)
-            {
-                if(i-beg+1<mn)
-                {
-                    mn=i-beg+1;
-                    in=beg;
-                }
-                
-                m[s[beg]]++;
-                if(m[s[beg]]>0)
-                    c--;
-                
-                beg++;
-            }
-        }
-        
-        if(mn==INT_MAX)
-            return "";
-        
-        return s.substr(in,mn);
-    }
-    */
-    
-    /*
-    string minWindow(string s, string t) {
-        
-        int beg=0,mn=INT_MAX;
-        int c=0,in=-1;
-        int ns=s.length(),nt=t.length();
-        unordered_map<char,int> m1,m2;
-        int i;
-        
-        for(i=0;i<nt;i++)
-            m1[t[i]]++;
-        
-        
-        for(i=0;i<ns;i++)
-        {
-            if(c<nt)
-            {
-                m2[s[i]]++;
-                
-                if(m1.find(s[i])!=m1.end() && m2[s[i]]<=m1[s[i]])
-                    c++;
-            }
-            
-            while(c==nt && beg<=i)
-            {
-                if(i-beg+1<mn)
-                {
-                    mn=i-beg+1;
-                    in=beg;
-                }
-                m2[s[beg]]--;
-                
-                if(m1.find(s[beg])!=m1.end() && m2[s[beg]]<m1[s[beg]])
-                    c--;
-                
-                beg++;
-            }
-        }
-        
-        if(mn==INT_MAX)
-            return "";
-        
-        return s.substr(in,mn);
-    }
-    */
 };
