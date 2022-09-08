8a3142e96761a570fa24c6fc6638146eaa55ff27
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-08 13:44:45 +0530

    Time: 47 ms (26.10%), Space: 14.3 MB (39.55%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,5 +1,43 @@
 class Solution {
 public:
+    bool topo_sort(vector<int> *g,int n,vector<int> &vis,int in)
+    {
+        if(vis[in]==1)
+            return 0;
+        
+        if(vis[in]==2)
+            return 1;
+        
+        vis[in]=1;
+        
+        for(auto x:g[in])
+        {
+            if(!topo_sort(g,n,vis,x))
+                return 0;
+        }
+        
+        vis[in]=2;
+        return 1;
+    }
+    
+    bool canFinish(int n, vector<vector<int>>& a)
+    {
+        vector<int> vis(n,0);
+        vector<int> g[n];
+        
+        for(auto v:a)
+            g[v[0]].push_back(v[1]);
+        
+        for(int i=0;i<n;i++)
+        {
+            if(!topo_sort(g,n,vis,i))
+                return 0;
+        }
+        
+        return 1;
+    }
+    
+    /*
     bool canFinish(int n, vector<vector<int>>& a) {
         vector<int> g[n];
         vector<int> in(n,0);
@@ -34,4 +72,5 @@ public:
         
         return c==n;
     }
+    */
 };
