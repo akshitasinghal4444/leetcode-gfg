412fa9939c2e85719dbae0de7b44cd95a5f4eb14
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-08 20:10:54 +0530

    Time: 26 ms (91.35%), Space: 13.5 MB (84.60%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,73 +1,37 @@
 class Solution {
 public:
     
-    bool dfs(vector<vector<int>>& g,int in,int n,vector<int> &color,int c)
+    bool color(vector<vector<int>>& g,int n,int in,int c,vector<int> &col)
     {
-        color[in]=c;
-
-        for(int i:g[in])
+        if(col[in]!=0)
         {
-            if((color[i]==color[in]) || (color[i]==0 && !dfs(g,i,n,color,-c)))
-                return 0;      
-        }
-        
-        return 1;
-    }
-    
-    bool isBipartite(vector<vector<int>>& g)
-    {
-        int i,n=g.size();
-        vector<int> color(n,0);
-        
-        for(i=0;i<n;i++)
-        {
-            if(color[i]==0 && !dfs(g,i,n,color,1))
+            if(col[in]!=c)
                 return 0;
-        }
-        
-        return 1;
-    }
-    
-    /*
-    bool bfs(vector<vector<int>>& g,int in,int n,vector<int> &color)
-    {
-        color[in]=1;
-        queue<int> q;
-        q.push(in);
-        
-        while(!q.empty())
-        {
-            in=q.front();
-            q.pop();
             
-            for(int i:g[in])
-            {
-                if(color[i]==color[in])
-                    return 0;
+            return 1;
+        }
                 
-                if(color[i]==0)
-                {
-                    color[i]=-color[in];
-                    q.push(i);
-                }
-            }
+        col[in]=c;
+                
+        for(int x:g[in])
+        {
+            if(!color(g,n,x,-c,col))
+                return 0;         
         }
         
         return 1;
     }
     
-    bool isBipartite(vector<vector<int>>& g)
-    {
-        int i,n=g.size();
-        vector<int> color(n,0);
+    bool isBipartite(vector<vector<int>>& g) {
+        int n=g.size();
+        vector<int> col(n,0);
         
-        for(i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            if(color[i]==0 && !bfs(g,i,n,color))
+            if(!col[i] && !color(g,n,i,1,col))
                 return 0;
         }
         
         return 1;
     }
-    */
 };
