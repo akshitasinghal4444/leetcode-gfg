7c8b7c4a6216a3037b863c27fd5db24450421b56
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-08 13:39:09 +0530

    Time: 57 ms (13.22%), Space: 13.7 MB (69.17%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,80 +1,37 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& a)
-    {
+    bool canFinish(int n, vector<vector<int>>& a) {
         vector<int> g[n];
-        vector<int> indegree(n,0);
-        int i,l=a.size();
-        
-        for(i=0;i<l;i++)
-        {
-            g[a[i][1]].push_back(a[i][0]);
-            indegree[a[i][0]]++;
-        }
-            
-        int c=0;
+        vector<int> in(n,0);
         queue<int> q;
+        int i,c=0;
+        
+        for(auto v:a)
+        {
+            in[v[1]]++;
+            g[v[0]].push_back(v[1]);
+        }
         
         for(i=0;i<n;i++)
         {
-            if(indegree[i]==0)
+            if(in[i]==0)
                 q.push(i);
         }
         
         while(!q.empty())
         {
-            int in=q.front();
+            i=q.front();
             q.pop();
             c++;
             
-            l=g[in].size();
-            
-            for(i=0;i<l;i++)
+            for(int x:g[i])
             {
-                indegree[g[in][i]]--;
-                
-                if(indegree[g[in][i]]==0)
-                    q.push(g[in][i]);
+                in[x]--;
+                if(in[x]==0)
+                    q.push(x);
             }
         }
         
         return c==n;
     }
-    
-    /*
-    bool topo_sort(vector<int> *g,int in,vector<int> &vis)
-    {
-        int n=g[in].size();
-        vis[in]=1;
-        
-        for(int i=0;i<n;i++)
-        {
-            if(vis[g[in][i]]==1)
-                return 0;
-            
-            if(vis[g[in][i]]==0 && !topo_sort(g,g[in][i],vis))
-                return 0;
-        }
-        
-        vis[in]=2;
-        return 1;
-    }
-    
-    bool canFinish(int n, vector<vector<int>>& a) {
-        vector<int> g[n];
-        vector<int> vis(n,0);
-        int i;
-        
-        for(i=0;i<a.size();i++)
-            g[a[i][1]].push_back(a[i][0]);
-        
-        for(i=0;i<n;i++)
-        {
-            if(vis[i]!=2 && !topo_sort(g,i,vis))
-                return 0;
-        }
-            
-        return 1;
-    }
-    */
 };
