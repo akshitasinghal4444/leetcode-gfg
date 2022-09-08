c5516e637d40bd164e7977f4346c0f43afbde992
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-08 20:17:04 +0530

    Time: 45 ms (41.43%), Space: 13.7 MB (29.54%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,7 +1,50 @@
 class Solution {
 public:
     
-    bool color(vector<vector<int>>& g,int n,int in,int c,vector<int> &col)
+    bool bfs(vector<vector<int>>& g,int n,int in,int c,vector<int> &col)
+    {
+        queue<int> q;
+        q.push(in);
+        col[in]=c;
+        
+        while(!q.empty())
+        {
+            in=q.front();
+            q.pop();
+            
+            for(int x:g[in])
+            {
+                if(col[x])
+                {
+                    if(col[x]==col[in])
+                        return 0;
+                }
+                else
+                {
+                    col[x]=-col[in];
+                    q.push(x);
+                }
+            }
+        }
+        
+        return 1;
+    }
+    
+    bool isBipartite(vector<vector<int>>& g) {
+        int n=g.size();
+        vector<int> col(n,0);
+        
+        for(int i=0;i<n;i++)
+        {
+            if(!col[i] && !bfs(g,n,i,1,col))
+                return 0;
+        }
+        
+        return 1;
+    }
+    
+    /*
+    bool dfs(vector<vector<int>>& g,int n,int in,int c,vector<int> &col)
     {
         if(col[in]!=0)
         {
@@ -15,7 +58,7 @@ public:
                 
         for(int x:g[in])
         {
-            if(!color(g,n,x,-c,col))
+            if(!dfs(g,n,x,-c,col))
                 return 0;         
         }
         
@@ -28,10 +71,11 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            if(!col[i] && !color(g,n,i,1,col))
+            if(!col[i] && !dfs(g,n,i,1,col))
                 return 0;
         }
         
         return 1;
     }
+    */
 };
