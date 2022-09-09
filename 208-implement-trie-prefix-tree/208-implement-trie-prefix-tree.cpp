57c213678b43ab0022bc8e65847d6c2d28e067b1
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-09 12:57:35 +0530

    Time: 129 ms (29.34%), Space: 45 MB (50.42%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -1,116 +1,58 @@
 class Trie {
 public:
-    bool end;
-    Trie *next[26];
+    struct trie
+    {
+        bool end;
+        trie *next[26];
+        
+    }*root;
     
     Trie() {
-        this->end=0;
-        for(int i=0;i<26;i++)
-            this->next[i]=NULL;
+        root=new trie();
     }
     
     void insert(string word) {
-        Trie *t=this;
+        trie *t=root;
         
         for(char c:word)
         {
-            int in=c-'a';
+            if(!t->next[c-'a'])
+                t->next[c-'a']=new trie();
             
-            if(!t->next[in])
-                t->next[in]=new Trie();
-            t=t->next[in];
+            t=t->next[c-'a'];
         }
+        
         t->end=1;
     }
     
     bool search(string word) {
-        Trie *t=this;
+        trie *t=root;
         
         for(char c:word)
         {
-            int in=c-'a';
-            
-            if(!t->next[in])
+            if(!t->next[c-'a'])
                 return 0;
-            t=t->next[in];
+            
+            t=t->next[c-'a'];
         }
-        return t->end;
+        
+        return t->end==1;
     }
     
     bool startsWith(string prefix) {
-        Trie *t=this;
+        trie *t=root;
         
         for(char c:prefix)
         {
-            int in=c-'a';
-            
-            if(!t->next[in])
+            if(!t->next[c-'a'])
                 return 0;
-            t=t->next[in];
+            
+            t=t->next[c-'a'];
         }
+        
         return 1;
     }
 };
-
-/*
-class Trie {
-public:
-
-    bool end;
-    Trie *next[26];
-    
-    Trie() {
-        Trie *t=this;
-        t->end=false;
-        for(int i=0;i<26;i++)
-            t->next[i]=NULL;
-    }
-    
-    void insert(string w) {
-        int n=w.length();
-        Trie *t=this;
-        for(int i=0;i<n;i++)
-        {
-            int in=w[i]-'a';
-            if(!t->next[in])
-            {
-                t->next[in]=new Trie();
-            }
-                
-            t=t->next[in];
-        }
-        t->end=1;
-    }
-    
-    bool search(string w) {
-        int n=w.length();
-        Trie *t=this;
-        for(int i=0;i<n;i++)
-        {
-            int in=w[i]-'a';
-            if(!t->next[in])
-                return false;
-            t=t->next[in];
-        }
-        if(t->end)
-            return true;
-        return false;
-    }
-    
-    bool startsWith(string w) {
-        int n=w.length();
-        Trie *t=this;
-        for(int i=0;i<n;i++)
-        {
-            int in=w[i]-'a';
-            if(!t->next[in])
-                return false;
-            t=t->next[in];
-        }
-        return true;
-    }
-};
-*/
 
 /**
  * Your Trie object will be instantiated and called as such:
