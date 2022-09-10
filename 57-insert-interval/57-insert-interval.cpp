85bacb70574a2d54589349d6d551ef75bad6e621
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-10 12:48:49 +0530

    Time: 34 ms (30.53%), Space: 17.1 MB (74.86%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -4,6 +4,54 @@ public:
         int i,n=a.size();
         vector<vector<int>> ans;
         int st,end;
+                        
+        for(i=0;i<n;i++)
+        {
+            if(a[i][1]<x[0])
+                ans.push_back(a[i]);
+            else
+                break;
+        }
+        
+        if(i>=n)
+        {
+            ans.push_back(x);
+            return ans;
+        }
+            
+        if(x[0]<=a[i][0])
+        {
+            st=x[0];
+            end=x[1];
+        }
+        else
+        {
+            st=a[i][0];
+            end=a[i][1];
+            a[i]=x;
+        }
+        
+        for(i;i<n;i++)
+        {
+            if(a[i][0]>end)
+            {
+                ans.push_back({st,end});
+                st=a[i][0];
+                end=a[i][1];
+            }
+            else
+                end=max(end,a[i][1]);
+        }
+        ans.push_back({st,end});
+        
+        return ans;
+    }
+    
+    /*
+    vector<vector<int>> insert(vector<vector<int>>& a, vector<int>& x) {
+        int i,n=a.size();
+        vector<vector<int>> ans;
+        int st,end;
         
         a.push_back(x);
         sort(a.begin(),a.end());
@@ -26,4 +74,5 @@ public:
         
         return ans;
     }
+    */
 };
