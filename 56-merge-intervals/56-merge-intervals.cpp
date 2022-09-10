fc5efae4893b5d47f80ea8546159b66cbae39988
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-10 13:04:04 +0530

    Time: 46 ms (80.98%), Space: 20 MB (32.23%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& a) {
-        sort(a.begin(),a.end());
-        
-        int s=a[0][0],e=a[0][1];
+        int st,end;
         vector<vector<int>> ans;
+        
+        sort(a.begin(),a.end());
+        st=a[0][0],end=a[0][1];
         
         for(auto v:a)
         {
-            if(v[0]>e)
+            if(v[0]>end)
             {
-                ans.push_back({s,e});
-                s=v[0];
-                e=v[1];
+                ans.push_back({st,end});
+                st=v[0];
+                end=v[1];
             }
             else
-                e=max(e,v[1]);
-        
+                end=max(end,v[1]);
         }
-        ans.push_back({s,e});
+        
+        ans.push_back({st,end});
+        
         return ans;
     }
 };
