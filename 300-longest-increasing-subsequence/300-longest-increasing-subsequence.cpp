a591faf4b059a96aaacba61fc080f8ffda35f337
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-10 15:46:04 +0530

    Time: 363 ms (52.40%), Space: 10.3 MB (97.89%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& a)
-    {
-        int i,n=a.size();
-        vector<int> dp;
-        
-        for(i=0;i<n;i++)
-        {
-            if(dp.size()==0 || a[i]>dp.back())
-                dp.push_back(a[i]);
-            else
-            {
-                int j=lower_bound(dp.begin(),dp.end(),a[i])-dp.begin();
-                dp[j]=a[i];
-            }
-        }
-        
-        return dp.size();
-    }
-    
-    /*
     int lengthOfLIS(vector<int>& a) {
         int n=a.size();
         int i,j,ans=1;
-        vector<int> dp(n,0);
-        dp[0]=1;
+        int dp[n];
         
-        for(i=1;i<n;i++)
+        for(i=0;i<n;i++)
         {
+            int mx=0;
             for(j=i-1;j>=0;j--)
             {
-                if(a[i]>a[j])
-                    dp[i]=max(dp[i],dp[j]);
+                if(a[j]<a[i])
+                    mx=max(mx,dp[j]);
             }
-            
-            dp[i]++;
+                    
+            dp[i]=mx+1;
             ans=max(ans,dp[i]);
         }
         
         return ans;
     }
-    */
 };
