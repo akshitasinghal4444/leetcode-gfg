ef27531613f49676e57d105447bf742667d3f094
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-11 15:04:47 +0530

    Time: 58 ms (5.15%), Space: 17.8 MB (41.17%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& a) {
+        vector<int> ans=a;
+        
+        do
+        {
+            a=ans;
+            ans={};
+            int i,n=a.size();
+            
+            for(i=0;i<n-1;i++)
+            {
+                if((a[i]<0)^(a[i+1]<0)==0 || a[i]<0 && a[i+1]>0)
+                    ans.push_back(a[i]);
+                else
+                {
+                    if(abs(a[i])>abs(a[i+1]))
+                        ans.push_back(a[i]);
+                    else if(abs(a[i])<abs(a[i+1]))
+                        ans.push_back(a[i+1]);
+                    
+                    i++;
+                }
+            }
+            
+            if(i==n-1)
+                ans.push_back(a[i]);
+
+        }while(a!=ans);
+        
+        return a;
+    }
+};
