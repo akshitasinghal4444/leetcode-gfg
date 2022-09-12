c12c44f6f0fb606256de534548914bf3b24c51e3
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-12 13:50:39 +0530

    Time: 115 ms (66.34%), Space: 19.9 MB (31.48%) - LeetHub

--- 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,6 +1,64 @@
 class Solution {
 public:
+    int find(int u,vector<int> &parent)
+    {
+        if(parent[u]<0)
+            return u;
+        
+        return parent[u]=find(parent[u],parent);
+    }
     
+    void unionByWt(int u,int v,vector<int> &parent)
+    {
+        int pu=find(u,parent),pv=find(v,parent);
+        
+        if(pu==pv)
+            return;
+        
+        if(parent[pu]<=parent[pv])
+        {
+            parent[pu]+=parent[pv];
+            parent[pv]=pu;
+        }
+        else
+        {
+            parent[pv]+=parent[pu];
+            parent[pu]=pv;
+        }
+    }
+    
+    int removeStones(vector<vector<int>>& a)
+    {
+        int i,j,n=a.size();
+        int ans=0;
+        vector<int> parent(n,-1);
+        unordered_map<int,vector<int>> row,col;
+        
+        for(i=0;i<n;i++)
+        {
+            row[a[i][0]].push_back(i);
+            col[a[i][1]].push_back(i);
+        }
+        
+        for(i=0;i<n;i++)
+        {
+            for(int x:row[a[i][0]])
+                unionByWt(x,i,parent);
+            
+            for(int x:col[a[i][1]])
+                unionByWt(x,i,parent);
+        }
+        
+        for(i=0;i<n;i++)
+        {
+            if(parent[i]<0)
+                ans+=abs(parent[i])-1;
+        }
+        
+        return ans;
+    }
+    
+    /*
     void dfs(vector<vector<int>>& a,int n,vector<bool> &vis,int in,int &c)
     {
         if(vis[in])
@@ -34,6 +92,7 @@ public:
         
         return ans;      
     }
+    */
     
     /*
     void dfs(vector<int> *g,int n,vector<bool> &vis,int in,int &c)
