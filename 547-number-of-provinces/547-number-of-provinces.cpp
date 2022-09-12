b190f430f411254c0ad0c80e929c1ce12cf8336f
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-12 13:04:30 +0530

    Time: 67 ms (9.87%), Space: 14 MB (42.69%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,6 +1,46 @@
 class Solution {
 public:
     
+    void bfs(vector<vector<int>>& a,int n,vector<bool> &vis,int in)
+    {
+        queue<int> q;
+        q.push(in);
+        vis[in]=1;
+        
+        while(!q.empty())
+        {
+            in=q.front();
+            q.pop();
+            
+            for(int i=0;i<n;i++)
+            {
+                if(a[in][i] && !vis[i])
+                {
+                    vis[i]=1;
+                    q.push(i);
+                }
+            }
+        }  
+    }
+    
+    int findCircleNum(vector<vector<int>>& a) {
+        int i,n=a.size();
+        int ans=0;
+        vector<bool> vis(n,0);
+        
+        for(i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                ans++;
+                bfs(a,n,vis,i);
+            }
+        }
+        
+        return ans;
+    }
+    
+    /*
     void dfs(vector<vector<int>>& a,int n,vector<bool> &vis,int in)
     {
         vis[in]=1;
@@ -28,4 +68,5 @@ public:
         
         return ans;
     }
+    */
 };
