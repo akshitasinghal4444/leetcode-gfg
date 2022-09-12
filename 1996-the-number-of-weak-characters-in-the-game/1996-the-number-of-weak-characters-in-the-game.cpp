c334a3ef89dcf2caf215865fa8da4ea12c7fb632
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-12 12:03:46 +0530

    Time: 1716 ms (20.40%), Space: 125.1 MB (68.89%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-   int numberOfWeakCharacters(vector<vector<int>>& nums) {
-        sort(nums.begin(),nums.end());
-        int ans=0,size=nums.size(),a=nums[size-1][0],b=nums[size-1][1],lmax=nums[size-1][1];;
-        for(int i=size-2;i>=0;i--)
-        {
-            if(nums[i][0]<nums[i+1][0]) 
-            {
-                b=max(lmax,b);
-               lmax=max(b,nums[i][1]); 
-            }
-            if(nums[i][0]<a && nums[i][1]<b)
-              ans++;
-        }
-        return ans; 
+   static bool comp(vector<int> &a,vector<int> &b)
+   {
+       if(a[0]==b[0])
+           return a[1]<=b[1];
+       return a[0]>=b[0];
+   }
+    
+   int numberOfWeakCharacters(vector<vector<int>>& a) {
+       int ans=0,mx=0;
+       int i,n=a.size();
+       
+       sort(a.begin(),a.end(),comp);
+       
+       for(i=1;i<n;i++)
+       {
+           if(a[i][1]<a[mx][1])
+               ans++;
+           else
+               mx=i;
+       }
         
+       return ans; 
     }
 };
