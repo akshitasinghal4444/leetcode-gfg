8301a616628ce3aa7d7774ade92dce2f8eb4f3aa
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-13 15:56:28 +0530

    Time: 27 ms (36.73%), Space: 14 MB (45.00%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    vector<vector<int>> combinationSum(vector<int>& a, int t)
+    {
+        int i,j,n=a.size();
+        vector<vector<vector<int>>> dp(t+1);
+        dp[0].push_back({});
+        
+        for(i=0;i<n;i++)
+        {
+            for(j=1;j<=t;j++)
+            {
+                if(j<a[i])
+                    continue;
+                
+                for(auto v:dp[j-a[i]])
+                {
+                    v.push_back(a[i]);
+                    dp[j].push_back(v);
+                }
+            }
+        }
+        
+        return dp[t];
+    }
+    
+    /*
     vector<vector<int>> ans;
     vector<int> x;
     
@@ -29,4 +54,5 @@ public:
         sum(a,t,0,a.size());
         return ans;
     }
+    */
 };
