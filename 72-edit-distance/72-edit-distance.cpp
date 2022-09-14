7434769bef361efdc808ecd52e13dfce0a74086c
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-14 14:02:10 +0530

    Time: 18 ms (64.24%), Space: 7.1 MB (90.56%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    /*
     int find(string s1, string s2,int n1,int n2,vector<vector<int>> &dp)
     {
         if(n1==0)
@@ -22,5 +24,30 @@ public:
         
         vector<vector<int>> dp(s1.length()+1,vector<int>(s2.length()+1,-1));
         return find(s1,s2,s1.length(),s2.length(),dp);
+    }
+    */
+    
+    int minDistance(string s1, string s2)
+    {
+        int n1=s1.length(),n2=s2.length();
+        int i,j;
+        int dp[n1+1][n2+1];
+        
+        for(i=0;i<=n1;i++)
+        {
+            for(j=0;j<=n2;j++)
+            {
+                if(i==0)
+                    dp[i][j]=j;
+                else if(j==0)
+                    dp[i][j]=i;
+                else if(s1[i-1]==s2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]})+1;
+            }
+        }
+        
+        return dp[n1][n2];
     }
 };
