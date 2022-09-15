b9b8ef5e1418b74f9ab872bd23d3cd5b8380c52b
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-15 09:04:38 +0530

    Time: 932 ms (19.15%), Space: 144.2 MB (45.11%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -7,6 +7,35 @@ public:
         if(n%2!=0)
             return {};
         
+        map<int,int> m;
+        
+        for(i=0;i<n;i++)
+            m[a[i]]++;
+        
+        for(auto it:m)
+        {
+            while(m[it.first]>0)
+            {
+                m[it.first]--;
+                if(m[2*it.first]<=0)
+                    return {};
+            
+                m[2*it.first]--;
+                ans.push_back(it.first);
+            }
+        }
+        
+        return ans;
+    }
+    
+    /*
+    vector<int> findOriginalArray(vector<int>& a) {
+        int i,j,n=a.size();
+        vector<int> ans;
+        
+        if(n%2!=0)
+            return {};
+        
         sort(a.begin(),a.end());
         unordered_map<int,int> m;
         
@@ -28,4 +57,5 @@ public:
         
         return ans;
     }
+    */
 };
