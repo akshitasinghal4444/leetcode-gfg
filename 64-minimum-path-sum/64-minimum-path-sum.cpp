a388f2d39d97a55fbd617ae1b9122238acf4622c
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-15 09:41:53 +0530

    Time: 22 ms (22.77%), Space: 9.7 MB (91.70%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,56 +1,25 @@
 class Solution {
 public:
-    
-    int minPathSum(vector<vector<int>>& a)
-    {
-        int m,n,i,j;
-        m=a.size();
-        n=a[0].size();
-        
-        for(i=0;i<m;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(i==0 && j==0)
-                    continue;
-                
-                if(i==0)
-                    a[i][j]+=a[i][j-1];
-                else if(j==0)
-                    a[i][j]+=a[i-1][j];
-                else
-                    a[i][j]+=min(a[i][j-1],a[i-1][j]);
-                    
-            }
-        }
-        return a[m-1][n-1];
-    }
-    
-    /*
     int minPathSum(vector<vector<int>>& a) {
-        int m,n;
-        m=a.size();
-        n=a[0].size();
-        
+        int m=a.size(),n=a[0].size();
         int i,j;
+        
         for(i=m-1;i>=0;i--)
         {
             for(j=n-1;j>=0;j--)
             {
                 if(i==m-1 && j==n-1)
-                    a[i][j]=a[i][j];
-                else if(i==m-1)
+                    continue;
+                
+                if(i==m-1)
                     a[i][j]+=a[i][j+1];
                 else if(j==n-1)
                     a[i][j]+=a[i+1][j];
                 else
-                {
-                    a[i][j]+=min(a[i][j+1],a[i+1][j]);
-                }
-                    
+                    a[i][j]+=min(a[i+1][j],a[i][j+1]);
             }
         }
+        
         return a[0][0];
     }
-    */
 };
