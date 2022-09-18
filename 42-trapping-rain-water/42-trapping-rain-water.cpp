a5477e6e46cd51c36320f257b4a3c80a7da57da2
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-18 12:11:35 +0530

    Time: 42 ms (11.19%), Space: 19.8 MB (81.79%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,102 +1,22 @@
 class Solution {
 public:
-    int trap(vector<int>& a)
-    {
-        int n=a.size();
-        int l=0,r=n-1,maxl=0,maxr=0;
+    int trap(vector<int>& a) {
+        
+        int l=0,r=a.size()-1;
+        int lmax=a[l],rmax=a[r];
         int ans=0;
         
         while(l<=r)
         {
-            maxl=max(a[l],maxl);
-            maxr=max(a[r],maxr);
+            lmax=max(a[l],lmax);
+            rmax=max(a[r],rmax);
             
-            if(maxl<=maxr)
-            {
-                ans+=maxl-a[l];
-                l++;
-            }
+            if(lmax<=rmax)
+                ans+=(lmax-a[l++]);
             else
-            {
-                ans+=maxr-a[r];
-                r--;
-            }
+                ans+=(rmax-a[r--]);
         }
         
         return ans;
     }
-    
-    /*
-    int trap(vector<int>& a) {
-        int i,j,n=a.size();
-        int ans=0;
-        vector<int> w(n,INT_MAX);
-        
-        int mxl=0,mxr=0;
-        for(i=0,j=n-1;i<n;i++,j--)
-        {
-            mxl=max(a[i],mxl);
-            mxr=max(a[j],mxr);
-            w[i]=min(w[i],mxl-a[i]);
-            w[j]=min(w[j],mxr-a[j]);
-        }
-        
-        for(i=0;i<n;i++)
-            ans+=w[i];
-        
-        return ans;
-    }
-    */
-    
-    /*
-    int trap(vector<int>& a) {
-        int i,j,n=a.size();
-        int w[n],mx;
-        int ans=0;
-        
-        mx=0;
-        for(i=0;i<n;i++)
-        {
-            mx=max(a[i],mx);
-            w[i]=mx-a[i];
-        }
-        
-        mx=0;
-        for(i=n-1;i>=0;i--)
-        {
-            mx=max(a[i],mx);
-            w[i]=min(w[i],mx-a[i]);
-            ans+=w[i];
-        }
-        
-        return ans;
-    }
-    */
-    
-    /*
-    int trap(vector<int>& a) {
-        int i,j,n=a.size();
-        int l[n],r[n],mx;
-        
-        mx=0;
-        for(i=0;i<n;i++)
-        {
-            mx=max(a[i],mx);
-            l[i]=mx-a[i];
-        }
-        
-        mx=0;
-        for(i=n-1;i>=0;i--)
-        {
-            mx=max(a[i],mx);
-            r[i]=mx-a[i];
-        }
-        
-        int ans=0;
-        for(i=0;i<n;i++)
-            ans+=min(l[i],r[i]);
-        
-        return ans;
-    }
-    */
 };
