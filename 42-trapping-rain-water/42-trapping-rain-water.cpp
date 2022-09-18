0fc4c2c4c67daa6ce59a2c8a88b403e4bfc907d9
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-18 12:13:39 +0530

    Time: 43 ms (9.88%), Space: 19.7 MB (81.79%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -19,4 +19,78 @@ public:
         
         return ans;
     }
+    
+    /*
+    int trap(vector<int>& a) {
+        int i,j,n=a.size();
+        int ans=0;
+        vector<int> w(n,INT_MAX);
+        
+        int mxl=0,mxr=0;
+        for(i=0,j=n-1;i<n;i++,j--)
+        {
+            mxl=max(a[i],mxl);
+            mxr=max(a[j],mxr);
+            w[i]=min(w[i],mxl-a[i]);
+            w[j]=min(w[j],mxr-a[j]);
+        }
+        
+        for(i=0;i<n;i++)
+            ans+=w[i];
+        
+        return ans;
+    }
+    */
+    
+    /*
+    int trap(vector<int>& a) {
+        int i,j,n=a.size();
+        int w[n],mx;
+        int ans=0;
+        
+        mx=0;
+        for(i=0;i<n;i++)
+        {
+            mx=max(a[i],mx);
+            w[i]=mx-a[i];
+        }
+        
+        mx=0;
+        for(i=n-1;i>=0;i--)
+        {
+            mx=max(a[i],mx);
+            w[i]=min(w[i],mx-a[i]);
+            ans+=w[i];
+        }
+        
+        return ans;
+    }
+    */
+    
+    /*
+    int trap(vector<int>& a) {
+        int i,j,n=a.size();
+        int l[n],r[n],mx;
+        
+        mx=0;
+        for(i=0;i<n;i++)
+        {
+            mx=max(a[i],mx);
+            l[i]=mx-a[i];
+        }
+        
+        mx=0;
+        for(i=n-1;i>=0;i--)
+        {
+            mx=max(a[i],mx);
+            r[i]=mx-a[i];
+        }
+        
+        int ans=0;
+        for(i=0;i<n;i++)
+            ans+=min(l[i],r[i]);
+        
+        return ans;
+    }
+    */
 };
