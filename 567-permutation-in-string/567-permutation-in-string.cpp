64ff4b5bcc5f206ff7fc5bbd5f16de30e7538011
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-19 11:13:19 +0530

    Time: 8 ms (82.49%), Space: 7.3 MB (73.74%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+    
+    bool comp(int *f1,int *f2)
+    {
+        int i;
+        for(i=0;i<26;i++)
+        {
+            if(f1[i]!=f2[i])
+                return 0;
+        }
+        return 1;
+    }
+    
+    bool checkInclusion(string s1, string s2) {
+        int n1,n2;
+        n1=s1.length();
+        n2=s2.length();
+        
+        if(n2<n1)
+            return 0;
+        
+        int i,j;
+        int f1[26]={0},f2[26]={0};
+        
+        for(i=0;i<n1;i++)
+        {
+            f1[s1[i]-'a']++;
+            f2[s2[i]-'a']++;
+        }
+        
+        if(comp(f1,f2))
+            return 1;
+        
+        for(i;i<n2;i++)
+        {
+            f2[s2[i]-'a']++;
+            f2[s2[i-n1]-'a']--;
+            if(comp(f1,f2))
+                return 1;
+        }
+        return 0;
+    }
+    
+    /*
     bool checkInclusion(string s1, string s2) {
         int n1=s1.length(),n2=s2.length();
         int i,j;
@@ -28,4 +71,5 @@ public:
         
         return 0;
     }
+    */
 };
