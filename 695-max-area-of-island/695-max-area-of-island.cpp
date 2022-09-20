2eea248720d55e5d04c40b072ce399e92a3a4ae6
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-20 13:01:52 +0530

    Time: 44 ms (23.10%), Space: 23.3 MB (45.44%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int ans = 0;
-        n = grid.size(), m = grid[0].size();
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < m; j++)
-                if (grid[i][j]) ans = max(ans, trav(i, j, grid));
-        return ans;
+    int t;
+    
+    void dfs(vector<vector<int>>& g,int m,int n,int i,int j)
+    {
+        if(i<0 || j<0 || i>=m || j>=n || !g[i][j])
+            return;
+        
+        t++;
+        g[i][j]=0;
+        
+        dfs(g,m,n,i-1,j);
+        dfs(g,m,n,i+1,j);
+        dfs(g,m,n,i,j-1);
+        dfs(g,m,n,i,j+1);
     }
-private:
-    int n, m;
-    int trav(int i, int j, vector<vector<int>>& grid) {
-        if (i < 0 || j < 0 || i >= n || j >= m || !grid[i][j]) return 0;
-        grid[i][j] = 0;
-        return 1 + trav(i-1, j, grid) + trav(i, j-1, grid) + trav(i+1, j, grid) + trav(i, j+1, grid);
+    
+    int maxAreaOfIsland(vector<vector<int>>& g) {
+        int m=g.size(),n=g[0].size();
+        int i,j;
+        int ans=0;
+        
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(g[i][j])
+                {
+                    t=0;
+                    dfs(g,m,n,i,j);
+                    ans=max(ans,t);
+                }                  
+            }
+        }
+        
+        return ans;
     }
 };
