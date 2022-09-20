8e29a2e17d798abccdb5f7e54ccebf3579927ba5
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-20 12:52:57 +0530

    Time: 380 ms (70.40%), Space: 16.1 MB (81.84%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -10,6 +10,34 @@ public:
             return 0;
         
         for(i=0;i<=n1;i++)
+        {
+            for(j=0;j<=n2;j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j]=0;                
+                else if(a1[i-1]==a2[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=0;
+                
+                ans=max(ans,dp[i][j]);
+            }
+        }
+        
+        return ans;
+    }
+    
+    /*
+    int findLength(vector<int>& a1, vector<int>& a2) {
+        int n1=a1.size(),n2=a2.size();
+        int i,j;
+        int ans=0;
+        int dp[n1+1][n2+1];
+        
+        if(n1==0 || n2==0)
+            return 0;
+        
+        for(i=0;i<=n1;i++)
             dp[i][n2]=0;
         
         for(j=0;j<=n2;j++)
@@ -30,4 +58,5 @@ public:
         
         return ans;
     }
+    */
 };
