af0a6a2581bd0b82783606d4014a77601c51f44d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-24 15:01:54 +0530

    Time: 245 ms (10.74%), Space: 51.3 MB (64.83%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    bool containsDuplicate(vector<int>& a)
+    {
+        unordered_set<int> s;
+        
+        for(int x:a)
+        {
+            if(s.find(x)!=s.end())
+                return 1;
+            
+            s.insert(x);
+        }
+        
+        return 0;
+    }
+    
+    /*
     bool containsDuplicate(vector<int>& a) {
         unordered_map<int,bool> m;
         
@@ -12,4 +28,5 @@ public:
         }
         return 0;
     }
+    */
 };
