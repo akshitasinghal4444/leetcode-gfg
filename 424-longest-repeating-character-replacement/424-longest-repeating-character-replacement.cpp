4ff1738a26cc4b19ef3b08bd286ceb6fb31df62d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-24 11:38:21 +0530

    Time: 19 ms (52.91%), Space: 7 MB (84.91%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -15,6 +15,30 @@ public:
                 mlen=max(mlen,len);
             else
             {
+                f[s[st]]--;
+                st++;
+            }            
+        }
+        
+        return mlen;
+    }
+    
+    /*
+    int characterReplacement(string s, int k) {
+        int i,n=s.length();
+        int st=0,mlen=1,mf=0;
+        int f[256]={0};
+        
+        for(i=0;i<n;i++)
+        {
+            f[s[i]]++;
+            mf=max(mf,f[s[i]]);
+            int len=i-st+1;
+            
+            if(len-mf<=k)
+                mlen=max(mlen,len);
+            else
+            {
                 while(st<=i && i-st+1-mf>k)
                 {
                     f[s[st]]--;
@@ -25,4 +49,5 @@ public:
         
         return mlen;
     }
+    */
 };
