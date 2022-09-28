543c5b68b135fc587a0349e46daa536af75615d0
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-28 13:33:22 +0530

    Time: 19 ms (28.81%), Space: 13.6 MB (84.70%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& N, int T) {
-        pair<vector<int>::iterator,vector<int>::iterator> range;
-        range = equal_range(N.begin(), N.end(), T);
-        int Tleft = distance(N.begin(), range.first);
-        if (Tleft == N.size() || N[Tleft] != T) return {-1, -1};
-        return {Tleft, (int)distance(N.begin(), range.second) - 1};
+    vector<int> searchRange(vector<int>& a, int t) {
+        int n=a.size();
+        int l=0,r=n-1,mid;
+        
+        while(l<=r)
+        {
+            mid=(l+r)/2;
+            
+            if(a[mid]==t)
+                break;
+            
+            if(a[mid]<t)
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+        
+        if(l>r)
+            return {-1,-1};
+        
+        int st=mid,end=mid;
+        
+        while(st>=l && a[st]==t)
+            st--;
+        
+        while(end<=r && a[end]==t)
+            end++;
+        
+        return {st+1,end-1};
     }
 };
