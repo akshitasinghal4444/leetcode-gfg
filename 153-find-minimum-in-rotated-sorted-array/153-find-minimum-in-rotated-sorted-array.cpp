ab3a4fae26f254f90e3926bd60e1f57ee0d50a97
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-09-30 19:12:57 +0530

    Time: 12 ms (13.00%), Space: 10.3 MB (22.12%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,68 +1,25 @@
 class Solution {
 public:
-    int findMin(vector<int>& a)
-    {
-        int l=0,r=a.size()-1,mid;
+    int findMin(vector<int>& a) {
+        int ans=a[0];
+        int l=0,r=a.size()-1;
         
         while(l<=r)
         {
-            mid=(l+r)/2;
-            
-            if(a[mid]>=a[r])
-                l=mid+1;
-            else
-                r=mid;
-        }
-        return a[mid];
-    }
-    
-    /*
-    int findMin(vector<int>& a)
-    {
-        int l=0,r=a.size()-1;
-        if(a[l]<a[r])
-            return a[l];
-        
-        while(l<r)
-        {
             int mid=(l+r)/2;
             
-            if(a[mid]>a[mid+1])
-                return a[mid+1];
-            
-            if(a[mid-1]>a[mid])
-                return a[mid];
-            
-            if(a[mid]>a[l])
+            if(a[mid]>=a[l])
+            {
+                ans=min(ans,a[l]);
                 l=mid+1;
+            }
             else
+            {
+                ans=min(ans,a[mid]);
                 r=mid-1;
+            }
         }
         
-        return a[0];
-    }
-    */
-    
-    /*
-    int binSearch(vector<int>& a,int l,int h)
-    {
-        int ans=INT_MAX;
-        while(l<=h)
-        {
-            int mid=(l+h)/2;
-            ans=min(ans,a[mid]);
-            if(a[mid]>a[h])
-                l=mid+1;
-            else
-                h=mid-1;               
-        }
         return ans;
     }
-    
-    int findMin(vector<int>& a) {
-        int n=a.size();
-        return binSearch(a,0,n-1);
-    }
-    
-    */
 };
