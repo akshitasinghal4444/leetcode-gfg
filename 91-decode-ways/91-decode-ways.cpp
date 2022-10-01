285dd73e22bc91160b595665d448cd17c0b723ce
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-01 13:43:43 +0530

    Time: 8 ms (18.06%), Space: 6 MB (93.04%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,81 +1,48 @@
 class Solution {
 public:
     
-    int numDecodings(string s)
-    {
-        if(s[0]=='0')
-            return 0;
-        
-        int n=s.length();
-        int dp[n];
-        dp[0]=1;
-        
-        for(int i=1;i<n;i++)
-        {
-            if(s[i]=='0')
-            {
-                if(s[i-1]=='1' || s[i-1]=='2')      //10,20
-                    dp[i]=(i-2>=0)?dp[i-2]:1;
-                else                                //00,30,40,....
-                    return 0;
-            }
-            else
-            {
-                dp[i]=dp[i-1];                      //0 !0
-                
-                if(s[i-1]!='0')
-                {
-                    string x="";
-                    x+=s[i-1];
-                    x+=s[i];
-                    if(stoi(x)<=26)                 //!0 !0 
-                        dp[i]+=(i-2>=0)?dp[i-2]:1;
-                }
-            }
-        }
-        return dp[n-1];
-    }
     
-    /*
-    int numDecodings(string s)
-    {
-        int n=s.length();
-        if(n==0 || s[0]=='0')
-            return 0;
+    
+    int numDecodings(string s) {
         
-        int i;
-        vector<int> dp(n,0);
+        if(s[0]=='0')
+        return 0;
+        
+        int i,n=s.length();
+        int dp[n];
         dp[0]=1;
         
         for(i=1;i<n;i++)
         {
-            if(s[i]=='0' && s[i-1]=='0')
-                return 0;
-            
-            if(s[i]!=0 && s[i-1]=='0')
+            if(s[i]=='0')
             {
-                dp[i]=dp[i-1];
-            }
-            else if(s[i]=='0' && s[i-1]!='0')
-            {
-                if(s[i-1]=='1' || s[i-1]=='2')
-                    dp[i]=(i-2>=0)?dp[i-2]:1;
-                else
+                if(s[i-1]=='0' || s[i-1]>'2')
                     return 0;
+                
+                if(i-2>=0)
+                dp[i]=dp[i-2];
+                else
+                dp[i]=1;
             }
-            else
+            else    
             {
                 dp[i]=dp[i-1];
                 
-                string x="";
-                x+=s[i-1];
-                x+=s[i];
-                
-                if(stoi(x)<=26)
-                    dp[i]+=(i-2>=0)?dp[i-2]:1;
-            }
+                string t="";
+                t+=s[i-1];
+                t+=s[i];
+                if(s[i-1]!='0' && stoi(t)<=26)
+                {
+                    if(i-2>=0)
+                        dp[i]+=dp[i-2];
+                    else
+                        dp[i]+=1;
+                }
+            } 
+            
+            // cout<<i<<" "<<dp[i]<<endl;
         }
+        
         return dp[n-1];
     }
-    */
 };
