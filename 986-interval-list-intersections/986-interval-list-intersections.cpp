0d77ddce234788f654107884253e7eaa4d03b86c
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-01 15:44:50 +0530

    Time: 39 ms (84.97%), Space: 18.7 MB (76.64%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.cpp
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& a1, vector<vector<int>>& a2) {
+        int n1=a1.size(),n2=a2.size();
+        int i=0,j=0;
+        vector<vector<int>> ans;
+        
+        while(i<n1 && j<n2)
+        {
+            int l=max(a1[i][0],a2[j][0]);
+            int r=min(a1[i][1],a2[j][1]);
+            
+            if(l<=r)
+                ans.push_back({l,r});
+            
+            if(a1[i][1]<=a2[j][1])
+                i++;
+            else
+                j++;
+        }
+        
+        return ans;
+    }
     
+    /*
     vector<int> find_common(vector<int> &a ,vector<int> &b)
     {
         if(a[0]>b[1] || b[0]>a[1])
@@ -29,4 +51,5 @@ public:
         
         return ans;
     }
+    */
 };
