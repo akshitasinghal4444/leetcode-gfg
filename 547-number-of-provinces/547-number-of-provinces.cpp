495bc29778041230beeacb827f7d14dbb885f8fd
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-03 15:15:43 +0530

    Time: 56 ms (28.23%), Space: 13.7 MB (68.66%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,6 +1,59 @@
 class Solution {
 public:
     
+    int find(int u,vector<int> &parent)
+    {
+        if(parent[u]<0)
+            return u;
+        
+        return parent[u]=find(parent[u],parent);
+    }
+    
+    void unionByWt(int u,int v,vector<int> &parent)
+    {
+        int pu=find(u,parent);
+        int pv=find(v,parent);
+        
+        if(pu==pv)
+            return;
+        
+        if(parent[pu]<=parent[pv])
+        {
+            parent[pu]+=parent[pv];
+            parent[pv]=pu;
+        }
+        else
+        {
+            parent[pv]+=parent[pu];
+            parent[pu]=pv;
+        }
+    }
+    
+    int findCircleNum(vector<vector<int>>& a)
+    {
+        int i,j,n=a.size();
+        int ans=0;
+        vector<int> parent(n,-1);
+        
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(a[i][j])
+                    unionByWt(i,j,parent);
+            }
+        }
+        
+        for(i=0;i<n;i++)
+        {
+            if(parent[i]<0)
+                ans++;
+        }
+        
+        return ans;
+    }
+    
+    /*
     void bfs(vector<vector<int>>& a,int n,vector<bool> &vis,int in)
     {
         queue<int> q;
@@ -23,6 +76,7 @@ public:
         }  
     }
     
+    
     int findCircleNum(vector<vector<int>>& a) {
         int i,n=a.size();
         int ans=0;
@@ -39,6 +93,7 @@ public:
         
         return ans;
     }
+    */
     
     /*
     void dfs(vector<vector<int>>& a,int n,vector<bool> &vis,int in)
