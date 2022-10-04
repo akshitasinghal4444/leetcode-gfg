bc116981e3ee9f7bdf6e8eef2dab38174d666fb1
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-04 14:01:47 +0530

    Time: 16 ms (67.61%), Space: 21.3 MB (38.76%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -15,10 +15,10 @@ public:
         if(!root)
             return 0;
         
-         t-=root->val;
-        if(!root->left && !root->right)
-            return t==0;
+        if(!root->left && !root->right && t==root->val)
+            return 1;
         
+        t-=root->val;
         return hasPathSum(root->left,t) || hasPathSum(root->right,t);
     }
 };
