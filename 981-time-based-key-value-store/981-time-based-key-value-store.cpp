18d6a603c72917130f7c4a4b0e47c1f1450359d5
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-06 12:27:17 +0530

    Time: 832 ms (11.43%), Space: 130.7 MB (19.24%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,5 +1,30 @@
 class TimeMap {
 public:
+    unordered_map<string,map<int,string>> m;
+    
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int time) {
+        m[key][time]=value;
+    }
+    
+    string get(string key, int time) {
+
+        auto it=m[key].upper_bound(time);
+        
+        if(it==m[key].begin())
+            return "";
+        
+        it--;
+        return it->second;
+    }
+};
+
+/*
+class TimeMap {
+public:
     unordered_map<string,vector<string>> m1;
     unordered_map<string,vector<int>> m2;
     
@@ -13,9 +38,7 @@ public:
     }
     
     string get(string key, int time) {
-//         if(m1[key].empty())
-//         return "";
-        
+
         int in=upper_bound(m2[key].begin(),m2[key].end(),time)-m2[key].begin();
         
         if(in==0)
@@ -24,6 +47,7 @@ public:
         return m1[key][in-1];
     }
 };
+*/
 
 /**
  * Your TimeMap object will be instantiated and called as such:
