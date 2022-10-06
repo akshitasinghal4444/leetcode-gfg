e04bb86ba607d2149dea0f29ef4346011998fdb3
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-06 12:51:38 +0530

    Time: 148 ms (40.54%), Space: 19.3 MB (84.88%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    bool isvalid(int i,int j,int m,int n,vector<vector<int>>& a)
-    {
-        return (i>=0 && j>=0 && i<m && j<n && a[i][j]==0);
-    }
-    
-    int shortestPathBinaryMatrix(vector<vector<int>>& a) {
+    int shortestPathBinaryMatrix(vector<vector<int>>& g) {
+        int m=g.size(),n=g[0].size();
+        int i,j,k,c=0;
         
-        int m=a.size(),n=a[0].size();
-        int i,j,k,ans=0;
-        
-        if(a[0][0]==1 || a[m-1][n-1]==1)
+        if(g[0][0]==1 || g[m-1][n-1]==1)
             return -1;
         
         if(m==1 && n==1)
             return 1;
         
-        int dx[]={-1,-1,0,1,1,1,0,-1};
-        int dy[]={0,1,1,1,0,-1,-1,-1};
-        
         queue<pair<int,int>> q;
         q.push({0,0});
+        g[0][0]=1;
+        
+        int dx[]={-1,-1,-1,0,0,1,1,1};
+        int dy[]={-1,0,1,-1,1,-1,0,1};
         
         while(!q.empty())
         {
-            ans++;
+            c++;
             int s=q.size();
             
             while(s--)
@@ -35,13 +30,13 @@ public:
                 
                 for(k=0;k<8;k++)
                 {
-                    if(isvalid(i+dx[k],j+dy[k],m,n,a))
+                    if(i+dx[k]>=0 && j+dy[k]>=0 && i+dx[k]<m && j+dy[k]<n && g[i+dx[k]][j+dy[k]]==0)
                     {
                         if(i+dx[k]==m-1 && j+dy[k]==n-1)
-                            return ans+1;
+                            return c+1;
                         
                         q.push({i+dx[k],j+dy[k]});
-                        a[i+dx[k]][j+dy[k]]=1;
+                        g[i+dx[k]][j+dy[k]]=1;
                     }
                 }
             }
