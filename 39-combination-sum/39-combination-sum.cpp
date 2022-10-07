e72c3298cbdc9241fd4c2189d830a721d5c33c49
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-07 16:20:02 +0530

    Time: 11 ms (68.71%), Space: 17 MB (37.49%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,58 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& a, int t)
-    {
-        int i,j,n=a.size();
-        vector<vector<vector<int>>> dp(t+1);
-        dp[0].push_back({});
-        
-        for(i=0;i<n;i++)
-        {
-            for(j=1;j<=t;j++)
-            {
-                if(j<a[i])
-                    continue;
-                
-                for(auto v:dp[j-a[i]])
-                {
-                    v.push_back(a[i]);
-                    dp[j].push_back(v);
-                }
-            }
-        }
-        
-        return dp[t];
-    }
-    
-    /*
     vector<vector<int>> ans;
-    vector<int> x;
+    vector<int> v;
     
-    void sum(vector<int>& a, int t,int in,int n)
+    void combination(vector<int>& a, int t,int in,int n)
     {
         if(in==n)
         {
             if(t==0)
-                ans.push_back(x);
+                ans.push_back(v);
             
-            return;
+            return ;
         }
         
         if(t<0)
             return;
         
-        if(x.empty() || x.back()!=a[in])
-        sum(a,t,in+1,n);
+        if(v.empty() || v.back()!=a[in])
+        combination(a,t,in+1,n);
         
-        x.push_back(a[in]);
-        sum(a,t-a[in],in+1,n);
-        sum(a,t-a[in],in,n);
-        x.pop_back();
+        v.push_back(a[in]);
+        combination(a,t-a[in],in,n);
+        combination(a,t-a[in],in+1,n);
+        v.pop_back();
     }
     
     vector<vector<int>> combinationSum(vector<int>& a, int t) {
-        sum(a,t,0,a.size());
+        combination(a,t,0,a.size());
         return ans;
     }
-    */
 };
