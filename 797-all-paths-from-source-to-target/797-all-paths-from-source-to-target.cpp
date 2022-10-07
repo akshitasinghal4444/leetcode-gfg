4da1f901f2c195e30977db1c3f8a6ec63672ddcb
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-07 16:35:50 +0530

    Time: 30 ms (44.55%), Space: 12 MB (50.70%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    
+public:
+    vector<vector<int>> ans;
+    vector<int> t;
+    
+    void dfs(vector<vector<int>>& g,int in,int n,vector<bool> &vis)
+    {
+        if(vis[in])
+            return;
+        
+        vis[in]=1;
+        t.push_back(in);
+        
+        if(in==n-1)
+        {
+            ans.push_back(t);
+            vis[in]=0;
+            t.pop_back();
+            return;
+        }
+        
+        for(int i:g[in])
+            dfs(g,i,n,vis);
+        
+        vis[in]=0;
+        t.pop_back();
+            
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& g) {
+        int n=g.size();
+        vector<bool> vis(n,0);
+        
+        dfs(g,0,n,vis);
+        
+        return ans;
+    }
+};
