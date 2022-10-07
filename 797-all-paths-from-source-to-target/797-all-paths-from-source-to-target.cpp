6ab20e699c3808401268b61c66b8b1b800e9406d
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-07 16:38:26 +0530

    Time: 26 ms (53.86%), Space: 12.1 MB (50.70%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -7,66 +7,24 @@ public:
     
     void dfs(vector<vector<int>>& g,int in,int n)
     {
+        t.push_back(in);
+        
+        if(in==n-1)
+            ans.push_back(t);
+        else
+        {
+            for(int i:g[in])
+                dfs(g,i,n);
+        }
+        
+        t.pop_back();
+    }
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& g) {
 
-        t.push_back(in);
-        
-        if(in==n-1)
-        {
-            ans.push_back(t);
-            t.pop_back();
-            return;
-        }
-        
-        for(int i:g[in])
-            dfs(g,i,n);
-        
-        t.pop_back();
-            
-    }
-    
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& g) {
-        int n=g.size();
-        
-        dfs(g,0,n);
-        
+        dfs(g,0,g.size());        
         return ans;
     }
     
-    /*
-    vector<vector<int>> ans;
-    vector<int> t;
     
-    void dfs(vector<vector<int>>& g,int in,int n,vector<bool> &vis)
-    {
-        if(vis[in])
-            return;
-        
-        vis[in]=1;
-        t.push_back(in);
-        
-        if(in==n-1)
-        {
-            ans.push_back(t);
-            vis[in]=0;
-            t.pop_back();
-            return;
-        }
-        
-        for(int i:g[in])
-            dfs(g,i,n,vis);
-        
-        vis[in]=0;
-        t.pop_back();
-            
-    }
-    
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& g) {
-        int n=g.size();
-        vector<bool> vis(n,0);
-        
-        dfs(g,0,n,vis);
-        
-        return ans;
-    }
-    */
 };
