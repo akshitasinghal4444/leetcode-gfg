e5cc876e5ba3d4ad4fd8dcda8a049e0301eeea57
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-07 17:04:34 +0530

    Time: 24 ms (51.71%), Space: 16.6 MB (6.07%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,6 +1,53 @@
 class Solution {
 public:
+    void dfs(vector<vector<char>>& a,int i,int j,int m,int n,vector<vector<bool>> &vis,char c)
+    {
+        if(i<0 || j<0 || i>=m || j>=n || vis[i][j] || a[i][j]=='X')
+            return;
+        
+        a[i][j]=c;
+        vis[i][j]=1;
+        
+        dfs(a,i-1,j,m,n,vis,c);
+        dfs(a,i+1,j,m,n,vis,c);
+        dfs(a,i,j-1,m,n,vis,c);
+        dfs(a,i,j+1,m,n,vis,c);
+    }
     
+    void solve(vector<vector<char>>& a) {
+        int m=a.size(),n=a[0].size();
+        vector<vector<bool>> vis(m,vector<bool>(n,0));
+        int i,j;
+        
+        for(i=0;i<m;i++)
+        {
+            if(a[i][0]=='O' && !vis[i][0])
+                dfs(a,i,0,m,n,vis,'O');
+            
+            if(a[i][n-1]=='O' && !vis[i][n-1])
+                dfs(a,i,n-1,m,n,vis,'O');                
+        }
+        
+        for(j=0;j<n;j++)
+        {
+            if(a[0][j]=='O' && !vis[0][j])
+                dfs(a,0,j,m,n,vis,'O');
+             
+            if(a[m-1][j]=='O' && !vis[m-1][j])
+                dfs(a,m-1,j,m,n,vis,'O');
+        }
+        
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(a[i][j]=='O' && !vis[i][j])
+                    dfs(a,i,j,m,n,vis,'X');
+            }
+        }
+    }
+    
+    /*
     void bfs(vector<vector<char>>& a,int i,int j,int m,int n,vector<vector<bool>> &vis,char c)
     {
         queue<pair<int,int>> q;
@@ -77,4 +124,5 @@ public:
             }
         }
     }
+    */
 };
