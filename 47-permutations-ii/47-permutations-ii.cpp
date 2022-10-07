da056ead0756fd7f68e8f6029796c4dcfc59a503
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-07 13:49:13 +0530

    Time: 3 ms (98.84%), Space: 11.5 MB (20.58%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
+    
+    void permute(vector<int>& a,int in,int n)
+    {
+        if(in==n)
+        {
+            ans.push_back(a);
+            return;
+        }
+            
+        unordered_set<int> vis;
+        
+        for(int i=in;i<n;i++)
+        {
+            if(vis.find(a[i])==vis.end())
+            {
+                vis.insert(a[i]);
+                swap(a[i],a[in]);
+                permute(a,in+1,n);
+                swap(a[i],a[in]);
+            }            
+        }
+        
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& a) {
+        permute(a,0,a.size());
+        
+        return ans;
+    }
+    
+    /*
     set<vector<int>> s;
     
     void permute(vector<int>& a,int in,int n)
@@ -25,4 +57,5 @@ public:
         vector<vector<int>> ans(s.begin(),s.end());
         return ans;
     }
+    */
 };
