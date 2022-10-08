401ebcc80cbc332f1db3d0b2e42f30d124f45ac0
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-08 13:57:04 +0530

    Time: 1594 ms (7.38%), Space: 8 MB (79.69%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+
     bool dfs(vector<vector<char>>& a, string s,int m,int n,int i,int j,int l,int in)
     {
         if(in==l)
@@ -9,13 +9,12 @@ public:
         if(i<0 || j<0 || i>=m || j>=n || a[i][j]!=s[in])
             return 0;
         
-        char c=a[i][j];
         a[i][j]='.';
         
         if(dfs(a,s,m,n,i-1,j,l,in+1) || dfs(a,s,m,n,i+1,j,l,in+1) || dfs(a,s,m,n,i,j-1,l,in+1) || dfs(a,s,m,n,i,j+1,l,in+1))
             return 1;
         
-        a[i][j]=c;
+        a[i][j]=s[in];
         return 0;
     }
     
