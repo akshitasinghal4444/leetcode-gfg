45723d93550f56cfc1abb613c31787f99c45bea4
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-08 13:44:29 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (77.25%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string> ans;
+    string t;
+    
+    void generate(int l,int r)
+    {
+        if(l==0 && r==0)
+        {
+            ans.push_back(t);
+            return;
+        }
+        
+        if(l>0)
+        {
+            t.push_back('(');
+            generate(l-1,r);
+            t.pop_back();
+        }
+        
+        if(l<r)
+        {
+            t.push_back(')');
+            generate(l,r-1);
+            t.pop_back();
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        generate(n,n);
+        return ans;
+    }
+};
