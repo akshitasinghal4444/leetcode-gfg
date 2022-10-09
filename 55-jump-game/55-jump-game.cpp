b0c664e6efcef867b74ea1541b8865e91b7c8937
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-09 14:38:53 +0530

    Time: 128 ms (55.86%), Space: 48.4 MB (77.45%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canJump(vector<int>& a) {
+        int i,j,n=a.size();
+        
+        if(n<=1)
+            return 1;
+        
+        int mx=0;
+        
+        for(i=0;i<n;i++)
+        {
+            if(i>mx)
+                return 0;
+            
+            mx=max(mx,a[i]+i);
+            
+            if(mx>=n-1)
+                return 1;
+        }
+        
+        return 0;
+    }
+};
