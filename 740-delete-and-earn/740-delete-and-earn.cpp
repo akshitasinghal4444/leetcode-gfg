fef9e06e609fc4ef22cd21acc8a48ba5ef31bd93
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-10 13:22:48 +0530

    Time: 28 ms (46.33%), Space: 16.3 MB (27.11%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,50 +1,24 @@
 class Solution {
 public:
-    int deleteAndEarn(vector<int>& a)
-    {
-        int mx=*(max_element(a.begin(),a.end()));
-        int mn=*(min_element(a.begin(),a.end()));
-        int n=mx-mn+1;
-                 
-        vector<int> dp(n,0);
+    int deleteAndEarn(vector<int>& a) {
+        int n=2 * 1e4+1;
         
-        for(int x:a)
-            dp[x-mn]+=x; 
-                 
+        vector<int> f(n,0);
         int inc=0,exc=0;
         
-        for(int i=0;i<n;i++)
+        for(int x:a)
+            f[x]++;
+        
+        for(int i=1;i<n;i++)
         {
             int ninc,nexc;
-            ninc=exc+dp[i];
+            ninc=exc+(i*f[i]);
             nexc=max(inc,exc);
             
             inc=ninc;
             exc=nexc;
         }
+        
         return max(inc,exc);
     }
-    
-    /*
-    int deleteAndEarn(vector<int>& a) {
-        int n=a.size();
-        int i,ans=0;
-        
-        int mx=*(max_element(a.begin(),a.end()));
-        int mn=*(min_element(a.begin(),a.end()));
-        
-        vector<int> dp(mx-mn+1,0);
-        for(i=0;i<n;i++)
-            dp[a[i]-mn]+=a[i];
-        
-        n=mx-mn+1;
-        if(n>1)
-        dp[1]=max(dp[0],dp[1]);
-        for(i=2;i<n;i++)
-        {
-            dp[i]=max(dp[i]+dp[i-2],dp[i-1]);
-        }
-        return dp[n-1];
-    }
-    */
 };
