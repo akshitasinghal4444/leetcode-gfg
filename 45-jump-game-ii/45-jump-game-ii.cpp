f3b55039a460d86417b6cb8d7dcb6c917ef4e23a
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-10 12:56:52 +0530

    Time: 1027 ms (9.79%), Space: 17.3 MB (26.41%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,67 +1,19 @@
 class Solution {
 public:
-    int jump(vector<int>& a)
-    {
-        int n=a.size();
-        if(n==1)
-            return 0;
-        
-        int j=0,curr_reach=0,max_reach=0;
-        int i;
-        
-        for(i=0;i<n-1;i++)
-        {
-            max_reach=max(max_reach,i+a[i]);
-            if(max_reach>=n-1)
-                return j+1;
-            
-            if(i==curr_reach)
-            {
-                curr_reach=max_reach;
-                j++;
-            }
-        }
-        return j;
-    }
-    
-    /*
     int jump(vector<int>& a) {
-        int n=a.size();
+        int i,j,n=a.size();
         vector<int> dp(n,INT_MAX);
         dp[0]=0;
-        int i,j;
         
         for(i=0;i<n-1;i++)
         {
-            for(j=min(i+a[i],n-1);j>i;j--)
-            {
-                if(dp[j]>dp[i]+1)
-                    dp[j]=dp[i]+1;
-                else
-                    break;
-            }
+            if(dp[i]==INT_MAX)
+                continue;
+            
+            for(j=1;j<=a[i] && i+j<n;j++)
+                dp[i+j]=min(dp[i+j],dp[i]+1);
         }
+        
         return dp[n-1];
     }
-    */
-    
-    /*
-    int jump(vector<int>& a) {
-        int n=a.size();
-        vector<int> dp(n,INT_MAX);
-        dp[n-1]=0;
-        int i,j;
-        
-        for(i=n-2;i>=0;i--)
-        {
-            for(j=1;j<=a[i] && j+i<n;j++)
-            {
-                if(dp[i+j]!=INT_MAX)
-                dp[i]=min(dp[i],dp[i+j]+1);
-        
-            }
-        }
-        return dp[0];
-    }
-    */
 };
