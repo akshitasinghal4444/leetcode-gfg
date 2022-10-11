d7f2debdc44de3f3fd830cc4306502d477782669
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-11 17:27:46 +0530

    Time: 44 ms (50.24%), Space: 16.4 MB (96.52%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    int jump(vector<int>& a)
+    {
+        int i,j,n=a.size();
+        
+        if(n==1)
+        return 0;
+        
+        int mx=0,curr=0;
+        
+        for(i=0,j=0;i<n-1;i++)
+        {
+            mx=max(mx,i+a[i]);
+            
+            if(mx>=n-1)
+                return j+1;
+            
+            if(i==curr)
+            {
+                curr=mx;
+                j++;
+            }
+        }
+        
+        return j+1;
+    }
+    
+    /*
+    int jump(vector<int>& a)
+    {
+        int i,j,n=a.size();
+        vector<int> dp(n,INT_MAX);
+        dp[0]=0;
+        
+        for(i=0;i<n-1;i++)
+        {
+            for(j=min(n-1,i+a[i]);j>i;j--)
+            {
+                if(j==n-1)
+                    return dp[i]+1;
+                
+                if(dp[j]>dp[i]+1)
+                    dp[j]=dp[i]+1;
+                else
+                    break;
+            }
+        }
+        
+        return dp[n-1];
+    }
+    */
+    
+    /*
+    int jump(vector<int>& a) {
+        int i,j,n=a.size();
+        vector<int> dp(n,INT_MAX);
+        dp[0]=0;
+        
+        for(i=0;i<n-1;i++)
+        {
+            if(dp[i]==INT_MAX)
+                continue;
+            
+            for(j=1;j<=a[i] && i+j<n;j++)
+            {
+                dp[i+j]=min(dp[i+j],dp[i]+1);
+                if(i+j==n-1)
+                    return dp[n-1];
+            }                
+        }
+        
+        return dp[n-1];
+    }
+    */
+};
