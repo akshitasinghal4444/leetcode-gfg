44c0db77ed7ad4f7a7c88e71899d3e7bdd20cb9f
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-10-14 12:15:26 +0530

    Time: 17 ms (87.38%), Space: 7.5 MB (84.52%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     
+    int minDistance(string s1, string s2)
+    {
+        int n1=s1.length(),n2=s2.length();
+        int i,j;
+        int dp[n1+1][n2+1];
+        
+        for(i=0;i<=n1;i++)
+        {
+            for(j=0;j<=n2;j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j]=i+j;
+                else if(s1[i-1]==s2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=min(dp[i-1][j],dp[i][j-1])+1;
+            }
+        }
+        
+        return dp[n1][n2];
+    }
+    
+    /*
     int lcs(string s1, string s2) {
         int n1=s1.length(),n2=s2.length();
         int i,j;
@@ -27,4 +50,5 @@ public:
         
         return s1.length()-n+s2.length()-n;
     }
+    */
 };
