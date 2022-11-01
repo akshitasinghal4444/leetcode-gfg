e82f3610c9d9493efe3412fe220833a8dee7d72c
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-11-01 12:35:34 +0530

    Time: 65 ms (23.74%), Space: 13.1 MB (95.61%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    
+    vector<int> findBall(vector<vector<int>>& a) {
+        int m=a.size(),n=a[0].size();
+        int i,j,k;
+        vector<int> ans;
+        
+        for(k=0;k<n;k++)
+        {
+            for(i=0,j=k;i<m;i++)
+            {
+                int next=j+a[i][j];
+                
+                if(next<0 || next>=n || a[i][j]!=a[i][next])
+                {
+                    ans.push_back(-1);
+                    break;
+                }
+                
+                j=next;
+            }
+            
+            if(i==m)
+            ans.push_back(j);
+        }
+        
+        return ans;
+    }
+    
+    /*
     vector<int> findBall(vector<vector<int>>& a) {
         int m=a.size(),n=a[0].size();
         int i,j,k;
@@ -24,4 +53,5 @@ public:
         
         return ans;
     }
+    */
 };
