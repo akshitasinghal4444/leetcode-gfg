e3db523e3198eade27144a8da62bb2fc185f73c0
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-11-01 12:29:40 +0530

    Time: 65 ms (23.74%), Space: 13.3 MB (52.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& a) {
         int m=a.size(),n=a[0].size();
-        long i,j,k;
+        int i,j,k;
         vector<int> ans;
         
         for(k=0;k<n;k++)
         {
-            j=k;
-            
-            for(i=0;i<m;i++)
+            for(i=0,j=k;i<m;i++)
             {
-                if(j<n-1 && a[i][j]==1 && a[i][j+1]==-1)
-                   break;
-                
-                if(j>0 && a[i][j]==-1 && a[i][j-1]==1)
-                   break;
-                
-                j+=a[i][j];
-                if(j<0 || j>=n)
+                if((j<n-1 && a[i][j]==1 && a[i][j+1]==-1) || (j>0 && a[i][j]==-1 && a[i][j-1]==1) || (j==0 && a[i][j]==-1) || (j==n-1 && a[i][j]==1))
+                {
+                    ans.push_back(-1);
                     break;
+                }
                 
+                if(a[i][j]==1)
+                    j++;
+                else
+                    j--;
             }
-            if(i!=m || j<0 || j==n)
-                ans.push_back(-1);
-            else
-                ans.push_back(j);
+            
+            if(i==m)
+            ans.push_back(j);
         }
         
         return ans;
