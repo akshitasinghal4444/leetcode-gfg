1b45f2ba636c4c1946b93768a64d2ce393c61041
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-11-07 12:13:36 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (45.85%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int maximum69Number (int num) {
         string s=to_string(num);
         int i,n=s.length();
@@ -14,5 +15,29 @@ public:
         }
         
         return stoi(s);
+    }
+    */
+    
+    int maximum69Number (int n)
+    {
+        int i;
+        int ans=0;
+        bool f=0;
+        
+        for(i=10000;i>0;i/=10)
+        {
+            if(!f && n/i==6)
+            {
+                ans=ans*10+9;
+                f=1;
+            }       
+            else
+                ans=ans*10+(n/i);
+            
+            n%=i;   
+            
+      }
+                
+        return ans;
     }
 };
