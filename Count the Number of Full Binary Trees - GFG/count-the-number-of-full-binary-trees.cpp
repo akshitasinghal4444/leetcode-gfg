01ccfb3b005ac184a48e2cdd1b453d55cc31f6ff
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2022-11-29 19:32:04 +0530

    Added solution - LeetHub

--- Count the Number of Full Binary Trees - GFG/count-the-number-of-full-binary-trees.cpp
+++ Count the Number of Full Binary Trees - GFG/count-the-number-of-full-binary-trees.cpp
@@ -5,74 +5,53 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 public:
-//   long long int numoffbt(long long int arr[], int n){
-        
-//         // Time complexity O(n*sqrt(n)) 
-//         // Space complexity O(n)
-        
-//         // To reduce the time complexity instead of sorting
-        
-//         long long int mini = 1e9;
-//         long long int maxi = -1e9;
-        
-//         int m[int(1e5+1)];
-//         memset(m, 0, sizeof(m));
-        
-//         for ( int i = 0; i<n; i++ ){
-            
-//             m[arr[i]] = 1;
-//             mini = min(arr[i], mini);
-//             maxi = max(arr[i], maxi);
-            
-//         }
-        
-        
-// // --------------------------------------------------------------------------------------        
-        
-//         long int mod = 1000000007;
-//         long int ans = 0;
-        
-//         for ( int i = mini; i<=maxi; i++ ){
-            
-//             if ( m[i] == 0 )
-//                 continue;
-            
-//             if ( m[i] != 1 )
-//                 continue;
-            
-//             for ( int j = mini; j*j<= i; j++ ){
-                
-//                 if ( i % j == 0 && m[i/j] != 0 && m[j] != 0 ){
-                    
-//                     if ( i/j != j )
-//                         m[i] += 2*(m[i/j] * m[j]);
-                        
-//                     else
-//                          m[i] += m[j]*m[j];
-                         
-//                     m[i] %= mod;
-                
-                    
-//                 }
-                
-//             }
-            
-//             ans += m[i];
-//             ans %= mod;
-            
-//         }
-            
-//         return ans;
-        
-//     }
-    
     long long int numoffbt(long long int a[], int n){
         // Your code goes here
         int mod=1000000007;
         long long i,j,mn,mx,ans=0;
         mx=mn=a[0];
-        // mx=*max_element(a,a+n);
+
+        vector<int> dp(1e5+1,0);
         
+        for(i=0;i<n;i++)
+        {
+            mx=max(mx,a[i]);
+            mn=min(mn,a[i]);
+            dp[a[i]]=1;
+        }
+
+        for(i=mn;i<mx+1;i++)
+        {
+            if(dp[i]==0)
+            continue;
+            
+            for(j=i+i;j/i<=i && j<=mx;j+=i)
+            {
+                if(dp[j]!=0 && dp[j/i]!=0)
+                {
+                    if(j/i==i)
+                    dp[j]+=dp[i]*dp[i];
+                    else
+                    dp[j]+=(2*dp[i]*dp[j/i]);
+                    
+                    dp[j]%=mod;
+                }
+            }
+            
+            ans=(ans+dp[i])%mod;
+            // cout<<i<<" "<<dp[i]<<" "<<ans<<endl; 
+        }
+        
+        return ans;
+    }
+    
+    /*
+    long long int numoffbt(long long int a[], int n){
+        // Your code goes here
+        int mod=1000000007;
+        long long i,j,mn,mx,ans=0;
+        mx=mn=a[0];
+
         vector<int> dp(1e5+1,0);
         
         for(i=0;i<n;i++)
@@ -105,6 +84,7 @@ public:
         
         return ans;
     }
+    */
 };
 
 
