6d618b6d6f1abd7feeb4292b67b3a06a33295f56
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2023-01-19 12:20:38 +0530

    Added solution - LeetHub

---
+++ Carpet into Box - GFG/carpet-into-box.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int carpetBox(int a, int b, int c, int d){
+        //code here
+        if(a>b)
+        swap(a,b);
+        
+        if(c>d)
+        swap(c,d);
+        
+        if(a<=c && b<=d)
+        return 0;
+        
+        if(b<=d)
+        return 1+carpetBox(a/2,b,c,d);
+        
+        if(a<=c || a<=d)
+        return 1+carpetBox(a,b/2,c,d);
+        
+        return 2+carpetBox(a/2,b/2,c,d);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int A,B,C,D;
+        cin>>A>>B>>C>>D;
+        
+        Solution ob;
+
+        int ans = ob.carpetBox(A,B,C,D);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
