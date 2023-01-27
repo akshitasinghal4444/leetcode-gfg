c426cf44332e72bb9d0024ba83b191457a6bf583
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2023-01-27 13:07:57 +0530

    Added solution - LeetHub

--- Total Decoding Messages - GFG/total-decoding-messages.cpp
+++ Total Decoding Messages - GFG/total-decoding-messages.cpp
@@ -1,49 +1,57 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution {
 	public:
-	
-	int m=1e9+7;
-	
-	int CountWays(string s){
-    // Code here
-        int n=s.length();
-		int i;
-		
-		if(s[0]=='0')
-		return 0;
-		
-		vector<int> dp(n,0);
-		
-		dp[0]=1;
-		for(i=1;i<n;i++)
-		{
-		    if(s[i]=='0')
+		int CountWays(string s){
+		    // Code here
+		    int i,n=s.length();
+		    vector<int> dp(n,0);
+		    int m=1e9+7;
+		    
+		    if(n==0)
+		    return 1;
+		    
+		    if(s[0]=='0')
+		    return 0;
+		    
+		    dp[0]=1;
+		    
+		    for(i=1;i<n;i++)
 		    {
-		        if(s[i-1]=='1' || s[i-1]=='2')
-		        dp[i]+=(i-2>=0?dp[i-2]:1);
-		        else
-		        return 0;
-		    }
-		    else
-		    {
-		        dp[i]=dp[i-1];
-		        if(s[i-1]!='0')
+		        if(s[i]=='0')
 		        {
-		            if(stoi(s.substr(i-1,2))<=26)
-		            dp[i]=(dp[i]+(i-2>=0?dp[i-2]:1))%m;
+		            if(s[i-1]=='0' || (s[i-1]!='1' && s[i-1]!=2))
+		            return 0;
+		            
+		            dp[i]=(i-2>=0)?dp[i-2]:1;
 		        }
+		        else
+		        {
+		            dp[i]=dp[i-1];
+		            
+		            if(s[i-1]!='0')
+		            {
+		                string t="";
+		                t+=s[i-1];
+		                t+=s[i];
+		                
+		                if(stoi(t)<=26)
+		                dp[i]+=(i-2>=0)?dp[i-2]:1;
+		            }
+		        }
+		        
+		        dp[i]%=m;
 		    }
+		    
+		    return dp[n-1];
 		}
-		return dp[n-1];
-	}
 
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main(){
 	int tc;
 	cin >> tc;
@@ -55,4 +63,5 @@ int main(){
 		cout << ans << "\n";
 	}
 	return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
