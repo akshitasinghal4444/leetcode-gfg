59fc4e74db855ff1bf60e52af5f175971174d3f5
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2023-02-25 20:42:59 +0530

    Added solution - LeetHub

---
+++ Valid Compressed String - GFG/valid-compressed-string.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int checkCompressed(string s, string t) {
+        // code here
+        int i,j,ns=s.length(),nt=t.length();
+        i=j=0;
+        
+        while(j<nt && i<ns)
+        {
+            if(t[j]>='A' && t[j]<='Z')
+            {
+                if(s[i]!=t[j])
+                return 0;
+            }
+            else
+            {
+                string x="";
+                
+                while(j<nt && !(t[j]>='A' && t[j]<='Z'))
+                x+=t[j++];
+                
+                i+=stoi(x);
+                i--;
+                j--;
+            }
+            
+            i++;
+            j++;
+        }
+        
+        if(i==ns && j==nt)
+        return 1;
+        return 0;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S,T;
+        
+        cin>>S>>T;
+
+        Solution ob;
+        cout << ob.checkCompressed(S,T) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
