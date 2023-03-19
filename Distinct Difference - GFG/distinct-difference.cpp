79eecdf596905326df668dcd04b6df1f5ec89af1
Akshita Singhal <56424809+akshitasinghal4444@users.noreply.github.com>
2023-03-19 16:50:35 +0530

    Added solution - LeetHub

---
+++ Distinct Difference - GFG/distinct-difference.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> getDistinctDifference(int n, vector<int> &a) {
+        // code here
+        vector<int> ans(n);
+        unordered_set<int> s1,s2;
+        int i;
+        
+        for(i=0;i<n;i++)
+        {
+            ans[i]=s1.size();
+            s1.insert(a[i]);
+        }
+        
+        for(i=n-1;i>=0;i--)
+        {
+            ans[i]-=s2.size();
+            s2.insert(a[i]);
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        vector<int> res = obj.getDistinctDifference(N, A);
+        
+        Array::print(res);
+        
+    }
+}
+
+// } Driver Code Ends
